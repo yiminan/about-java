97dafb86a8de77dda5eedc5d6a13a731c5cba19a
orgojy <giveawesome@gmail.com>
2021-02-22 12:07:59 +0900

    feat: comment for class "DontUseEr"

--- src/elegantobject/dontuseer/DontUseEr.java
+++ src/elegantobject/dontuseer/DontUseEr.java
@@ -1,5 +1,16 @@
 package elegantobject.dontuseer;
 
+/**
+ * 클래스는 객체의 팩토리입니다.
+ * new 키워드도 static method
+ * 클래스의 이름은 무엇을 하는지(what he does)보다 무엇인지(what he is)로 객체를 만들어야 합니다.
+ * 무엇을 하는지로 이름을 짓다보면 -er, -or과 같은 접미사가 붙게 됩니다. 이 접미사는 사용하면 안좋습니다.
+ * ex) Handler, Writer, Reader, Validator
+ * 예외도 물론 존재합니다. 처음에는 무엇을 하는지로 만들어졌다가 무엇인지로 변한 단어들입니다.
+ * ex) Computer, User
+ * 객체는 캡슐화된 데이터의 대표자입니다. 스스로 상태를 가지고, 행위가 가능해야합니다.
+ * PrimerFinder(X) => PrimerNumber(O)
+ */
 public class DontUseEr {
 
     public static void main(String[] args) {
