99d5defecf6fc821cc08b9bb94707e30c3e50cb9
orgojy <giveawesome@gmail.com>
2021-02-27 13:16:00 +0900

    feat: add invalid example class for "InvalidCash"

--- src/elegantobject/dontinputcodeinconstructor/DontInputCodeInConstructor.java
+++ src/elegantobject/dontinputcodeinconstructor/DontInputCodeInConstructor.java
@@ -1,4 +1,11 @@
 package elegantobject.dontinputcodeinconstructor;
 
 public class DontInputCodeInConstructor {
+    static class InvalidCash {
+        private final int dollars;
+
+        public InvalidCash(String dollars) {
+            this.dollars = Integer.parseInt(dollars);
+        }
+    }
 }
