2465d2e65319f702aa10b8bb808f6225144073cd
orgojy <giveawesome@gmail.com>
2021-02-27 14:25:02 +0900

    feat: caching class "CachedValidCash"

--- src/elegantobject/dontinputcodeinconstructor/DontInputCodeInConstructor.java
+++ src/elegantobject/dontinputcodeinconstructor/DontInputCodeInConstructor.java
@@ -44,6 +44,25 @@ public class DontInputCodeInConstructor {
         }
     }
 
+    static class CachedValidCash {
+        private final Number dollars;
+
+        // 생성자는 값을 셋팅만 하고, 필요한 시점에 변환해서 가져와서 사용한다.
+        // 부 생성자
+        public CachedValidCash(String dollars) {
+            this(new CachedNumber(new StringAsInteger(dollars)));
+        }
+
+        // 주 생성자
+        public CachedValidCash(Number dollars) {
+            this.dollars = dollars;
+        }
+
+        public Number getDollars() {
+            return dollars;
+        }
+    }
+
     interface Number {
         int intValue();
     }
@@ -60,6 +79,26 @@ public class DontInputCodeInConstructor {
         @Override
         public int intValue() {
             return Integer.parseInt(this.source);
+        }
+    }
+
+    static class CachedNumber implements Number {
+
+        private final Number origin;
+        private final List<Integer> cached = new ArrayList<>(1);
+
+        public CachedNumber(Number number) {
+            this.origin = number;
+        }
+
+        // 캐싱이 안되어있으면 최초 한 번 캐싱하고,
+        // 캐싱이 되어있으면, 캐시 값을 반환
+        @Override
+        public int intValue() {
+            if (this.cached.isEmpty()) {
+                this.cached.add(this.origin.intValue());
+            }
+            return this.cached.get(0);
         }
     }
 }
