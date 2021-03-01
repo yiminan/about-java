8b26880cb158281a96d2591caa9400f477d791bc
orgojy <giveawesome@gmail.com>
2021-03-01 22:53:13 +0900

    feat: expression for calculation for 1 year

--- src/elegantobject/atleastencapsulatesomething/AtLeastEncapsulateSomething.java
+++ src/elegantobject/atleastencapsulatesomething/AtLeastEncapsulateSomething.java
@@ -4,7 +4,7 @@ public class AtLeastEncapsulateSomething {
 
     static class WrongYear {
         public long read() {
-            return System.currentTimeMillis() / (1000L * 60L * 60L * 24L * 30L * 12L) - 1970L;
+            return System.currentTimeMillis() / (1000L * 60L * 60L * 24L * 365L) + 1970L;
         }
     }
 }
