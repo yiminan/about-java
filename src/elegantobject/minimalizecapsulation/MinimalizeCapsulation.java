6121ce88607ef2fd448b283f3887dcf66afef948
orgojy <giveawesome@gmail.com>
2021-03-01 13:43:44 +0900

    feat: example class "Cash" for capsulation

--- src/elegantobject/minimalizecapsulation/MinimalizeCapsulation.java
+++ src/elegantobject/minimalizecapsulation/MinimalizeCapsulation.java
@@ -1,4 +1,15 @@
 package elegantobject.minimalizecapsulation;
 
 public class MinimalizeCapsulation {
+    static class Cash {
+        private final Integer dollars;
+        private final Integer cents;
+        private final String currency;
+
+        public Cash(Integer dollars, Integer cents, String currency) {
+            this.dollars = dollars;
+            this.cents = cents;
+            this.currency = currency;
+        }
+    }
 }
