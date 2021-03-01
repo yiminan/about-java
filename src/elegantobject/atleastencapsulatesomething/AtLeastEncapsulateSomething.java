e96676a95b2dbfa96aeed0252331ff15ff8fdcc4
orgojy <giveawesome@gmail.com>
2021-03-01 22:54:05 +0900

    feat: interface for Number to apply OCP

--- src/elegantobject/atleastencapsulatesomething/AtLeastEncapsulateSomething.java
+++ src/elegantobject/atleastencapsulatesomething/AtLeastEncapsulateSomething.java
@@ -7,4 +7,12 @@ public class AtLeastEncapsulateSomething {
             return System.currentTimeMillis() / (1000L * 60L * 60L * 24L * 365L) + 1970L;
         }
     }
+
+    interface Number<T> {
+        T divide(long digits);
+
+        T plus(long digits);
+
+        long longValue();
+    }
 }
