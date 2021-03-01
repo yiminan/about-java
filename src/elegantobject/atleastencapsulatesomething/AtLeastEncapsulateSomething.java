75e1ec37cc76548b6f36fdb043e9bbd620eac0eb
orgojy <giveawesome@gmail.com>
2021-03-01 21:38:07 +0900

    feat: wrong example class for "WrongYear"

--- src/elegantobject/atleastencapsulatesomething/AtLeastEncapsulateSomething.java
+++ src/elegantobject/atleastencapsulatesomething/AtLeastEncapsulateSomething.java
@@ -1,4 +1,10 @@
 package elegantobject.atleastencapsulatesomething;
 
 public class AtLeastEncapsulateSomething {
+
+    static class WrongYear {
+        public long read() {
+            return System.currentTimeMillis() / (1000L * 60L * 60L * 24L * 30L * 12L) - 1970L;
+        }
+    }
 }
