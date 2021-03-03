8a3cc28aab219945a948048b3289c731a54bc7f9
orgojy <giveawesome@gmail.com>
2021-03-03 11:53:44 +0900

    feat: implementation class "DefaultCash" about interface "Cash"

--- src/elegantobject/usealwaysinterface/UseAlwaysInterface.java
+++ src/elegantobject/usealwaysinterface/UseAlwaysInterface.java
@@ -5,4 +5,22 @@ class UseAlwaysInterface {
         Cash multiply(int factor);
         int getCash();
     }
+
+    private static class DefaultCash implements Cash {
+        private final int dollars;
+
+        public DefaultCash(int dollars) {
+            this.dollars = dollars;
+        }
+
+        @Override
+        public Cash multiply(int factor) {
+            return new DefaultCash(this.dollars * factor);
+        }
+
+        @Override
+        public int getCash() {
+            return this.dollars;
+        }
+    }
 }
