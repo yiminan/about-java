8b904be958ae8185f4bb786aa1af5bb232a54ecf
orgojy <giveawesome@gmail.com>
2021-03-05 22:47:32 +0900

    feat: example interface "BuilderMethod" for class "MakeMethodNameCarefully"

--- src/elegantobject/education/makemethodnamecarefully/MakeMethodNameCarefully.java
+++ src/elegantobject/education/makemethodnamecarefully/MakeMethodNameCarefully.java
@@ -1,6 +1,17 @@
 package elegantobject.education.makemethodnamecarefully;
 
 class MakeMethodNameCarefully {
+
+    interface BuilderMethod {
+        int pow(int base, int power);
+
+        float speed();
+
+        Employee employee(int id);
+
+        String parseCell(int x, int y);
+    }
+
     private static class Employee {
     }
 }
