9365fa7ffb12910617589b0a4bd6bdc2dcf584ed
orgojy <giveawesome@gmail.com>
2021-03-05 23:01:29 +0900

    feat: comment for example method "BuilderMethod"

--- src/elegantobject/education/makemethodnamecarefully/MakeMethodNameCarefully.java
+++ src/elegantobject/education/makemethodnamecarefully/MakeMethodNameCarefully.java
@@ -2,6 +2,10 @@ package elegantobject.education.makemethodnamecarefully;
 
 class MakeMethodNameCarefully {
 
+    /**
+     * Builder Method의 예시입니다.
+     * 반환 타입은 void가 될 수 없으며, 항상 이름은 명사이어야합니다.
+     */
     interface BuilderMethod {
         int pow(int base, int power);
 
