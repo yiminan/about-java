e0f0d5f43a8a1975bd236c8c88492735ac3f2c07
orgojy <giveawesome@gmail.com>
2021-03-06 16:07:07 +0900

    feat: comment for interface "BuilderMethod"

--- src/elegantobject/education/makemethodnamecarefully/MakeMethodNameCarefully.java
+++ src/elegantobject/education/makemethodnamecarefully/MakeMethodNameCarefully.java
@@ -5,7 +5,7 @@ class MakeMethodNameCarefully {
     /**
      * Builder Method
      * 반환 타입 : void가 될 수 없다
-     * 이름 : 명사
+     * 이름 : 명사 (명사 or 형용사 + 명사)
      */
     private interface BuilderMethod {
         int pow(int base, int power);
