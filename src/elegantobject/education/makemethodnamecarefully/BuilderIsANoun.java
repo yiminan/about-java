7c5cb9e12684be064eeaca8eb3139d4505b4a9ec
orgojy <giveawesome@gmail.com>
2021-03-07 19:03:27 +0900

    feat: comment for interface "GoodBuilder"

--- src/elegantobject/education/makemethodnamecarefully/BuilderIsANoun.java
+++ src/elegantobject/education/makemethodnamecarefully/BuilderIsANoun.java
@@ -20,14 +20,21 @@ public class BuilderIsANoun {
         int add(int x, int y);
     }
 
+    /**
+     * 잘 사용된 빌더 이름 예시입니다.
+     * 객체를 존중해주고, 원하는 부분만 요청을 통해서 가져옵니다.
+     */
     private interface GoodBuilder {
         // InputStream load(URL url); =변환=>
+        // 직접 load를 지시하지 않고, url을 통해서 stream을 요청해서 가져옵니다.
         InputStream stream(URL url);
 
         // String read(File file); =변환=>
+        // 직접 read를 지시하지 않고, file을 전달하면 content를 요청해서 가져옵니다.
         String content(File file);
 
         // int add(int x, int y); =변환=>
+        // 직접 add를 GoodBuilder에게 지시하지 않고, sum을 요청해서 가져옵니다.
         int sum(int x, int y);
     }
 }
