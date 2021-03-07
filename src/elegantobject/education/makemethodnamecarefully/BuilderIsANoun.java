41d653dd1a19da49f99266f977e8869c3c6bb768
orgojy <giveawesome@gmail.com>
2021-03-07 17:35:15 +0900

    feat: example wrong interface "WrongBuilder"

--- src/elegantobject/education/makemethodnamecarefully/BuilderIsANoun.java
+++ src/elegantobject/education/makemethodnamecarefully/BuilderIsANoun.java
@@ -1,4 +1,16 @@
 package elegantobject.education.makemethodnamecarefully;
 
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+
 public class BuilderIsANoun {
+
+    private interface WrongBuilder {
+        InputStream load(URL url);
+
+        String read(File file);
+
+        int add(int x, int y);
+    }
 }
