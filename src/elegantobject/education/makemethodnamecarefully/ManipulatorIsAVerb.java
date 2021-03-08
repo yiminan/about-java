d85a46396e5b6a5aba0ae149a5349d79dadb395d
orgojy <giveawesome@gmail.com>
2021-03-08 21:33:21 +0900

    feat: example comment for manipulator in Pixel

--- src/elegantobject/education/makemethodnamecarefully/ManipulatorIsAVerb.java
+++ src/elegantobject/education/makemethodnamecarefully/ManipulatorIsAVerb.java
@@ -3,7 +3,6 @@ package elegantobject.education.makemethodnamecarefully;
 class ManipulatorIsAVerb {
 
     private static class Pixel {
-
         private final int x;
         private final int y;
         private Color color;
@@ -13,10 +12,14 @@ class ManipulatorIsAVerb {
             this.y = y;
         }
 
+        /**
+         * 조정자 예시입니다.
+         * Pixel에는 color를 셋팅해달라고 요청합니다.
+         * 요청 이후에 무언 가를 달라고 하지 않습니다.
+         */
         void paint(Color color) {
             this.color = color;
         }
-
     }
 
     private static class Color {
