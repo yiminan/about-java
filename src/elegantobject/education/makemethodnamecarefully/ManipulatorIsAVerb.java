7cf27dd5db901a81b1e199b43bfb0eb5dd3a10e3
orgojy <giveawesome@gmail.com>
2021-03-08 21:28:07 +0900

    feat: example class "Pixel" for Manipulator

--- src/elegantobject/education/makemethodnamecarefully/ManipulatorIsAVerb.java
+++ src/elegantobject/education/makemethodnamecarefully/ManipulatorIsAVerb.java
@@ -1,4 +1,29 @@
 package elegantobject.education.makemethodnamecarefully;
 
 class ManipulatorIsAVerb {
+
+    private static class Pixel {
+
+        private final int x;
+        private final int y;
+        private Color color;
+
+        public Pixel(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        void paint(Color color) {
+            this.color = color;
+        }
+
+    }
+
+    private static class Color {
+        private final String name;
+
+        public Color(String name) {
+            this.name = name;
+        }
+    }
 }
