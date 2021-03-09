269d7aa08cc15bb444aee5438f5d126fdb24505d
orgojy <giveawesome@gmail.com>
2021-03-09 19:46:49 +0900

    feat: wrong example interface "WrongBooleanMethod"

--- src/elegantobject/education/makemethodnamecarefully/MethodReturnedBooleanIsAdjective.java
+++ src/elegantobject/education/makemethodnamecarefully/MethodReturnedBooleanIsAdjective.java
@@ -1,4 +1,11 @@
 package elegantobject.education.makemethodnamecarefully;
 
 class MethodReturnedBooleanIsAdjective {
+
+    private interface WrongBooleanMethod {
+
+        boolean equals(Object obj);
+
+        boolean exists();
+    }
 }
