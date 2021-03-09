e573788a25f91d229165a77a7302c832959d3fd4
orgojy <giveawesome@gmail.com>
2021-03-09 19:47:14 +0900

    feat: wrong example interface "GoodBooleanMethod"

--- src/elegantobject/education/makemethodnamecarefully/MethodReturnedBooleanIsAdjective.java
+++ src/elegantobject/education/makemethodnamecarefully/MethodReturnedBooleanIsAdjective.java
@@ -8,4 +8,23 @@ class MethodReturnedBooleanIsAdjective {
 
         boolean exists();
     }
+
+    private interface GoodBooleanMethod {
+
+        /**
+         * 잘못된 boolean equals(Object obj); 는 다음과 같이 변환한다. A is equal to obj.
+         */
+        boolean equalTo(Object obj);
+
+        /**
+         * 잘못된 boolean exists(); 는 다음과 같이 변환한다. A is present.
+         */
+        boolean present();
+
+        boolean empty();
+
+        boolean readable();
+
+        boolean negative();
+    }
 }
