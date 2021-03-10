721f134c10e6ed825bf043b73a128b7428800ccc
orgojy <giveawesome@gmail.com>
2021-03-10 20:47:37 +0900

    feat: wrong example class for "Public Constant"

--- src/elegantobject/education/donotusepublicconstant/DoNotUsePublicConstant.java
+++ src/elegantobject/education/donotusepublicconstant/DoNotUsePublicConstant.java
@@ -1,4 +1,30 @@
 package elegantobject.education.donotusepublicconstant;
 
 class DoNotUsePublicConstant {
+
+    /**
+     * 이 퍼블릭 상수를 사용하는 모든 객체의 결합도는 높아지고, 응집도는 낮아집니다.
+     */
+    private static class WrongPublicConstant {
+        public static final String EOL = "\r\n";
+    }
+
+    private static class WrongExampleCar {
+        void print() {
+            System.out.println("This is car." + WrongPublicConstant.EOL);
+        }
+    }
+
+    private static class WrongExampleBus {
+        void print() {
+            System.out.println("This is bus." + WrongPublicConstant.EOL);
+        }
+    }
+
+    /**
+     * 여기까지 위 예시는 퍼블릭 상수를 하나 사용하면서 코드의 중복도는 낮추었지만,
+     * 높은 결합도와 낮은 응집도를 구현하고 있습니다. 잘못된 구현입니다.
+     * <p>
+     * 여기서 부터는 객체를 이용해서 잘 구현된 퍼블릭 상수의 리팩토링 예시입니다.
+     */
 }
