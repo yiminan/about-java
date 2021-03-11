f3aca148733f25ddb4b2e4ba11aa4f6c1265ba16
orgojy <giveawesome@gmail.com>
2021-03-11 22:00:13 +0900

    feat: example main method for class "MakeAImmutableObject"

--- src/elegantobject/education/makeaimmutableobject/MakeAImmutableObject.java
+++ src/elegantobject/education/makeaimmutableobject/MakeAImmutableObject.java
@@ -1,6 +1,17 @@
 package elegantobject.education.makeaimmutableobject;
 
 class MakeAImmutableObject {
+
+    public static void main(String[] args) {
+        MutableCash five = new MutableCash(5);
+        five.multiply(10);
+        System.out.println(five);// $50
+
+        ImmutableCash _five = new ImmutableCash(5);
+        ImmutableCash _fifty = _five.multiply(10);
+        System.out.println(_fifty);// $50
+    }
+
     private static class MutableCash {
         private int dollars;
 
