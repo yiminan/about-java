26198f9a02c17656495ad0d92dccfbe60f5bbf61
orgojy <giveawesome@gmail.com>
2021-03-11 21:58:49 +0900

    feat: mutable example class for "MutableCash"

--- src/elegantobject/education/makeaimmutableobject/MakeAImmutableObject.java
+++ src/elegantobject/education/makeaimmutableobject/MakeAImmutableObject.java
@@ -1,4 +1,20 @@
 package elegantobject.education.makeaimmutableobject;
 
 class MakeAImmutableObject {
+    private static class MutableCash {
+        private int dollars;
+
+        public MutableCash(int dollars) {
+            this.dollars = dollars;
+        }
+
+        public void multiply(int factor) {
+            this.dollars *= factor;
+        }
+
+        @Override
+        public String toString() {
+            return "$" + dollars;
+        }
+    }
 }
