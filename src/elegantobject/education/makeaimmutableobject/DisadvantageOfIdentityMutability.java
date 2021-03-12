a637aed00e7818874d87559c2db5a4a7d5b4899d
orgojy <giveawesome@gmail.com>
2021-03-12 20:19:36 +0900

    feat: example class "ImmutableCash"

--- src/elegantobject/education/makeaimmutableobject/DisadvantageOfIdentityMutability.java
+++ src/elegantobject/education/makeaimmutableobject/DisadvantageOfIdentityMutability.java
@@ -1,4 +1,7 @@
 package elegantobject.education.makeaimmutableobject;
+
+import java.util.Objects;
+
 class DisadvantageOfIdentityMutability {
     /**
      * 가변 클래스 예시
@@ -24,6 +27,39 @@ class DisadvantageOfIdentityMutability {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             MutableCash that = (MutableCash) o;
+            return dollars == that.dollars;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(dollars);
+        }
+    }
+
+    /**
+     * 불변 클래스 예시
+     */
+    private static class ImmutableCash {
+        private final int dollars;
+
+        public ImmutableCash(int dollars) {
+            this.dollars = dollars;
+        }
+
+        public ImmutableCash multiply(int factor) {
+            return new ImmutableCash(this.dollars * factor);
+        }
+
+        @Override
+        public String toString() {
+            return "$" + dollars;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ImmutableCash that = (ImmutableCash) o;
             return dollars == that.dollars;
         }
 
