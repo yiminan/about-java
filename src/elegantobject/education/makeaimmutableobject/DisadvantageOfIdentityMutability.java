621b6449a067fd54110f4405bfeb8c0e6d427542
orgojy <giveawesome@gmail.com>
2021-03-12 20:06:06 +0900

    feat: example class "MutableCash"

--- src/elegantobject/education/makeaimmutableobject/DisadvantageOfIdentityMutability.java
+++ src/elegantobject/education/makeaimmutableobject/DisadvantageOfIdentityMutability.java
@@ -1,3 +1,22 @@
 package elegantobject.education.makeaimmutableobject;
 class DisadvantageOfIdentityMutability {
+    /**
+     * 가변 클래스 예시
+     */
+    private static class MutableCash {
+        private int dollars;
+
+        public MutableCash(int dollars) {
+            this.dollars = dollars;
+        }
+
+        public void multiply(int factor) {
+            this.dollars *= factor;
+        }
+
+        @Override
+        public String toString() {
+            return "$" + dollars;
+        }
+    }
 }
