a58bf91d440a13e239fcf211b3dc6e584d92c1b1
orgojy <giveawesome@gmail.com>
2021-03-13 16:29:38 +0900

    feat: failure example class "MutableCash"

--- src/elegantobject/education/makeaimmutableobject/AdvantageOfFailureAtomicity.java
+++ src/elegantobject/education/makeaimmutableobject/AdvantageOfFailureAtomicity.java
@@ -2,4 +2,16 @@ package elegantobject.education.makeaimmutableobject;
 
 class AdvantageOfFailureAtomicity {
 
+    private static class MutableCash {
+        private int dollars;
+        private int cents;
+
+        public void multiply(int factor) {
+            this.dollars *= factor;
+            if (true) {
+                throw new RuntimeException("객체의 연산 도중 예외 발생");
+            }
+            this.cents *= factor;
+        }
+    }
 }
