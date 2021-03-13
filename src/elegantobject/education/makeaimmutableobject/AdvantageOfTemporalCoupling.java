35ca1dd6139c31fa071fb602eda6c86927542df6
orgojy <giveawesome@gmail.com>
2021-03-13 18:49:52 +0900

    feat: example class "JavaBeansPatternCash" for "AdvantageOfTemporalCoupling"

--- src/elegantobject/education/makeaimmutableobject/AdvantageOfTemporalCoupling.java
+++ src/elegantobject/education/makeaimmutableobject/AdvantageOfTemporalCoupling.java
@@ -1,4 +1,22 @@
 package elegantobject.education.makeaimmutableobject;
 
 class AdvantageOfTemporalCoupling {
+
+    private static class JavaBeansPatternCash {
+        private int dollars;
+        private int cents;
+
+        public void setDollars(int dollars) {
+            this.dollars = dollars;
+        }
+
+        public void setCents(int cents) {
+            this.cents = cents;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("$%d.%d", dollars, cents);
+        }
+    }
 }
