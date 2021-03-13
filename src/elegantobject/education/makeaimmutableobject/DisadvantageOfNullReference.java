e23a8c016f225c90ac9ac752fe5c3299117ca50a
orgojy <giveawesome@gmail.com>
2021-03-14 00:48:37 +0900

    feat: example class "User" in DisadvantageOfNullReference

--- src/elegantobject/education/makeaimmutableobject/DisadvantageOfNullReference.java
+++ src/elegantobject/education/makeaimmutableobject/DisadvantageOfNullReference.java
@@ -1,4 +1,37 @@
 package elegantobject.education.makeaimmutableobject;
 
 class DisadvantageOfNullReference {
+
+    private static class User {
+        private final int id;
+        private Age age;
+
+        public User(int id) {
+            this.id = id;
+        }
+
+        public void setAge(Age age) {
+            this.age = age;
+        }
+
+        public int id() {
+            return id;
+        }
+
+        public Age age() {
+            return age;
+        }
+    }
+
+    private static class Age {
+        private final int value;
+
+        public Age(int value) {
+            this.value = value;
+        }
+
+        public int value() {
+            return value;
+        }
+    }
 }
