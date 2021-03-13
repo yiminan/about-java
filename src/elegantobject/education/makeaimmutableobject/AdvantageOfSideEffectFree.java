4fdc920bf9469981c302a8f522eb97ccd7742978
orgojy <giveawesome@gmail.com>
2021-03-13 20:24:08 +0900

    feat: example class "MutableObjectCash"

--- src/elegantobject/education/makeaimmutableobject/AdvantageOfSideEffectFree.java
+++ src/elegantobject/education/makeaimmutableobject/AdvantageOfSideEffectFree.java
@@ -1,4 +1,21 @@
 package elegantobject.education.makeaimmutableobject;
 
 class AdvantageOfSideEffectFree {
+
+    private static class MutableObjectCash {
+        private int dollars;
+
+        public MutableObjectCash(int dollars) {
+            this.dollars = dollars;
+        }
+
+        public void multiply(int factor) {
+            this.dollars *= factor;
+        }
+
+        @Override
+        public String toString() {
+            return "$" + dollars;
+        }
+    }
 }
