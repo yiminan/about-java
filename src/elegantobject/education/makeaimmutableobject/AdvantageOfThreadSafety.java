6e7aaadb67fe2bb526cab6d7811e26fb53865814
orgojy <giveawesome@gmail.com>
2021-03-14 15:01:28 +0900

    feat: example class "NonThreadSafetyCash"

--- src/elegantobject/education/makeaimmutableobject/AdvantageOfThreadSafety.java
+++ src/elegantobject/education/makeaimmutableobject/AdvantageOfThreadSafety.java
@@ -1,4 +1,23 @@
 package elegantobject.education.makeaimmutableobject;
 
 class AdvantageOfThreadSafety {
+    private static class NonThreadSafetyCash {
+        private int dollars;
+        private int cents;
+
+        public NonThreadSafetyCash(int dollars, int cents) {
+            this.dollars = dollars;
+            this.cents = cents;
+        }
+
+        public void multiply(int factor) {
+            this.dollars *= factor;
+            this.cents *= factor;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("$%d.%d", dollars, cents);
+        }
+    }
 }
