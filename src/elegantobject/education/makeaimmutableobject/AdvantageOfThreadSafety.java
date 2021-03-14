157ff7572839f698fc322e96a5cba320016a98b4
orgojy <giveawesome@gmail.com>
2021-03-14 15:25:43 +0900

    feat: example method to execute non-thread safety case

--- src/elegantobject/education/makeaimmutableobject/AdvantageOfThreadSafety.java
+++ src/elegantobject/education/makeaimmutableobject/AdvantageOfThreadSafety.java
@@ -1,6 +1,22 @@
 package elegantobject.education.makeaimmutableobject;
 
 class AdvantageOfThreadSafety {
+    private static void executeNonThreadSafetyCase() {
+        final NonThreadSafetyCash nonThreadSafetyCash = new NonThreadSafetyCash(10,45);
+        CountDownLatch start = new CountDownLatch(1);
+        Callable<Object> script = () -> {
+            start.await();
+            nonThreadSafetyCash.multiply(2);
+            System.out.println(nonThreadSafetyCash);
+            return null;
+        };
+        final ExecutorService svc = Executors.newCachedThreadPool();
+        IntStream.rangeClosed(1, TEST_SAMPLE_COUNT).forEach(i -> svc.submit(script));
+        start.countDown();
+
+        svc.shutdown();
+    }
+
     private static class NonThreadSafetyCash {
         private int dollars;
         private int cents;
