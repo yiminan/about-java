ff148d61ff8eecd75b99ff62ba128187f851d201
orgojy <giveawesome@gmail.com>
2021-03-15 19:24:50 +0900

    feat: test class "MakeATestInsteadOfDocsTest" for "Cash"

--- src/elegantobject/education/makeatestinsteadofdocs/MakeATestInsteadOfDocs.java
+++ src/elegantobject/education/makeatestinsteadofdocs/MakeATestInsteadOfDocs.java
@@ -1,5 +1,9 @@
 package elegantobject.education.makeatestinsteadofdocs;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 class MakeATestInsteadOfDocs {
     
     private static class Cash {
@@ -15,6 +19,32 @@ class MakeATestInsteadOfDocs {
 
         public Cash mul(int factor) {
             return new Cash(this.dollars * factor);
+        }
+    }
+
+    private static class MakeATestInsteadOfDocsTest {
+        @Test
+        void summarizes() {
+            assertEquals(
+                    new Cash(8),
+                    new Cash(5).plus(new Cash(3))
+            );
+        }
+
+        @Test
+        void deducts() {
+            assertEquals(
+                    new Cash(-4),
+                    new Cash(7).plus(new Cash(-11))
+            );
+        }
+
+        @Test
+        void multiplies() {
+            assertEquals(
+                    new Cash(6),
+                    new Cash(2).mul(3)
+            );
         }
     }
 }
