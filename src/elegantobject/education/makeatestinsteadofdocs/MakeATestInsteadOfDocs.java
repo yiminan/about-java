fdc5936bba017d99a20ed0aa2c60ed5ded28bcc3
orgojy <giveawesome@gmail.com>
2021-03-15 19:24:15 +0900

    feat: example class "Cash" in MakeATestInsteadOfDocs

--- src/elegantobject/education/makeatestinsteadofdocs/MakeATestInsteadOfDocs.java
+++ src/elegantobject/education/makeatestinsteadofdocs/MakeATestInsteadOfDocs.java
@@ -1,4 +1,20 @@
 package elegantobject.education.makeatestinsteadofdocs;
 
 class MakeATestInsteadOfDocs {
+    
+    private static class Cash {
+        private final int dollars;
+
+        public Cash(int dollars) {
+            this.dollars = dollars;
+        }
+
+        public Cash plus(Cash cash) {
+            return new Cash(this.dollars + cash.dollars);
+        }
+
+        public Cash mul(int factor) {
+            return new Cash(this.dollars * factor);
+        }
+    }
 }
