0d57858a9be54258820e21b713259bb56827958c
orgojy <giveawesome@gmail.com>
2021-03-17 21:56:07 +0900

    feat: example class "Cash" for "UseFakeObjectInsteadOfMockObject"

--- src/elegantobject/education/usefakeobjectinsteadofmockobject/UseFakeObjectInsteadOfMockObject.java
+++ src/elegantobject/education/usefakeobjectinsteadofmockobject/UseFakeObjectInsteadOfMockObject.java
@@ -1,4 +1,24 @@
 package elegantobject.education.usefakeobjectinsteadofmockobject;
 
 class UseFakeObjectInsteadOfMockObject {
+    private static class Cash {
+        private final Exchange exchange;
+        private final int cents;
+
+        public Cash(Exchange exchange, int cents) {
+            this.exchange = exchange;
+            this.cents = cents;
+        }
+
+        public Cash in(String currency) {
+            return new Cash(this.exchange,
+                    (int) (this.cents * this.exchange.rate("USD", currency))
+            );
+        }
+
+        public int cents() {
+            return cents;
+        }
+    }
+
 }
