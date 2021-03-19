0106a0daade2b60842b139a4e843a7242a949344
orgojy <giveawesome@gmail.com>
2021-03-19 16:10:05 +0900

    feat: example interface "Exchange"

--- src/elegantobject/education/useshortinterfaceusingbysmartclass/UseShortInterfaceUsingBySmartClass.java
+++ src/elegantobject/education/useshortinterfaceusingbysmartclass/UseShortInterfaceUsingBySmartClass.java
@@ -2,4 +2,7 @@ package elegantobject.education.useshortinterfaceusingbysmartclass;
 
 class UseShortInterfaceUsingBySmartClass {
 
+    private interface Exchange {
+        float rate(String origin, String target);
+    }
 }
