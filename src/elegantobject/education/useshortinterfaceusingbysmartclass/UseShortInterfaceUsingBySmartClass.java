29a9eeecf2e4b71a6cd5e1f51a8cc4d940f5dd79
orgojy <giveawesome@gmail.com>
2021-03-20 00:44:46 +0900

    feat: wrong example interface "WrongExchange"

--- src/elegantobject/education/useshortinterfaceusingbysmartclass/UseShortInterfaceUsingBySmartClass.java
+++ src/elegantobject/education/useshortinterfaceusingbysmartclass/UseShortInterfaceUsingBySmartClass.java
@@ -2,6 +2,17 @@ package elegantobject.education.useshortinterfaceusingbysmartclass;
 
 class UseShortInterfaceUsingBySmartClass {
 
+    private interface WrongExchange {
+        /**
+         * float rate(String origin, String target) 메서드만 있어도 충분합니다.
+         * 이 중복 기능을 하는 float rate(String target) 메서드는 필요없습니다.
+         * 중복 기능을 제공하는 이 메서드를 없애고 기능은 그대로 제공하기 위해서 Smart Class / Decorator Class를 구현합니다.
+         */
+        float rate(String target);
+
+        float rate(String origin, String target);
+    }
+
     private interface Exchange {
         float rate(String origin, String target);
 
