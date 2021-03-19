bc2685f99333fb1d8e93a8dd002993ea6d70e298
orgojy <giveawesome@gmail.com>
2021-03-19 16:11:15 +0900

    feat: smart class "Fast" implemented "Exchange"

--- src/elegantobject/education/useshortinterfaceusingbysmartclass/UseShortInterfaceUsingBySmartClass.java
+++ src/elegantobject/education/useshortinterfaceusingbysmartclass/UseShortInterfaceUsingBySmartClass.java
@@ -4,5 +4,28 @@ class UseShortInterfaceUsingBySmartClass {
 
     private interface Exchange {
         float rate(String origin, String target);
+
+        final class Fast implements Exchange {
+            private final Exchange orgin;
+
+            public Fast(Exchange orgin) {
+                this.orgin = orgin;
+            }
+
+            @Override
+            public float rate(String source, String target) {
+                final float rate;
+                if (source.equals(target)) {
+                    rate = 1.0f;
+                    return rate;
+                }
+                rate = this.orgin.rate(source, target);
+                return rate;
+            }
+
+            public float toUsd(String source) {
+                return this.orgin.rate(source, "USD");
+            }
+        }
     }
 }
