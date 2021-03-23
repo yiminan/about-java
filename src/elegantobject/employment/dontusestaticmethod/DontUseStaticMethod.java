a5b2c62e9273764e15116911161126bb92f18769
orgojy <giveawesome@gmail.com>
2021-03-23 20:30:40 +0900

    feat: example class for static method

--- src/elegantobject/employment/dontusestaticmethod/DontUseStaticMethod.java
+++ src/elegantobject/employment/dontusestaticmethod/DontUseStaticMethod.java
@@ -1,4 +1,11 @@
 package elegantobject.employment.dontusestaticmethod;
 
 class DontUseStaticMethod {
+    private static class WebPageWithStaticMethod {
+        public static String read(String uri) {
+            // HTTP 요청을 만들고,
+            // UTF-8 문자열로 변환한다.
+            return "";
+        }
+    }
 }
