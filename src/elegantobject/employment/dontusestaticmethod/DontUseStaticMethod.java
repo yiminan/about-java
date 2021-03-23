830fa6660d2cb7be96b20239c5bd9d349d37d50e
orgojy <giveawesome@gmail.com>
2021-03-23 20:32:58 +0900

    feat: example main method for "DontUseStaticMethod"

--- src/elegantobject/employment/dontusestaticmethod/DontUseStaticMethod.java
+++ src/elegantobject/employment/dontusestaticmethod/DontUseStaticMethod.java
@@ -1,6 +1,14 @@
 package elegantobject.employment.dontusestaticmethod;
 
 class DontUseStaticMethod {
+
+    public static void main(String[] args) {
+        // static method를 사용한 예시
+        System.out.println(WebPageWithStaticMethod.read("https://www.java.com"));
+        // object를 사용한 예시
+        System.out.println(new WebPageWithObject("https://www.java.com").content());
+    }
+
     private static class WebPageWithStaticMethod {
         public static String read(String uri) {
             // HTTP 요청을 만들고,
