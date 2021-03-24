f3d245412ec67dde502f76abd7e1c22f151ffefd
orgojy <giveawesome@gmail.com>
2021-03-24 10:31:22 +0900

    feat: example class "Max" to get max value between a and b

--- src/elegantobject/employment/dontusestaticmethod/ObjectVsComputerThinking.java
+++ src/elegantobject/employment/dontusestaticmethod/ObjectVsComputerThinking.java
@@ -1,6 +1,16 @@
 package elegantobject.employment.dontusestaticmethod;
 
 class ObjectVsComputerThinking {
+    private static class Max implements Number {
+        private final Number a;
+        private final Number b;
+
+        public Max(Number a, Number b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
     private static class Integer implements Number {
         private final int value;
 
