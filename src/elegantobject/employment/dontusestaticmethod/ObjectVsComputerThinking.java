d1506318e05e46ddf1d5f9d9cce75017091be680
orgojy <giveawesome@gmail.com>
2021-03-24 10:30:46 +0900

    feat: example class "Integer"

--- src/elegantobject/employment/dontusestaticmethod/ObjectVsComputerThinking.java
+++ src/elegantobject/employment/dontusestaticmethod/ObjectVsComputerThinking.java
@@ -1,6 +1,14 @@
 package elegantobject.employment.dontusestaticmethod;
 
 class ObjectVsComputerThinking {
+    private static class Integer implements Number {
+        private final int value;
+
+        public Integer(int value) {
+            this.value = value;
+        }
+    }
+
     private interface Number {
     }
 }
