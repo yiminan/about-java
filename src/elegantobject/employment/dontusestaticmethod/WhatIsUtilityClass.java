e66faaca0b2c3d9f6f5bb0050d89e99e9dcdf1c1
orgojy <giveawesome@gmail.com>
2021-03-31 19:42:39 +0900

    feat: comment for class "WhatIsUtilityClass"

--- src/elegantobject/employment/dontusestaticmethod/WhatIsUtilityClass.java
+++ src/elegantobject/employment/dontusestaticmethod/WhatIsUtilityClass.java
@@ -1,4 +1,10 @@
 package elegantobject.employment.dontusestaticmethod;
 
+/**
+ * "유틸리티 클래스(Utility classes)란?"
+ * 실제로 인스턴스화가 되지않고, "편의를 위한 정적 메서드들을 모아 놓은 정적 메서드들의 컬렉션"이라고 합니다.
+ * 다른 말로는 헬퍼(helper)라고 부릅니다.
+ * 유틸리티 클래스는 어떤 것의 팩토리가 아니기 때문에 진짜 클래스라고 부를 수 없습니다.
+ */
 public class WhatIsUtilityClass {
 }
