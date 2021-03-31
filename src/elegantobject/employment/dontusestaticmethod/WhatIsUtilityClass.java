73a20e9eb78db38e33c9fa9890c353184e6b92a4
orgojy <giveawesome@gmail.com>
2021-03-31 19:47:16 +0900

    feat: comment for class "WhatIsUtilityClass"

--- src/elegantobject/employment/dontusestaticmethod/WhatIsUtilityClass.java
+++ src/elegantobject/employment/dontusestaticmethod/WhatIsUtilityClass.java
@@ -5,6 +5,9 @@ package elegantobject.employment.dontusestaticmethod;
  * 실제로 인스턴스화가 되지않고, "편의를 위한 정적 메서드들을 모아 놓은 정적 메서드들의 컬렉션"이라고 합니다.
  * 다른 말로는 헬퍼(helper)라고 부릅니다.
  * 유틸리티 클래스는 어떤 것의 팩토리가 아니기 때문에 진짜 클래스라고 부를 수 없습니다.
+ * 의도적으로 'private contructor'를 추가해서 인스턴스화가 안되게 합니다.
+ * 유틸리티 클래스는 절차적인 프로그래머들이 OOP에서 거둔 승리의 상징입니다.
+ * 유틸리티 클래스는 나쁜 요소들을 모아 놓은 안티 패턴 집합체입니다.
  */
 public class WhatIsUtilityClass {
 
