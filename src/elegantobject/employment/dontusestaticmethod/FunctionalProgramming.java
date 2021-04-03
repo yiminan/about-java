e0f3361185c0f59efade19a4d2ecd001b7d2a39b
orgojy <giveawesome@gmail.com>
2021-04-03 12:56:37 +0900

    Add example class "Max" using Number

--- src/elegantobject/employment/dontusestaticmethod/FunctionalProgramming.java
+++ src/elegantobject/employment/dontusestaticmethod/FunctionalProgramming.java
@@ -5,4 +5,19 @@ class FunctionalProgramming {
     private interface Number {
         int intValue();
     }
+
+    private static class Max implements Number {
+        private final int a;
+        private final int b;
+
+        public Max(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public int intValue() {
+            return Math.max(this.a, this.b);
+        }
+    }
 }
