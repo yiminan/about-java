71d8dc9c289228e857c8126e712d2edcf4863427
orgojy <giveawesome@gmail.com>
2021-04-04 17:00:45 +0900

    Add class "Client" for "GoodOopExample"

--- src/elegantobject/employment/dontusestaticmethod/GoodOopExample.java
+++ src/elegantobject/employment/dontusestaticmethod/GoodOopExample.java
@@ -1,4 +1,15 @@
 package elegantobject.employment.dontusestaticmethod;
 
 class GoodOopExample {
+    private static class Client {
+        private final int age;
+
+        public Client(int age) {
+            this.age = age;
+        }
+
+        public int age() {
+            return age;
+        }
+    }
 }
