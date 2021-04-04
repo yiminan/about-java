4ef06e61f612ede79a8f38af6ed521e67ac6b35f
orgojy <giveawesome@gmail.com>
2021-04-04 14:28:31 +0900

    Add example main method for decorator in "ComposableDecorator"

--- src/elegantobject/employment/dontusestaticmethod/ComposableDecorator.java
+++ src/elegantobject/employment/dontusestaticmethod/ComposableDecorator.java
@@ -1,6 +1,17 @@
 package elegantobject.employment.dontusestaticmethod;
 
 class ComposableDecorator {
+
+    public static void main(String[] args) {
+        /**
+         * 프로그래머가 작성해야할 객체 인스턴스의 데코레이터 조합 형태
+         */
+        File file = new File(
+                new Directory("/var/usrs"),
+                new FileName("filename.jpg")
+        );
+    }
+
     private static class File {
         private final Directory directory;
         private final FileName fileName;
