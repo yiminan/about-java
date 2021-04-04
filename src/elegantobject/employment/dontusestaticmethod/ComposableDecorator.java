6bbd560ca36d2cb2e4dfc715832766ee35ddae85
orgojy <giveawesome@gmail.com>
2021-04-04 14:28:03 +0900

    Add example class "File" for decorator

--- src/elegantobject/employment/dontusestaticmethod/ComposableDecorator.java
+++ src/elegantobject/employment/dontusestaticmethod/ComposableDecorator.java
@@ -1,6 +1,16 @@
 package elegantobject.employment.dontusestaticmethod;
 
 class ComposableDecorator {
+    private static class File {
+        private final Directory directory;
+        private final FileName fileName;
+
+        public File(Directory directory, FileName fileName) {
+            this.directory = directory;
+            this.fileName = fileName;
+        }
+    }
+
     private static class Directory {
         private final String path;
 
