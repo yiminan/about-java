020ba298fd434b5494752ed297d49215dd25b538
orgojy <giveawesome@gmail.com>
2021-04-04 14:27:48 +0900

    Add example class "Directory" for decorator

--- src/elegantobject/employment/dontusestaticmethod/ComposableDecorator.java
+++ src/elegantobject/employment/dontusestaticmethod/ComposableDecorator.java
@@ -1,6 +1,14 @@
 package elegantobject.employment.dontusestaticmethod;
 
 class ComposableDecorator {
+    private static class Directory {
+        private final String path;
+
+        public Directory(String path) {
+            this.path = path;
+        }
+    }
+
     private static class FileName {
         private final String name;
 
