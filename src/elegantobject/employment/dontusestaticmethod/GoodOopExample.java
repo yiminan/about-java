aa38ba7f7ac94253e334db8490fc1d8d76e87449
orgojy <giveawesome@gmail.com>
2021-04-04 17:01:23 +0900

    Add class "If" for "GoodOopExample"

--- src/elegantobject/employment/dontusestaticmethod/GoodOopExample.java
+++ src/elegantobject/employment/dontusestaticmethod/GoodOopExample.java
@@ -1,6 +1,25 @@
 package elegantobject.employment.dontusestaticmethod;
 
 class GoodOopExample {
+    private static class If {
+        private final GreaterThan greaterThan;
+        private final float trueValue;
+        private final float falseValue;
+
+        public If(GreaterThan greaterThan, float trueValue, float falseValue) {
+            this.greaterThan = greaterThan;
+            this.trueValue = trueValue;
+            this.falseValue = falseValue;
+        }
+
+        public float result() {
+            if (greaterThan.right()) {
+                return trueValue;
+            }
+            return falseValue;
+        }
+    }
+
     private static class GreaterThan {
         private final AgeOf ageOf;
         private final int comparedAge;
