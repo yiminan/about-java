c1b7b70569b1284c836f4a30b84920e37248b85c
orgojy <giveawesome@gmail.com>
2021-04-06 23:52:12 +0900

    Add comment for class "DontAllowNullAsParameters"

--- src/elegantobject/employment/dontallownullasparameters/DontAllowNullAsParameters.java
+++ src/elegantobject/employment/dontallownullasparameters/DontAllowNullAsParameters.java
@@ -1,4 +1,11 @@
 package elegantobject.employment.dontallownullasparameters;
 
+/**
+ * "파라미터로 null을 절대 허용하면 안됩니다"
+ * "null 사용"은 "객체의 가변성", "정적 메서드"와 같이 OOP 골칫덩이입니다.
+ * 코드 어딘가 null이 존재한다면 잘못된 코드입니다.
+ * 이번에는 메서드의 인자값으로 null을 사용하는 경우를 살펴보겠습니다.
+ */
 class DontAllowNullAsParameters {
+
 }
