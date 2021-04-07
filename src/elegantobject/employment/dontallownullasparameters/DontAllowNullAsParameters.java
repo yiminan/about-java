c2fed4863191f5b8013d703cf9c1a04d4e4dc036
orgojy <giveawesome@gmail.com>
2021-04-07 22:20:26 +0900

    Update comments for class "DontAllowNullAsParameters"

--- src/elegantobject/employment/dontallownullasparameters/DontAllowNullAsParameters.java
+++ src/elegantobject/employment/dontallownullasparameters/DontAllowNullAsParameters.java
@@ -8,9 +8,19 @@ import java.util.Collections;
  * "null 사용"은 "객체의 가변성", "정적 메서드"와 같이 OOP 골칫덩이입니다.
  * 코드 어딘가 null이 존재한다면 잘못된 코드입니다.
  * 이번에는 메서드의 인자값으로 null을 사용하는 경우를 살펴보겠습니다.
+ * 메서드의 파라미터를 null을 허용하면 경우의 수가 줄어 메서드를 적게 만들어 효율적이라고 생각하지만,
+ * SRP(Single Responsibility Principle)에 위반되고, 메서드 내부 구현 내용이 복잡해집니다.
+ * 복잡해진 코드는 메서드의 이름으로 객체의 행동을 판단할 수 없습니다.
+ * 파라미터로 null을 전달하면, 내부에서 null check하는 조건문이 있는 경우가 있는데, 완전히 잘못된 구현방법입니다.
+ * <p>
+ * 애초에 OOP에서는 "null 사용"을 허용하면 안됩니다.
+ * 모든 메서드는 파라미터로 null을 허용하지 않고, 메서드 분리를 통해서 구현해야합니다.
  */
 class DontAllowNullAsParameters {
 
+    /**
+     * null을 허용하는 잘못된 예시의 메서드입니다.
+     */
     public Iterable<File> find(String mask) {
         if (mask == null) {
             /**
