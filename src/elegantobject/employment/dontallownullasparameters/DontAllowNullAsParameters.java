3ab0eabc0cb3a1a64483dc1e0a1c384f941e9c05
orgojy <giveawesome@gmail.com>
2021-04-07 21:32:08 +0900

    Add methods to find files in class "DontAllowNullAsParameters"

--- src/elegantobject/employment/dontallownullasparameters/DontAllowNullAsParameters.java
+++ src/elegantobject/employment/dontallownullasparameters/DontAllowNullAsParameters.java
@@ -1,6 +1,7 @@
 package elegantobject.employment.dontallownullasparameters;
 
 import java.io.File;
+import java.util.Collections;
 
 /**
  * "파라미터로 null을 절대 허용하면 안됩니다"
@@ -10,9 +11,23 @@ import java.io.File;
  */
 class DontAllowNullAsParameters {
 
-    private interface NonNullableParameter {
-        Iterable<File> findAll();
+    public Iterable<File> find(String mask) {
+        if (mask == null) {
+            /**
+             * findAll()과 같은 기능
+             * 모든 파일을 찾는 경우
+             */
+            return Collections.singleton(new File("")); // 실제 반환값은 모든 파일
+        } else {
+            /**
+             * find(String mask)와 같은 기능
+             * mask를 사용하여 해당하는 파일을 찾는 경우
+             */
+            return Collections.singleton(new File("")); // 실제는 특정 mask 파일
+        }
+    }
 
-        Iterable<File> find(String mask);
+    public Iterable<File> findAll() {
+        return Collections.singleton(new File("")); // 실제 반환값은 모든 파일
     }
 }
