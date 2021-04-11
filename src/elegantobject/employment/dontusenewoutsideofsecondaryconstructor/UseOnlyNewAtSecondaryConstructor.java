6856cd1ef56f59c2472617fb5b40751aa8a6d41e
orgojy <giveawesome@gmail.com>
2021-04-11 20:16:53 +0900

    Add example interface "Exchange" for class "UseOnlyNewAtSecondaryConstructor"

--- src/elegantobject/employment/dontusenewoutsideofsecondaryconstructor/UseOnlyNewAtSecondaryConstructor.java
+++ src/elegantobject/employment/dontusenewoutsideofsecondaryconstructor/UseOnlyNewAtSecondaryConstructor.java
@@ -2,4 +2,8 @@ package elegantobject.employment.dontusenewoutsideofsecondaryconstructor;
 
  */
 class UseOnlyNewAtSecondaryConstructor {
+
+    private interface Exchange {
+        float rate(String origin, String target);
+    }
 }
