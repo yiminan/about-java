43493431f62a30662a2a199b93b1c53d9d699ff9
orgojy <giveawesome@gmail.com>
2021-04-11 20:19:34 +0900

    Add example class "HardCodingCash" in "UseOnlyNewAtSecondaryConstructor"

--- src/elegantobject/employment/dontusenewoutsideofsecondaryconstructor/UseOnlyNewAtSecondaryConstructor.java
+++ src/elegantobject/employment/dontusenewoutsideofsecondaryconstructor/UseOnlyNewAtSecondaryConstructor.java
@@ -3,6 +3,23 @@ package elegantobject.employment.dontusenewoutsideofsecondaryconstructor;
  */
 class UseOnlyNewAtSecondaryConstructor {
 
+    private static class HardCodingCash {
+        private final int dollars;
+
+        public HardCodingCash(int dollars) {
+            this.dollars = dollars;
+        }
+
+        public float euro() {
+            /**
+             * 이 부분에서 환율을 제공하는 NYSE 객체를 하드코딩으로 사용합니다.
+             * 만약 사용되는 환율 서버가 바뀌거나 객체가 바뀌는 경우에는 내부 코드가 변경되어야합니다.
+             * 결국 문제가 되는 부분입니다.
+             */
+            return new NYSE().rate("USD", "EUR") * dollars;
+        }
+    }
+
     private static class NYSE implements Exchange {
         @Override
         public float rate(String origin, String target) {
