671a28f79a3f17bcd5c6560dabd87b9356c5a5c9
orgojy <giveawesome@gmail.com>
2021-04-11 20:18:35 +0900

    Add example class "NYSE" for "UseOnlyNewAtSecondaryConstructor"

--- src/elegantobject/employment/dontusenewoutsideofsecondaryconstructor/UseOnlyNewAtSecondaryConstructor.java
+++ src/elegantobject/employment/dontusenewoutsideofsecondaryconstructor/UseOnlyNewAtSecondaryConstructor.java
@@ -3,6 +3,25 @@ package elegantobject.employment.dontusenewoutsideofsecondaryconstructor;
  */
 class UseOnlyNewAtSecondaryConstructor {
 
+    private static class NYSE implements Exchange {
+        @Override
+        public float rate(String origin, String target) {
+            if ("EUR".equals(origin)) {
+                switch (target) {
+                    case "USD":
+                        return 1.19F;
+                }
+            }
+            if ("USD".equals(origin)) {
+                switch (target) {
+                    case "EUR":
+                        return 0.84F;
+                }
+            }
+            throw new IllegalArgumentException();
+        }
+    }
+
     private interface Exchange {
         float rate(String origin, String target);
     }
