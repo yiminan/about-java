2b899452959bf6d22cd5c336ca717fd515ed9fe5
orgojy <giveawesome@gmail.com>
2021-04-12 22:18:23 +0900

    Add example class "InjectionCash" for "UseOnlyNewAtSecondaryConstructor"

--- src/elegantobject/employment/dontusenewoutsideofsecondaryconstructor/UseOnlyNewAtSecondaryConstructor.java
+++ src/elegantobject/employment/dontusenewoutsideofsecondaryconstructor/UseOnlyNewAtSecondaryConstructor.java
@@ -4,6 +4,42 @@ package elegantobject.employment.dontusenewoutsideofsecondaryconstructor;
 class UseOnlyNewAtSecondaryConstructor {
 
     /**
+     * 부 생성자를 통해서 내부에 exchange 프로퍼티 객체를 주입하여 사용하면,
+     * 하드코딩된 내부 의존성을 개선할 수 있다.
+     * 개선된 점은 의존성을 제어하는 주체가 InjectionCash 객체가 아닌 프로그래머가 됩니다.
+     */
+    private static class InjectionCash {
+        private final int dollars;
+        private final Exchange exchange;
+
+        /**
+         * 부 생성자
+         */
+        public InjectionCash() {
+            this(0);
+        }
+
+        /**
+         * 부 생성자
+         */
+        public InjectionCash(int dollars) {
+            this(dollars, new NYSE());
+        }
+
+        /**
+         * 주 생성자
+         */
+        public InjectionCash(int dollars, Exchange exchange) {
+            this.dollars = dollars;
+            this.exchange = exchange;
+        }
+
+        public float euro() {
+            return exchange.rate("USD", "EUR") * dollars;
+        }
+    }
+
+    /**
      * 내부에 new로 객체를 하드코딩하여 의존성이 문제가 되는 Cash
      * 의존성 제어의 주체가 HardCodingCash 객체가 됩니다.
      */
