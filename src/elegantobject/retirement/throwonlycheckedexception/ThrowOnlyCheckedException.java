fecfeb694f3576517799d308c90e11eb78b96e3e
orgojy <giveawesome@gmail.com>
2021-04-21 22:36:35 +0900

    Add method to calculate length for File with 'fail safe'

--- src/elegantobject/retirement/throwonlycheckedexception/ThrowOnlyCheckedException.java
+++ src/elegantobject/retirement/throwonlycheckedexception/ThrowOnlyCheckedException.java
@@ -9,6 +9,12 @@ import java.io.IOException;
  * 대부분의 객체지향 언어들은 언체크 예외만 제공하지만,
  * Java는 두 종류(checked or unchecked exception)의 예외를 모두 제공합니다.
  * Ruby, C#, Python, Scala를 포함한 대부분의 언어들은 언체크 예외만 사용할 수 있습니다.
+ * <p>
+ * 메서드를 설계할 때,
+ * 1. 모든 예외를 잡아서 안전한 메서드를 만들지 (fail safe)
+ * 2. 상위로 문제를 전파할지 (fail fast가 가능)
+ * 를 명확하게 선택해야 합니다.
+ * 가능하면 상위 레벨로 예외를 전파하는 것이 좋습니다.
  */
 class ThrowOnlyCheckedException {
 
@@ -35,5 +41,19 @@ class ThrowOnlyCheckedException {
             );
         }
         return content(file).length;
+    }
+
+    /**
+     * 예외를 try ~ catch로 잡아서 실패를 안전하게 처리합니다.
+     * 하지만, 분명 실패한 부분을 인식하고 수정이 필요한데 소프트웨어는 이상없이 잘돌아갑니다.
+     * 결국 신뢰할 수 없는 메서드가 됩니다.
+     * 예외를 되도록 잡아서 안전하게 실패하기를 기반으로 처리하면 안됩니다.
+     */
+    public int length2(File file) {
+        try {
+            return content(file).length;
+        } catch (IOException ex) {
+            return 0;
+        }
     }
 }
