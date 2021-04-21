f5a585fe609a143713b9775e3766bd737bdf5ac9
orgojy <giveawesome@gmail.com>
2021-04-21 21:48:50 +0900

    Update comment for class "ThrowOnlyCheckedException"

--- src/elegantobject/retirement/throwonlycheckedexception/ThrowOnlyCheckedException.java
+++ src/elegantobject/retirement/throwonlycheckedexception/ThrowOnlyCheckedException.java
@@ -1,4 +1,10 @@
 package elegantobject.retirement.throwonlycheckedexception;
 
+/**
+ * "체크된 예외(checked exception)만 던져야 합니다"
+ * 대부분의 객체지향 언어들은 언체크 예외만 제공하지만,
+ * Java는 두 종류(checked or unchecked exception)의 예외를 모두 제공합니다.
+ * Ruby, C#, Python, Scala를 포함한 대부분의 언어들은 언체크 예외만 사용할 수 있습니다.
+ */
 class ThrowOnlyCheckedException {
 }
