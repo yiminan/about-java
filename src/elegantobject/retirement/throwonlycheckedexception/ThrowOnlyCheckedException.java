e2fb8f3ab9e1834398ab7bc873a003184e351aef
orgojy <giveawesome@gmail.com>
2021-04-21 21:51:03 +0900

    Add method to build content with checked exception

--- src/elegantobject/retirement/throwonlycheckedexception/ThrowOnlyCheckedException.java
+++ src/elegantobject/retirement/throwonlycheckedexception/ThrowOnlyCheckedException.java
@@ -1,5 +1,9 @@
 package elegantobject.retirement.throwonlycheckedexception;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 /**
  * "체크된 예외(checked exception)만 던져야 합니다"
  * 대부분의 객체지향 언어들은 언체크 예외만 제공하지만,
@@ -7,4 +11,10 @@ package elegantobject.retirement.throwonlycheckedexception;
  * Ruby, C#, Python, Scala를 포함한 대부분의 언어들은 언체크 예외만 사용할 수 있습니다.
  */
 class ThrowOnlyCheckedException {
+
+    public byte[] content(File file) throws IOException {
+        byte[] array = new byte[1000];
+        new FileInputStream(file).read(array);
+        return array;
+    }
 }
