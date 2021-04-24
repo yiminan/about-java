86fa7cf17fcb0f9d76919b5f6855efd659a58a4f
orgojy <giveawesome@gmail.com>
2021-04-25 00:45:37 +0900

    Add example class "Document"

--- src/elegantobject/retirement/finalorabstract/FinalOrAbstract.java
+++ src/elegantobject/retirement/finalorabstract/FinalOrAbstract.java
@@ -1,4 +1,28 @@
 package elegantobject.retirement.finalorabstract;
 
 class FinalOrAbstract {
+
+    private static class Document {
+        private final File file;
+
+        public Document(File file) {
+            this.file = file;
+        }
+
+        public int length() {
+            try {
+                return this.content().length;
+            } catch (Exception ex) {
+                return 0;
+            }
+        }
+
+        public byte[] content() throws Exception {
+            try {
+                return new FileInputStream(file).readAllBytes();
+            } catch (IOException ex) {
+                throw new Exception(ex);
+            }
+        }
+    }
 }
