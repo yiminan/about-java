4ff5d5b5c37839baabf4de3168c2c4251bfad91c
orgojy <giveawesome@gmail.com>
2021-04-24 13:26:29 +0900

    Add example method to recovery case for age

--- src/elegantobject/retirement/throwonlycheckedexception/RecoveryJustOneTimeAfterException.java
+++ src/elegantobject/retirement/throwonlycheckedexception/RecoveryJustOneTimeAfterException.java
@@ -2,6 +2,19 @@ package elegantobject.retirement.throwonlycheckedexception;
 
 class RecoveryJustOneTimeAfterException {
 
+    /**
+     * 예외 후, 복구 예시<p>
+     */
+    public int age(String text) {
+        int age;
+        try {
+            age = parsedInteger(text);
+        } catch (Exception ex) {
+            age = -1;// 예외 후, 최상위 수준 복구 지점에서 복구 예시
+        }
+        return age;
+    }
+
     private int parsedInteger(String text) throws Exception {
         try {
             return Integer.parseInt(text);
