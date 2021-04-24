b98876f89a5dd17821bd44a21031cf514071f15a
orgojy <giveawesome@gmail.com>
2021-04-24 13:25:20 +0900

    Add example method to build parsedInteger

--- src/elegantobject/retirement/throwonlycheckedexception/RecoveryJustOneTimeAfterException.java
+++ src/elegantobject/retirement/throwonlycheckedexception/RecoveryJustOneTimeAfterException.java
@@ -1,4 +1,12 @@
 package elegantobject.retirement.throwonlycheckedexception;
 
 class RecoveryJustOneTimeAfterException {
+
+    private int parsedInteger(String text) throws Exception {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException ex) {
+            throw new Exception(ex);
+        }
+    }
 }
