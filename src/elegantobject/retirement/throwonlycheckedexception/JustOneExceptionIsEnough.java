c8aa3815d7df55a3217b7a7bbc82ffa37cf204be
orgojy <giveawesome@gmail.com>
2021-04-24 17:30:53 +0900

    Update method to show example by exception

--- src/elegantobject/retirement/throwonlycheckedexception/JustOneExceptionIsEnough.java
+++ src/elegantobject/retirement/throwonlycheckedexception/JustOneExceptionIsEnough.java
@@ -1,4 +1,33 @@
 package elegantobject.retirement.throwonlycheckedexception;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 class JustOneExceptionIsEnough {
+
+    public void example() {
+        try {
+            fileInputStream("a.jpg");
+            age("12");
+        } catch (Exception ex) {
+            System.out.println(String.format("example message: %s", ex.getMessage()));
+        }
+    }
+
+    private FileInputStream fileInputStream(String fileName) throws Exception {
+        try {
+            return new FileInputStream(fileName);
+        } catch (IOException ex) {
+            throw new Exception(String.format("This file(%s) can't open to stream.", fileName));
+        }
+    }
+
+    private int age(String age) throws Exception {
+        try {
+            return Integer.parseInt(age);
+        } catch (NumberFormatException ex) {
+            throw new Exception(String.format("Invalid age %s", age));
+        }
+    }
+
 }
