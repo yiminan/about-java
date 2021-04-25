6f95956bc05df06c2afa5fc598f1b408901e1552
orgojy <giveawesome@gmail.com>
2021-04-25 15:31:34 +0900

    Add example abstract class "Document"

--- src/elegantobject/retirement/finalorabstract/GoodFinalClassOrMethodWithAbstractClass.java
+++ src/elegantobject/retirement/finalorabstract/GoodFinalClassOrMethodWithAbstractClass.java
@@ -6,4 +6,18 @@ import java.io.IOException;
 
 class GoodFinalClassOrMethodWithAbstractClass {
 
+    /**
+     * abstract class 예시
+     */
+    private static abstract class Document {
+        public final int length() {
+            try {
+                return this.content().length;
+            } catch (Exception ex) {
+                return 0;
+            }
+        }
+
+        public abstract byte[] content() throws Exception;
+    }
 }
