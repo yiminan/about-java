c8966d4d1fc4bce5e3500608fc510631e2abf60a
orgojy <giveawesome@gmail.com>
2021-04-25 16:42:32 +0900

    Add example class "Text" implemented by AutoClosable

--- src/elegantobject/retirement/useraii/UseRAII.java
+++ src/elegantobject/retirement/useraii/UseRAII.java
@@ -1,4 +1,24 @@
 package elegantobject.retirement.useraii;
 
 class UseRAII {
+
+    /**
+     * AutoCloseable 인터페이스를 이용한 향상시킨 예외 처리
+     */
+    private static class Text implements AutoCloseable {
+        private final File file;
+
+        public Text(File file) {
+            this.file = file;
+        }
+
+        @Override
+        public void close() {
+            this.file.deleteOnExit();
+        }
+
+        public byte[] content() throws IOException {
+            return new FileInputStream(file).readAllBytes();
+        }
+    }
 }
