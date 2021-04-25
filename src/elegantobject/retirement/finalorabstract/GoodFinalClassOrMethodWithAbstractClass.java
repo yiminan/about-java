4a9685ac6210141b2918e9ae4a5d00a4a85c8fd9
orgojy <giveawesome@gmail.com>
2021-04-25 15:32:03 +0900

    Add extended classes "DefaultDocument", "EncryptedDocument"

--- src/elegantobject/retirement/finalorabstract/GoodFinalClassOrMethodWithAbstractClass.java
+++ src/elegantobject/retirement/finalorabstract/GoodFinalClassOrMethodWithAbstractClass.java
@@ -20,4 +20,41 @@ class GoodFinalClassOrMethodWithAbstractClass {
 
         public abstract byte[] content() throws Exception;
     }
+
+    /**
+     * Document를 상속해서 사용하므로써 오버라이딩이 가능한 content()와 상속한 length()를 사용 가능
+     */
+    private static final class DefaultDocument extends Document {
+        private final File file;
+
+        public DefaultDocument(File file) {
+            this.file = file;
+        }
+
+        @Override
+        public byte[] content() throws Exception {
+            try {
+                return new FileInputStream(file).readAllBytes();
+            } catch (IOException ex) {
+                throw new Exception(ex);
+            }
+        }
+    }
+
+    /**
+     * Document를 상속해서 사용하므로써 오버라이딩이 가능한 content()와 상속한 length()를 사용 가능
+     */
+    private static final class EncryptedDocument extends Document {
+        private final Document plain;
+
+        public EncryptedDocument(Document plain) {
+            this.plain = plain;
+        }
+
+        @Override
+        public byte[] content() throws Exception {
+            byte[] raw = this.plain.content();
+            return raw;// Content.decrypt(raw); 이 지점에서 복호화된 content를 반환한다.
+        }
+    }
 }
