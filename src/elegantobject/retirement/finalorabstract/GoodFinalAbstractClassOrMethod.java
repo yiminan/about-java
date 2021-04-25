0a4e1176fb8fb0192716477b1235d4cfa423f897
orgojy <giveawesome@gmail.com>
2021-04-25 11:43:19 +0900

    Add example final class "DocumentWithFinal"

--- src/elegantobject/retirement/finalorabstract/GoodFinalAbstractClassOrMethod.java
+++ src/elegantobject/retirement/finalorabstract/GoodFinalAbstractClassOrMethod.java
@@ -1,4 +1,32 @@
 package elegantobject.retirement.finalorabstract;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 class GoodFinalAbstractClassOrMethod {
+
+    private static final class DocumentWithFinal {
+        private final File file;
+
+        public DocumentWithFinal(File file) {
+            this.file = file;
+        }
+
+        public int length() {
+            try {
+                return this.content().length;
+            } catch (Exception ex) {
+                return 0;
+            }
+        }
+
+        public byte[] content() throws Exception {
+            try {
+                return new FileInputStream(file).readAllBytes();
+            } catch (IOException ex) {
+                throw new Exception(ex);
+            }
+        }
+    }
 }
