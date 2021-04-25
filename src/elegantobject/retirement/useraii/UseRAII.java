7b2aeff8e12186a553b05a5ee2eb0bfe7212e8a2
orgojy <giveawesome@gmail.com>
2021-04-25 16:42:42 +0900

    Add example class "TextFile" implemented by AutoClosable

--- src/elegantobject/retirement/useraii/UseRAII.java
+++ src/elegantobject/retirement/useraii/UseRAII.java
@@ -21,4 +21,38 @@ class UseRAII {
             return new FileInputStream(file).readAllBytes();
         }
     }
+
+    /**
+     * example1과 example2는 같은 로직입니다.<p>
+     * 다만 Java7부터 지원하는 Closable을 구현한 코드가 example1입니다.
+     */
+    private static class TextFile {
+        private final File file;
+
+        public TextFile(File file) {
+            this.file = file;
+        }
+
+        public byte[] example1() throws Exception {
+            try (Text text = new Text(file)) {
+                return text.content();
+            } catch (IOException ex) {
+                throw new Exception(ex);
+            }
+        }
+
+        public byte[] example2() throws Exception {
+            Text text = null;
+            try {
+                text = new Text(file);
+                return text.content();
+            } catch (IOException ex) {
+                throw new Exception(ex);
+            } finally {
+                if (text != null) {
+                    text.close();
+                }
+            }
+        }
+    }
 }
