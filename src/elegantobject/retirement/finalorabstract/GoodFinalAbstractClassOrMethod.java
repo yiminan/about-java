f8209270388975c1281f06860d31a6e771d0b105
orgojy <giveawesome@gmail.com>
2021-04-25 11:44:32 +0900

    Add example class "DefaultDocument" implemented by "DocumentWithInterface"

--- src/elegantobject/retirement/finalorabstract/GoodFinalAbstractClassOrMethod.java
+++ src/elegantobject/retirement/finalorabstract/GoodFinalAbstractClassOrMethod.java
@@ -35,4 +35,30 @@ class GoodFinalAbstractClassOrMethod {
 
         byte[] content() throws Exception;
     }
+
+    private static final class DefaultDocument implements DocumentWithInterface {
+        private final File file;
+
+        public DefaultDocument(File file) {
+            this.file = file;
+        }
+
+        @Override
+        public int length() {
+            try {
+                return this.content().length;
+            } catch (Exception ex) {
+                return 0;
+            }
+        }
+
+        @Override
+        public byte[] content() throws Exception {
+            try {
+                return new FileInputStream(file).readAllBytes();
+            } catch (IOException ex) {
+                throw new Exception(ex);
+            }
+        }
+    }
 }
