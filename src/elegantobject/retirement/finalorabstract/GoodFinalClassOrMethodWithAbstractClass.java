3ef5dd1943992665f4953669be8de950f51ee47a
orgojy <giveawesome@gmail.com>
2021-04-25 15:32:30 +0900

    Update comment for class "GoodFinalClassOrMethodWithAbstractClass"

--- src/elegantobject/retirement/finalorabstract/GoodFinalClassOrMethodWithAbstractClass.java
+++ src/elegantobject/retirement/finalorabstract/GoodFinalClassOrMethodWithAbstractClass.java
@@ -4,6 +4,17 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/**
+ * "상속의 문제를 없애면서 상속을 활용할 수 있는 abstract class"<p>
+ * <p>
+ * 1) '상속해서 사용하지만 수정할 수 없는 메서드' // final class의 상속 불가능 보완<p>
+ * 2) '오버라이드로 내용을 정의해야하는 메서드' // class 상속의 장점<p>
+ * 두 가지를 다 사용할 수 있다.<p>
+ * 결론)<p>
+ * final class의 단점인 상속 불가능을 필요한 경우 상속 가능으로 바꾸어주고,<p>
+ * 상속하면 발생하는 부모 클래스의 코드를 자식 클래스에서 오버라이딩으로 수정하므로써 생기는 문제점을,<p>
+ * abstract 메서드 선언으로 수정 가능한 메서드를 지정하므로써 문제가 발생하지 않게 합니다.
+ */
 class GoodFinalClassOrMethodWithAbstractClass {
 
     /**
