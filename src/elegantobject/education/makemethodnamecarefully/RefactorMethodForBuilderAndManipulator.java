1ec6514e6205dd2ed109c86afa89d75c3e365172
orgojy <giveawesome@gmail.com>
2021-05-06 21:43:01 +0900

    Update comment for class "RefactorMethodForBuilderAndManipulator"

--- src/elegantobject/education/makemethodnamecarefully/RefactorMethodForBuilderAndManipulator.java
+++ src/elegantobject/education/makemethodnamecarefully/RefactorMethodForBuilderAndManipulator.java
@@ -4,17 +4,18 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /***
- * "빌더와 조정자를 혼합해서 사용하는 방법"
- * 잘못 사용된 빌더 + 조정자 메서드를 객체를 생성해서 본연의 역할을 하는 빌더, 조정자로 분리합니다.
- * OOP의 전체 목적은 개념을 고립시켜 복잡성을 낮추는 것입니다.
- * 고립시킨 개념이 작을수록 이해하고 유지보수하기도 더 쉬워집니다.
+ * "빌더와 조정자를 혼합해서 사용하는 방법"<p>
+ * <p>
+ * 잘못 사용된 빌더 + 조정자 메서드를 객체를 생성해서 본연의 역할을 하는 빌더, 조정자로 분리합니다.<p>
+ * OOP의 전체 목적은 개념을 고립시켜 복잡성을 낮추는 것입니다.<p>
+ * 고립시킨 개념이 작을수록 이해하고 유지보수하기도 더 쉬워집니다.<p>
  */
 class RefactorMethodForBuilderAndManipulator {
 
     /**
-     * 잘못 사용된 빌더 + 조정자 메서드
-     * 메서드 하나에서 많은 작업을 하게되니 반환 값이 있는 조정자가 됩니다.
-     * 조정자를 잘못된 사용법으로 사용하고 있습니다.
+     * 잘못 사용된 빌더 + 조정자 메서드<p>
+     * 메서드 하나에서 많은 작업을 하게되니 반환 값이 있는 조정자가 됩니다.<p>
+     * 조정자를 잘못된 사용법으로 사용하고 있습니다.<p>
      */
     private static class WrongDocument {
         private InputStream content;
@@ -26,8 +27,8 @@ class RefactorMethodForBuilderAndManipulator {
     }
 
     /**
-     * OutputPipe를 만들어서 빌더 메서드를 사용할 수 있습니다.
-     * 빌더와 조정자를 분리하는데에 OutputPipe이 객체가 사용됩니다.
+     * OutputPipe를 만들어서 빌더 메서드를 사용할 수 있습니다.<p>
+     * 빌더와 조정자를 분리하는데에 OutputPipe이 객체가 사용됩니다.<p>
      */
     private static class ProperDocument {
         private final OutputPipe outputPipe;
@@ -42,7 +43,7 @@ class RefactorMethodForBuilderAndManipulator {
     }
 
     /**
-     * 객체로 개념을 작게 고립시켜서 유지보수성을 높게하고, 복잡성을 낮춥니다.
+     * 객체로 개념을 작게 고립시켜서 유지보수성을 높게하고, 복잡성을 낮춥니다.<p>
      */
     private static class OutputPipe {
         private InputStream content;
@@ -52,14 +53,14 @@ class RefactorMethodForBuilderAndManipulator {
         }
 
         /**
-         * 본연의 목적을 가진 조정자 메서드 분리 완료
+         * 본연의 목적을 가진 조정자 메서드 분리 완료<p>
          */
         void write(InputStream content) {
             this.content = content;
         }
 
         /**
-         * 본연의 목적을 가진 빌더 메서드 분리 완료
+         * 본연의 목적을 가진 빌더 메서드 분리 완료<p>
          */
         int bytes() throws IOException {
             return content.read();
