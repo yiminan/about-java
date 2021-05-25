b437f7ffad4bb292210bc75f84313bef833ff9bc
orgojy <giveawesome@gmail.com>
2021-05-25 09:16:52 +0900

    Update comment for class "ConsiderStaticFactoryMethodInsteadOfConstructor"

--- src/effectivejava/item1/ConsiderStaticFactoryMethodInsteadOfConstructor.java
+++ src/effectivejava/item1/ConsiderStaticFactoryMethodInsteadOfConstructor.java
@@ -2,7 +2,18 @@ package effectivejava.item1;
 
 /**
  * "생성자 대신 정적 팩터리 메서드를 고려하라"<p>
- *
+ * <p>
+ * 클래스는 클라이언트에 public 생성자를 대신해서 정적 팩토리 메서드를 제공할 수 있습니다.<p>
+ * 정적 팩토리 메서드는 장점과 단점이 둘 다 존재합니다.<p>
+ * 장점)<p>
+ * 1. 이름을 가질 수 있습니다.<p>
+ * - 기존의 생성자는 new라는 키워드 밖에 없지만, 원하는 대로 팩토리 메서드의 이름을 작명할 수 있습니다.<p>
+ * 2. 호출될 때마다 인스턴스를 새로 생성하지는 않아도 됩니다.<p>
+ * - 정적 팩토리 메서드를 호출할 때마다 내부에서 생성자로 객체를 생성해도되고, 객체를 재사용해도 됩니다.<p>
+ * - 플라이웨이트(Flyweight Pattern)과 비슷한 패턴입니다.<p>
+ * - ex) Boolean.valueOf(boolean)<p>
+ * - 인스턴스를 살아있게 할지 통제가 가능합니다. 1) singleton object 2) non-instantiable object 3) immutable object를 선택 가능<p>
+ * 3. 반환 타입의 하위 타입 객체를 반환할 수 있는 능력이 있습니다.<p>
  */
 class ConsiderStaticFactoryMethodInsteadOfConstructor {
 }
