542048aa0e02b33abe727d9cfe8630759baac532
orgojy <giveawesome@gmail.com>
2021-05-26 11:16:39 +0900

    Update comment for class "ConsiderStaticFactoryMethodInsteadOfConstructor"

--- src/effectivejava/item1/ConsiderStaticFactoryMethodInsteadOfConstructor.java
+++ src/effectivejava/item1/ConsiderStaticFactoryMethodInsteadOfConstructor.java
@@ -19,7 +19,9 @@ package effectivejava.item1;
  * - Collections Class는 정적 팩토리 메서드를 통해서 원하는 인스턴스를 얻게 되어있습니다.<p>
  * - 하나의 클래스에서 하위 여러 객체를 반환 받을 수 있기 때문에 수 많은 클래스를 생성하지 않아도 됩니다.<p>
  * 4. 입력 매개변수에 따라 매번 다른 클래스의 객체를 반환할 수 있습니다.<p>
- * -
+ * - 반환 타입의 하위 타입이면 어떤 클래스의 객체를 반환해도 상관없습니다.<p>
+ * - ex) {@link java.util.EnumSet EnumSet}은 원소가 64개 이하이면 {@link java.util.RegularEnumSet RegularEnumSet}, 65개 이상이면 {@link java.util.JumboEnumSet JumboEnumSet}을 반환합니다.<p>
+ * - 클라이언트는 정적 팩토리 메서드를 사용만하면 되지, 무엇이 반환되는지는 신경 안써도 된다.<p>
  */
 class ConsiderStaticFactoryMethodInsteadOfConstructor {
 }
