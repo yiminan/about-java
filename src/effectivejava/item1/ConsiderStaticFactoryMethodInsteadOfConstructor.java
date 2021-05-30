13f62a463568b666a2f2d008e7f07edcc0692c3c
orgojy <giveawesome@gmail.com>
2021-05-30 17:18:41 +0900

    Update comment for class "ConsiderStaticFactoryMethodInsteadOfConstructor"

--- src/effectivejava/item1/ConsiderStaticFactoryMethodInsteadOfConstructor.java
+++ src/effectivejava/item1/ConsiderStaticFactoryMethodInsteadOfConstructor.java
@@ -66,7 +66,7 @@ class ConsiderStaticFactoryMethodInsteadOfConstructor {
          * - from이 호출되는 클래스 타입을 반환하는 정적 팩토리 메서드
          * - 2개이상 파라미터를 전달받는 경우
          */
-        EnumSet.of(Type.VIP, Type.GENERAL);
+        EnumSet types = EnumSet.of(Type.VIP, Type.GENERAL);
         /**
          * "valueOf"
          * -
