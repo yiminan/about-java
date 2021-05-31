fbd9932da2d7dbaf0f4b8256b8e68e5d1af9d2b5
orgojy <giveawesome@gmail.com>
2021-05-31 13:04:19 +0900

    Add class "User" to show example for ObjectMapper of Jackson

--- src/jackson/objectmapper/ObjectMapperExample.java
+++ src/jackson/objectmapper/ObjectMapperExample.java
@@ -1,4 +1,22 @@
 package jackson.objectmapper;
 
 class ObjectMapperExample {
+
+    private static class User {
+        private String name;
+        private int age;
+
+        public User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
 }
