73f93e6058cc9f6896d099beda41a45126ad394c
orgojy <giveawesome@gmail.com>
2021-06-10 10:55:12 +0900

    Update comment for class "ConsiderABuilderWhenIHaveManyParameters"

--- src/effectivejava/item2/ConsiderABuilderWhenIHaveManyParameters.java
+++ src/effectivejava/item2/ConsiderABuilderWhenIHaveManyParameters.java
@@ -3,9 +3,13 @@ package effectivejava.item2;
 /**
  * "생성자에 매개변수가 많다면 빌더를 고려하라"
  * <p>
- * 일반적으로 매개변수가 많아지면 "점층적 생성자 패턴(telescoping constructor pattern)"을 사용한다.<p>
- * 하지만 점층적 생성자 패턴을 쓰다보면 매개변수 개수가 많아지면 클라이언트 코드를 작성하거나 읽기 어려워진다.<p>
- * 생성자에 파라미터의 갯수가 많아지면 많아질수록 매개변수 입력에 대한 실수가 많아집니다.<p>
+ * 객체 생성 방식<p>
+ * 1.점층적 생성자 패턴(telescoping constructor pattern)<p>
+ * - 일반적으로 매개변수가 많아지면 "점층적 생성자 패턴(telescoping constructor pattern)"을 사용한다.<p>
+ * - 하지만 점층적 생성자 패턴을 쓰다보면 매개변수 개수가 많아지면 클라이언트 코드를 작성하거나 읽기 어려워진다.<p>
+ * - 생성자에 파라미터의 갯수가 많아지면 많아질수록 매개변수 입력에 대한 실수가 많아집니다.<p>
+ * 2.자바빈즈 패턴(javaBeans pattern)<p>
+ * -
  */
 class ConsiderABuilderWhenIHaveManyParameters {
 
