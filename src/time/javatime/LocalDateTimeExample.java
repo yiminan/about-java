c3ae9d307bca1814c7786742eb461eda745f16b0
orgojy <giveawesome@gmail.com>
2021-06-12 16:42:20 +0900

    Update inner main method for LocalDateTimeExample

--- src/time/javatime/LocalDateTimeExample.java
+++ src/time/javatime/LocalDateTimeExample.java
@@ -5,6 +5,12 @@ import java.time.ZoneId;
 import java.time.ZoneOffset;
 
 class LocalDateTimeExample {
+    public static void main(String[] args) {
+        Instant utcPlus9 = new LocalDateTime().instant(ZoneOffset.of("+09:00"));
+        Instant seoulTime = new LocalDateTime().instant(ZoneId.of("Asia/Seoul"));
+        System.out.println(utcPlus9.equals(seoulTime));
+    }
+
     private static class LocalDateTime {
         private final java.time.LocalDateTime now = java.time.LocalDateTime.now();
 
