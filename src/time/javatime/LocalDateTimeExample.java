d8fd756175f7b9bfbd12aa5f1c008b99a11fd7ec
orgojy <giveawesome@gmail.com>
2021-06-12 16:41:55 +0900

    Update inner example class for LocalDateTime

--- src/time/javatime/LocalDateTimeExample.java
+++ src/time/javatime/LocalDateTimeExample.java
@@ -1,4 +1,19 @@
 package time.javatime;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
 class LocalDateTimeExample {
+    private static class LocalDateTime {
+        private final java.time.LocalDateTime now = java.time.LocalDateTime.now();
+
+        public Instant instant(ZoneOffset zoneOffset) {
+            return now.toInstant(zoneOffset);
+        }
+
+        public Instant instant(ZoneId zoneId) {
+            return now.atZone(zoneId).toInstant();
+        }
+    }
 }
