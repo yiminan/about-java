94db0d0d8b6e1d0ed209b94467daa5a29496b62f
orgojy <giveawesome@gmail.com>
2021-06-12 16:51:24 +0900

    Update comment for class "LocalDateTimeExample"

--- src/time/javatime/LocalDateTimeExample.java
+++ src/time/javatime/LocalDateTimeExample.java
@@ -6,9 +6,18 @@ import java.time.ZoneOffset;
 
 class LocalDateTimeExample {
     public static void main(String[] args) {
+        /**
+         * UTC를 기준으로 시간을 표현해줍니다.
+         */
         Instant utcPlus9 = new LocalDateTime().instant(ZoneOffset.of("+09:00"));
+        /**
+         * Summer Time을 고려해서 해당 나라의 시간을 표시해주는 장점이 있습니다.
+         * Summer Time과 관계 없는 지역의 시간은 ZoneOffset과 같은 결과를 내어줍니다.
+         */
         Instant seoulTime = new LocalDateTime().instant(ZoneId.of("Asia/Seoul"));
-        System.out.println(utcPlus9.equals(seoulTime));
+
+        System.out.println(utcPlus9);
+        System.out.println(seoulTime);
     }
 
     private static class LocalDateTime {
