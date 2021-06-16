b292a46a56f3dce906c21759bf4323f9ffca1786
orgojy <giveawesome@gmail.com>
2021-06-16 20:28:05 +0900

    Add example abstract class "Pizza"

--- src/effectivejava/item2/Pizza.java
+++ src/effectivejava/item2/Pizza.java
@@ -1,4 +1,26 @@
 package effectivejava.item2;
 
-class Pizza {
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
+abstract class Pizza {
+    public enum Topping {HAM, MUSHROOM, ONION, PEPPER, SAUSAGE}
+    final Set<Topping> toppings;
+
+    abstract static class Builder<T extends Builder<T>> {
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+        public T addTopping(Topping topping) {
+            toppings.add(Objects.requireNonNull(topping));
+            return self();
+        }
+
+        abstract Pizza build();
+
+        protected abstract T self();
+    }
+
+    Pizza(Builder<?> builder) {
+        toppings = builder.toppings.clone();
+    }
 }
