a8f6b962cc6230f7e5144c3278c5212d8554a914
orgojy <giveawesome@gmail.com>
2021-06-25 12:08:48 +0900

    Update comment for class "GuaranteeSingletonWithPrivateConstructorOrEnum"

--- src/effectivejava/item3/GuaranteeSingletonWithPrivateConstructorOrEnum.java
+++ src/effectivejava/item3/GuaranteeSingletonWithPrivateConstructorOrEnum.java
@@ -8,15 +8,19 @@ package effectivejava.item3;
  * 설계상 유일해야하는 시스템 컴포넌트 : 스프링 bean과 같은 시스템에 유일해야하는 컴포넌트<p>
  * 클래스를 싱글턴으로 만들면 이를 사용하는 클라이언트를 테스트하기 힘들어집니다.<p>
  * 인터페이스로 구현된 싱글턴이 아닌 경우에는 싱글턴 인스턴스를 테스트에서 mock으로 구현하기 힘듭니다.<p>
+ * <p>
  * 싱글턴을 만드는 보통 방식)<p>
  * 공통) 클래스 생성자를 private으로 감추어두고, 내부 field로 publid static 멤버를 하나 마련하여 사용<p>
- * 1) 'public static final' field를 만들어서 사용하는 방식<p>
- * - 명확한 public static final field를 참조하기 때문에 싱글턴임이 명확하다.<p>
- * - 싱글턴 클래스를 간결하게 만들 수 있다.<p>
- * 2) 'static factory' method를 만들어서 사용하는 방식<p>
- * - 메서드로 싱글턴 인스턴스를 반환하기 때문에 API 코드 변경 없이 싱글턴이 아니게 변경이 가능합니다. 예를 들면, 쓰레드별 인스턴스를 제공이 가능합니다.<p>
- * - 정적 팩터리를 제네릭 싱글턴 팩토리로 만들 수 있습니다.<p>
- * - 정적 팩토리의 메서드 참조를 공급자(Supplier)로 사용할 수 있다는 점입니다. ex) User::getInstance -> Supplier&lt;User&gt;<p>
+ * <p>
+ * 1. 'public static final' field를 만들어서 사용하는 방식<p>
+ * 1.1. 명확한 public static final field를 참조하기 때문에 싱글턴임이 명확하다.<p>
+ * 1.2. 싱글턴 클래스를 간결하게 만들 수 있다.<p>
+ * <p>
+ * 2. 'static factory' method를 만들어서 사용하는 방식<p>
+ * 2.1. 메서드로 싱글턴 인스턴스를 반환하기 때문에 API 코드 변경 없이 싱글턴이 아니게 변경이 가능합니다. 예를 들면, 쓰레드별 인스턴스를 제공이 가능합니다.<p>
+ * 2.2. 정적 팩터리를 제네릭 싱글턴 팩토리로 만들 수 있습니다.<p>
+ * 2.3. 정적 팩토리의 메서드 참조를 공급자(Supplier)로 사용할 수 있다는 점입니다. ex) User::getInstance -> Supplier&lt;User&gt;<p>
+ * - 굳이 위에 나열한 장점이 필요없으면 'public static final' field를 사용하는 것이 좋다.<p>
  */
 class GuaranteeSingletonWithPrivateConstructorOrEnum {
 
