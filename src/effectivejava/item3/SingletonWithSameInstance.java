1cb5c7aa74c74f7021cdd77398d83e1b086e4b92
orgojy <giveawesome@gmail.com>
2021-06-26 17:46:54 +0900

    Add example class for "MySingleton"

--- src/effectivejava/item3/SingletonWithSameInstance.java
+++ src/effectivejava/item3/SingletonWithSameInstance.java
@@ -1,5 +1,26 @@
 package effectivejava.item3;
 
-class SingletonWithSameInstance {
+import java.io.Serializable;
 
+class SingletonWithSameInstance implements Serializable {
+
+    private static final class MySingleton {
+        private static final MySingleton INSTANCE = new MySingleton();
+
+        private MySingleton() {
+        }
+
+        public static MySingleton getINSTANCE() {
+            return INSTANCE;
+        }
+
+        /**
+         * readResolve 메서드를 정의하면서 싱글턴을 보장한다.
+         */
+        private Object readResolve() {
+            // INSTANCE를 반환으로 싱글턴을 보장한다.
+            return INSTANCE;
+        }
+
+    }
 }
