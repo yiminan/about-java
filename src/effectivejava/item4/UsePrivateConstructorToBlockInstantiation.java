040835d8ecda40a9a325639992211e67849f707b
orgojy <giveawesome@gmail.com>
2021-06-27 22:28:11 +0900

    Update main method to show generating exception for Util class

--- src/effectivejava/item4/UsePrivateConstructorToBlockInstantiation.java
+++ src/effectivejava/item4/UsePrivateConstructorToBlockInstantiation.java
@@ -2,6 +2,14 @@ package effectivejava.item4;
 
 class UsePrivateConstructorToBlockInstantiation {
 
+    public static void main(String[] args) {
+        try {
+            new ExampleUtility();
+        } catch (AssertionError ex) {
+            System.out.println("인스턴스화가 불가능한 클래스 예외");
+        }
+    }
+
     private static class ExampleUtility {
         private ExampleUtility() {
             throw new AssertionError();
