ef913568b4f5c5ece93ce9e4f8e6cacaaca36abe
orgojy <giveawesome@gmail.com>
2021-06-30 11:03:26 +0900

    Update example class for "SpellChecker"

--- src/effectivejava/item5/InjectDependencyObjectInsteadOfDirectNotation.java
+++ src/effectivejava/item5/InjectDependencyObjectInsteadOfDirectNotation.java
@@ -1,8 +1,19 @@
 package effectivejava.item5;
 
+import java.util.Objects;
+
 /**
  * "자원을 직접 명시하지 말고 의존 객체 주입을 사용해야합니다"<p>
  * <p>
  */
 class InjectDependencyObjectInsteadOfDirectNotation {
+
+    private static class SpellChecker {
+
+        private final Lexicon dictionary;
+
+        public SpellChecker(Lexicon dictionary) {
+            this.dictionary = Objects.requireNonNull(dictionary);
+        }
+    }
 }
