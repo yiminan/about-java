946bff1d05d514db03ffb50e120016b1f54879c7
orgojy <giveawesome@gmail.com>
2021-07-03 11:27:07 +0900

    Add example main method to show String examples

--- src/effectivejava/item6/AvoidUnnecessaryObjectCreation.java
+++ src/effectivejava/item6/AvoidUnnecessaryObjectCreation.java
@@ -5,4 +5,13 @@ package effectivejava.item6;
  * <p>
  */
 class AvoidUnnecessaryObjectCreation {
+
+    public static void main(String[] args) {
+        String s1 = new String("example");
+        String s2 = new String("example");
+        String s3 = "example";
+        String s4 = "example";
+        System.out.println(s1 == s2);// false
+        System.out.println(s3 == s4);// true
+    }
 }
