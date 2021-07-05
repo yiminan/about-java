696b0eec4e2d9124fc07aba764e0a941c57a9f7c
orgojy <giveawesome@gmail.com>
2021-07-05 11:27:45 +0900

    Minor moving method "exampleBoolean"

--- src/effectivejava/item6/AvoidUnnecessaryObjectCreation.java
+++ src/effectivejava/item6/AvoidUnnecessaryObjectCreation.java
@@ -13,15 +13,6 @@ class AvoidUnnecessaryObjectCreation {
         exampleBoolean();
     }
 
-    private static void exampleBoolean() {
-        Boolean b1 = Boolean.valueOf("true");
-        Boolean b2 = Boolean.valueOf("true");
-        Boolean b3 = new Boolean("false");
-        Boolean b4 = new Boolean("false");
-        System.out.println(b1 == b2);
-        System.out.println(b3 == b4);
-    }
-
     private static void exampleString() {
         String s1 = new String("example");
         String s2 = new String("example");
@@ -29,5 +20,14 @@ class AvoidUnnecessaryObjectCreation {
         String s4 = "example";
         System.out.println(s1 == s2);// false
         System.out.println(s3 == s4);// true
+    }
+
+    private static void exampleBoolean() {
+        Boolean b1 = Boolean.valueOf("true");
+        Boolean b2 = Boolean.valueOf("true");
+        Boolean b3 = new Boolean("false");
+        Boolean b4 = new Boolean("false");
+        System.out.println(b1 == b2);
+        System.out.println(b3 == b4);
     }
 }
