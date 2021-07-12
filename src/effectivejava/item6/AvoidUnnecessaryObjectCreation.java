d84b38bf931561c5ac3c104b43bf7df1a6b78969
orgojy <giveawesome@gmail.com>
2021-07-12 22:53:33 +0900

    Update access modifier for expensive object

--- src/effectivejava/item6/AvoidUnnecessaryObjectCreation.java
+++ src/effectivejava/item6/AvoidUnnecessaryObjectCreation.java
@@ -53,7 +53,7 @@ class AvoidUnnecessaryObjectCreation {
     }
 
     private static class RomanNumerals {
-        public static final Pattern ROMAN = Pattern.compile(
+        private static final Pattern ROMAN = Pattern.compile(
                 "^(?=.)M*(C[MD]|D?C{0,3})" + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$"
         );
 
