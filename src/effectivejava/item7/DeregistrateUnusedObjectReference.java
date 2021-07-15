9fce93e8f94eaaadb3e88b3c02b891245e7ea591
orgojy <giveawesome@gmail.com>
2021-07-15 17:32:32 +0900

    Add comment for class "DeregistrateUnusedObjectReference"

--- src/effectivejava/item7/DeregistrateUnusedObjectReference.java
+++ src/effectivejava/item7/DeregistrateUnusedObjectReference.java
@@ -2,6 +2,8 @@ package effectivejava.item7;
 
 /**
  * "다 쓴 객체 참조를 해제해야 합니다"<p>
+ * C, C++와 같은 언어를 사용하다가 Java와 같은 GC를 하는 언어를 사용하면 메모리 관리를 안해도 된다고 생각할 수 있습니다.<p>
+ * 하지만 Java도 메모리 관리를 신경 써야합니다.<p>
  *
  */
 class DeregistrateUnusedObjectReference {
