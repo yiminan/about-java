4e48f666a084f942eac42e24f183fd40d9e4e96c
orgojy <giveawesome@gmail.com>
2021-07-18 22:39:46 +0900

    Add comment for class "AvoidFinalizerAndCleaner"

--- src/effectivejava/item8/AvoidFinalizerAndCleaner.java
+++ src/effectivejava/item8/AvoidFinalizerAndCleaner.java
@@ -1,21 +1,35 @@
 package effectivejava.item8;
 
+import java.lang.ref.Cleaner;
+
 /**
  * "finalizer와 cleaner 사용을 피하라"<p>
  * <p>
+ * Java는 두 가지 객체 소멸자를 제공합니다.<p>
  */
 class AvoidFinalizerAndCleaner {
 
-    /**
-     * "Finalizer"
-     * finalize()는 객체가 JVM에게 GC 대상이 될 때 호출됩니다.
-     * 객체가 없어지기 전에 다른 연관 자원을 정리하려는 의도로 생긴 메서드입니다.
-     * 하지만 이 메서드는 java9+ 부터 Deprecated 되었습니다.
-     * 오류/시점/성능/수행성을 보장하지 못하며 수행되지 않거나 Lock으로 프로그램 전체가 Block될 수 있습니다.
-     * java9은 대안으로 Cleaner를 지원하게 되었습니다.
-     */
-    @Override
-    protected void finalize() {
-        // 구현 내용
+    public class FinalizerExample {
+
+        @Override
+        protected final void finalize() throws Throwable {
+            System.out.println("Clean up");
+        }
+
+        public void hello() {
+        }
+    }
+
+    public class SampleRunner {
+
+        public static void main(String[] args) {
+            SampleRunner runner = new SampleRunner();
+            runner.run();
+        }
+
+        priavte void run() {
+            FinalizerExample finalizerExample = new FinalizerExample();
+            finalizerExample.hello();
+        }
     }
 }
