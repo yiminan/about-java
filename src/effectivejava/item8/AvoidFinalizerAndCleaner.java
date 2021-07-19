e81ff93c88463cec0c81c2223806f428f9fb101c
orgojy <giveawesome@gmail.com>
2021-07-19 22:50:04 +0900

    Add example class "Room"

--- src/effectivejava/item8/AvoidFinalizerAndCleaner.java
+++ src/effectivejava/item8/AvoidFinalizerAndCleaner.java
@@ -37,4 +37,42 @@ class AvoidFinalizerAndCleaner {
             finalizerExample.hello();
         }
     }
+
+    private static class Room implements AutoCloseable {
+        // Room 인스턴스를 참조해서는 순환참조가 발생하므로 State는 static class 정의
+        private static final Cleaner cleaner = Cleaner.create();
+
+        private static class State implements Runnable {
+            // 방(Room) 안의 쓰레기 수
+            int numJunkPiles;
+
+            State(int numJunkPiles) {
+                this.numJunkPiles = numJunkPiles;
+            }
+
+            /**
+             * close 메서드나 cleaner가 호출한다.
+             */
+            @Override
+            public void run() {
+                System.out.println("Room Clean");
+                numJunkPiles = 0;
+            }
+        }
+
+        // 방의 상태, cleanable과 공유한다.
+        private final State state;
+        // cleanable 객체, 수거 대상이 되면 방을 청소한다.
+        private final Cleaner.Cleanable cleanable;
+
+        public Room(int numJunkPiles) {
+            state = new State(numJunkPiles);
+            cleanable = cleaner.register(this, state);
+        }
+
+        @Override
+        public void close() {
+            cleanable.clean();
+        }
+    }
 }
