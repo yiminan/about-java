48289c40e4c5c53d38714701c09b1ecf29401874
orgojy <giveawesome@gmail.com>
2021-08-02 12:34:09 +0900

    Update class logic for class "AvoidFinalizerAndCleaner"

--- src/effectivejava/item8/AvoidFinalizerAndCleaner.java
+++ src/effectivejava/item8/AvoidFinalizerAndCleaner.java
@@ -71,6 +71,21 @@ class AvoidFinalizerAndCleaner {
         // Room 인스턴스를 참조해서는 순환참조가 발생하므로 State는 static class 정의
         private static final Cleaner cleaner = Cleaner.create();
 
+        // 방의 상태, cleanable과 공유한다.
+        private final State state;
+        // cleanable 객체, 수거 대상이 되면 방을 청소한다.
+        private final Cleaner.Cleanable cleanable;
+
+        public Room(int numJunkPiles) {
+            state = new State(numJunkPiles);
+            cleanable = cleaner.register(this, state);
+        }
+
+        @Override
+        public void close() {
+            cleanable.clean();
+        }
+
         private static class State implements Runnable {
             // 방(Room) 안의 쓰레기 수
             int numJunkPiles;
@@ -87,21 +102,6 @@ class AvoidFinalizerAndCleaner {
                 System.out.println("Room Clean");
                 numJunkPiles = 0;
             }
-        }
-
-        // 방의 상태, cleanable과 공유한다.
-        private final State state;
-        // cleanable 객체, 수거 대상이 되면 방을 청소한다.
-        private final Cleaner.Cleanable cleanable;
-
-        public Room(int numJunkPiles) {
-            state = new State(numJunkPiles);
-            cleanable = cleaner.register(this, state);
-        }
-
-        @Override
-        public void close() {
-            cleanable.clean();
         }
     }
 }
