7361d16853fda505f7060a88c47391f554bbda26
orgojy <giveawesome@gmail.com>
2021-08-05 15:13:59 +0900

    Update comment for class "UseTryWithResourcesThanTryFinally"

--- src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
+++ src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
@@ -2,6 +2,8 @@ package effectivejava.item9;
 
 /**
  * "try-finally보다는 try-with-resources를 사용해야합니다."<p>
+ * 자바 라이브러리에는 close 메서드를 호출해 직접 닫아줘야 하는 자원이 많습니다.<p>
+ * {@link java.io.InputStream}, {@link java.io.OutputStream}, {@link java.sql.Connection} 등이 좋은 예시입니다.<p>
  */
 class UseTryWithResourcesThanTryFinally {
 
