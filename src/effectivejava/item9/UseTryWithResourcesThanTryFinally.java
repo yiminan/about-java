6c85831774f387bb2e3e10c648b0430d5d7b7fd7
orgojy <giveawesome@gmail.com>
2021-08-06 13:20:52 +0900

    Update example method for class "UseTryWithResourcesThanTryFinally"

--- src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
+++ src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
@@ -1,10 +1,24 @@
 package effectivejava.item9;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * "try-finally보다는 try-with-resources를 사용해야합니다."<p>
  * 자바 라이브러리에는 close 메서드를 호출해 직접 닫아줘야 하는 자원이 많습니다.<p>
  * {@link java.io.InputStream}, {@link java.io.OutputStream}, {@link java.sql.Connection} 등이 좋은 예시입니다.<p>
+ * 자원 닫기는 클라이언트가 놓치기 쉬워서 예측할 수 없는 성능 문제로 이어지기도 합니다.<p>
+ * 상당수가 안전망으로 finalizer를 활용하고 있지만, finalizer는 믿을만하지 못합니다.<p>
  */
 class UseTryWithResourcesThanTryFinally {
 
+    static String firstLineOfFile(String path) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            return br.readLine();
+        } finally {
+            br.close();
+        }
+    }
 }
