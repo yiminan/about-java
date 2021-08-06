c5cfeb661ef3abfc383dbc5aec1b726f53ff5551
orgojy <giveawesome@gmail.com>
2021-08-06 13:26:49 +0900

    Update wrapping class for class "ExampleForTryFinally"

--- src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
+++ src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
@@ -11,29 +11,31 @@ import java.io.*;
  */
 class UseTryWithResourcesThanTryFinally {
 
-    static String firstLineOfFile(String path) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(path));
-        try {
-            return br.readLine();
-        } finally {
-            br.close();
-        }
-    }
-
-    static void copy(String src, String dst) throws IOException {
-        InputStream in = new FileInputStream(src);
-        try {
-            OutputStream out = new FileOutputStream(dst);
+    private static class ExampleForTryFinally {
+        static String firstLineOfFile(String path) throws IOException {
+            BufferedReader br = new BufferedReader(new FileReader(path));
             try {
-                byte[] buf = new byte[100];
-                int n;
-                while ((n = in.read(buf)) >= 0)
-                    out.write(buf, 0, n);
+                return br.readLine();
             } finally {
-                out.close();
+                br.close();
             }
-        } finally {
-            in.close();
+        }
+
+        static void copy(String src, String dst) throws IOException {
+            InputStream in = new FileInputStream(src);
+            try {
+                OutputStream out = new FileOutputStream(dst);
+                try {
+                    byte[] buf = new byte[100];
+                    int n;
+                    while ((n = in.read(buf)) >= 0)
+                        out.write(buf, 0, n);
+                } finally {
+                    out.close();
+                }
+            } finally {
+                in.close();
+            }
         }
     }
 }
