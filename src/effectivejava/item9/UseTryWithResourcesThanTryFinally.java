9aa35892ea3940d7b215739185b33212e1ec5046
orgojy <giveawesome@gmail.com>
2021-08-07 23:38:15 +0900

    Update comment for class UseTryWithResourcesThanTryFinally

--- src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
+++ src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
@@ -11,6 +11,8 @@ import java.io.*;
  * 상당수가 안전망으로 finalizer를 활용하고 있지만, finalizer는 믿을만하지 못합니다.<p>
  * <p>
  * {@link AutoCloseable}을 사용하면, try-with-resources를 사용하여, 자원의 회수를 쉽게 할 수 있고, 가독성 있는 로직을 구현할 수 있습니다.<p>
+ * **중요**<p>
+ * 물론 {@link AutoCloseable}을 구현하는 클래스 내부에서 close()를 오버라이딩을 해야하지만, 비즈니스에 있던 자원회수 로직이 객체에 위임되고 분산된다.<p>
  * <p>
  * 결론)<p>
  * - 꼭 회수해야 하는 자원을 다룰 때는 try-finally 말고, try-with-resources를 사용해야한다.<p>
