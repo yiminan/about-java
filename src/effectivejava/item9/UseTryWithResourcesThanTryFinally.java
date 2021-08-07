734c4a396bfcc87b358adcd40442e5f3b2812d61
orgojy <giveawesome@gmail.com>
2021-08-07 22:55:02 +0900

    Add example method "copy" for class "ExampleForTryWithResources"

--- src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
+++ src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
@@ -50,5 +50,14 @@ class UseTryWithResourcesThanTryFinally {
                 return br.readLine();
             }
         }
+
+        static void copy(String src, String dst) throws IOException {
+            try (InputStream in = new FileInputStream(src); OutputStream out = new FileOutputStream(dst)) {
+                byte[] buf = new byte[100];
+                int n;
+                while ((n = in.read(buf)) >= 0)
+                    out.write(buf, 0, n);
+            }
+        }
     }
 }
