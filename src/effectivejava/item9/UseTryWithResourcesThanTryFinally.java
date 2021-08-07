790db12f8693b0f69c93d778966eec4425d3937e
orgojy <giveawesome@gmail.com>
2021-08-07 23:21:46 +0900

    Update method "firstLineOfFile" to catch statement

--- src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
+++ src/effectivejava/item9/UseTryWithResourcesThanTryFinally.java
@@ -45,15 +45,20 @@ class UseTryWithResourcesThanTryFinally {
     }
 
     private static class ExampleForTryWithResources {
-        static String firstLineOfFile(String path) throws IOException {
+
+        private static final int BUFFER_SIZE = 100;
+
+        static String firstLineOfFile(String path, String defaultValue) throws IOException {
             try (BufferedReader br = new BufferedReader(new FileReader(path))) {
                 return br.readLine();
+            } catch (IOException e) {
+                return defaultValue;
             }
         }
 
         static void copy(String src, String dst) throws IOException {
             try (InputStream in = new FileInputStream(src); OutputStream out = new FileOutputStream(dst)) {
-                byte[] buf = new byte[100];
+                byte[] buf = new byte[BUFFER_SIZE];
                 int n;
                 while ((n = in.read(buf)) >= 0)
                     out.write(buf, 0, n);
