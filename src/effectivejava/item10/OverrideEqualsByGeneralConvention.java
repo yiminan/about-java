76d984f2afc1687b9d33aaed7e1fc6b55b2fec51
orgojy <giveawesome@gmail.com>
2021-08-10 17:38:13 +0900

    Update comment for class "OverrideEqualsByGeneralConvention"

--- src/effectivejava/item10/OverrideEqualsByGeneralConvention.java
+++ src/effectivejava/item10/OverrideEqualsByGeneralConvention.java
@@ -4,6 +4,7 @@ package effectivejava.item10;
  * "equals는 일반 규약을 지켜 재정의해야한다"<p>
  * <p>
  * equals() 메서드를 재정의하는 것은 쉬워 보입니다. 하지만 잘못 정의하게 되면 객체를 비교하는 과정에서 심각한 문제가 발생될 수 있습니다.<p>
+ * 문제가 발생 안하게 하려면 equals()를 재정의하지 않는 방법이 있습니다.<p>
  */
 class OverrideEqualsByGeneralConvention {
 }
