6aa86682cfd369bba5ccb7e5cd31e332d4d2c987
orgojy <giveawesome@gmail.com>
2021-08-11 18:24:08 +0900

    Update comment for class "OverrideEqualsByGeneralConvention"

--- src/effectivejava/item10/OverrideEqualsByGeneralConvention.java
+++ src/effectivejava/item10/OverrideEqualsByGeneralConvention.java
@@ -8,7 +8,10 @@ package effectivejava.item10;
  * 재정의 하지 않으면 객체는 오직 자신과만 같아질 수 있습니다.<p>
  * <p>
  * equals()를 재정의 하지 않아도 되는 경우<p>
- * -
+ * 1.각 인스턴스가 본질적으로 고유한 경우
+ * 2.인스턴스의 '논리적 동치성(logical equality)'을 검사할 일이 없는 경우
+ * 3.상위 클래스에서 재정의한 equals가 하위 클래스에도 딱 들어맞는 경우
+ *
  */
 class OverrideEqualsByGeneralConvention {
 }
