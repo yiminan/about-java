379c41f3b8cde08b9d2dd97142172ea35c41d728
orgojy <giveawesome@gmail.com>
2021-08-23 01:17:13 +0900

    Update comment for class "PrincipleMethodEquals"

--- src/effectivejava/item10/PrincipleMethodEquals.java
+++ src/effectivejava/item10/PrincipleMethodEquals.java
@@ -6,9 +6,9 @@ package effectivejava.item10;
  * - null이 아닌 모든 참조 값 x에 대해, x.equals(x)는 true이다.<p>
  * - 즉, 자기 참조 객체를 equals()로 비교하였을때는 true라는 것이다.<p>
  * 2.대칭성(symmetry)<p>
- * - null이 아닌 모든 참조값 x, y에 대해, x.equals(y)가 true면 y.equals(x)도 true이다.<p>
+ * - null이 아닌 모든 참조 값 x, y에 대해, x.equals(y)가 true면 y.equals(x)도 true이다.<p>
  * 3.추이성(transitivity)<p>
- * - <p>
+ * - null이 아닌 모든 참조 값 x, y, z에 대해, x.equals(y)가 true이고, y.equals(z)가 true면, x.equals(z)는 true이다.<p>
  * 4.일관성(consistency)<p>
  * - <p>
  * 5.null이 아님<p>
