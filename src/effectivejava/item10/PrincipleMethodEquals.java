43945e002e7895ba9f98a5b162289fb74279acd9
orgojy <giveawesome@gmail.com>
2021-08-23 01:37:44 +0900

    Update comment for class "PrincipleMethodEquals"

--- src/effectivejava/item10/PrincipleMethodEquals.java
+++ src/effectivejava/item10/PrincipleMethodEquals.java
@@ -4,7 +4,7 @@ package effectivejava.item10;
  * "equals 메서드를 재정의할 때는 반드시 일반 규약을 따라야 합니다."<p>
  * <p>
  * Object 명세에 적힌 일반 규약은 다음과 같다.<p>
- * 핵심) equals 메서드는 동치관계(equivalence relation)를 구현하며, 다음을 다섯가지를 만족한다.<p>
+ * 핵심) equals 메서드는 동치관계(equivalence relation)를 구현하며, 다음을 다섯가지를 만족해야한다.<p>
  * 1.반사성(reflexivity)<p>
  * - null이 아닌 모든 참조 값 x에 대해, x.equals(x)는 true이다.<p>
  * - 즉, 자기 참조 객체를 equals()로 비교하였을때는 true라는 것이다.<p>
@@ -18,7 +18,7 @@ package effectivejava.item10;
  * - null이 아닌 모든 참조 값 x에 대해, x.equals(null)은 false이다.<p>
  * <p>
  * 반드시 위의 규약은 지켜져야한다.<p>
- * 이유는 컬렉션 클래스를 포함한 많은 클래스는 전달받은 객체가 equals 규약을 지킨다고 가정하고 동작한다.<p>
+ * 이유는 컬렉션 클래스를 포함한 많은 클래스는 전달받은 객체가 equals 규약을 지킨다고 가정하고 동작한다. 그렇기 때문에 지켜지지 않으면 오동작을 발생한다.<p>
  *
  */
 class PrincipleMethodEquals {
