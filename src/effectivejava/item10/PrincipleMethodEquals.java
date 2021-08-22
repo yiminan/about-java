b57fbe78b747515ab62a54156a9df99d43068a02
orgojy <giveawesome@gmail.com>
2021-08-23 00:51:19 +0900

    Update comment for class "PrincipleMethodEquals"

--- src/effectivejava/item10/PrincipleMethodEquals.java
+++ src/effectivejava/item10/PrincipleMethodEquals.java
@@ -2,11 +2,16 @@ package effectivejava.item10;
 
 /**
  * "equals 메서드는 동치관계(equivalence relation)를 구현하며, 다음을 만족한다.<p>
- * 1.
- * 2.
- * 3.
- * 4.
- * 5.
+ * 1.반사성(reflexivity)<p>
+ * - null이 아닌 모든 참조 값 x에 대해, x.equals(x)는 true이다.<p>
+ * 2.<p>
+ * - <p>
+ * 3.<p>
+ * - <p>
+ * 4.<p>
+ * - <p>
+ * 5.<p>
+ * - <p>
  */
 class PrincipleMethodEquals {
 }
