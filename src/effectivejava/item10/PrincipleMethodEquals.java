69ca70ac154f81f37d2a372605d319cf0014c4d9
orgojy <giveawesome@gmail.com>
2021-08-23 01:24:21 +0900

    Update comment for class "PrincipleMethodEquals"

--- src/effectivejava/item10/PrincipleMethodEquals.java
+++ src/effectivejava/item10/PrincipleMethodEquals.java
@@ -1,6 +1,9 @@
 package effectivejava.item10;
 
 /**
+ * "equals 메서드를 재정의할 때는 반드시 일반 규약을 따라야 합니다."<p>
+ * <p>
+ * Object 명세에 적힌 일반 규약은 다음과 같다.<p>
  * "equals 메서드는 동치관계(equivalence relation)를 구현하며, 다음을 만족한다.<p>
  * 1.반사성(reflexivity)<p>
  * - null이 아닌 모든 참조 값 x에 대해, x.equals(x)는 true이다.<p>
