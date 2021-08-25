819915ae4330d82f62c0642ef8cfd8f391b24330
orgojy <giveawesome@gmail.com>
2021-08-25 12:56:14 +0900

    Update example main method for class "ReflexivityForEquivalenceRelation"

--- src/effectivejava/item10/equivalence_relation/ReflexivityForEquivalenceRelation.java
+++ src/effectivejava/item10/equivalence_relation/ReflexivityForEquivalenceRelation.java
@@ -1,10 +1,34 @@
 package effectivejava.item10.equivalence_relation;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * "동치관계(equivalence relation)의 반사성(reflexivity)"
+ * "동치관계(equivalence relation)의 반사성(reflexivity)"<p>
+ *
  */
 class ReflexivityForEquivalenceRelation {
     public static void main(String[] args) {
+        Member member = new Member("Ryan");
+        if (member.equals(member)) {
+            System.out.println("Member is member.");
+        }
+        List<Member> members = Arrays.asList(member);
+        if (members.contains(member)) {
+            System.out.println("Member contains in members.");
+        }
+    }
 
+    private static class Member {
+        private final String name;
+
+        private Member(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return this == o;// 같은
+        }
     }
 }
