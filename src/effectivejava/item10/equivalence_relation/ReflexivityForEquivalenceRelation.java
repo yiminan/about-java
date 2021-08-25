b5f27185df2db47f4edc22c8e07e8ae183043e4d
orgojy <giveawesome@gmail.com>
2021-08-25 11:09:27 +0900

    Update example main method for class "ReflexivityForEquivalenceRelation"

--- src/effectivejava/item10/equivalence_relation/ReflexivityForEquivalenceRelation.java
+++ src/effectivejava/item10/equivalence_relation/ReflexivityForEquivalenceRelation.java
@@ -4,4 +4,7 @@ package effectivejava.item10.equivalence_relation;
  * "동치관계(equivalence relation)의 반사성(reflexivity)"
  */
 class ReflexivityForEquivalenceRelation {
+    public static void main(String[] args) {
+
+    }
 }
