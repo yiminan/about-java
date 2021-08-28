56c2d31d830b0bf7a26b02177aed783dc93f0842
orgojy <giveawesome@gmail.com>
2021-08-28 20:49:29 +0900

    Update example main method for class "ReflexivityForEquivalenceRelation"

--- src/effectivejava/item10/equivalence_relation/ReflexivityForEquivalenceRelation.java
+++ src/effectivejava/item10/equivalence_relation/ReflexivityForEquivalenceRelation.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 /**
  * "동치관계(equivalence relation)의 반사성(reflexivity)"<p>
- * -
+ * - 단순히 객체는 자기 자신과 같아야한다는 것<p>
+ * - 일부러 어기지 않는 이상 객체는 자기 자신과 같을 수 밖에 없다.<p>
+ * - 반사성을 어기면 컬렉션에 객체를 넣고, contains 메서드를 호출하면 false를 내보내게 됩니다.<p>
  */
 class ReflexivityForEquivalenceRelation {
     public static void main(String[] args) {
