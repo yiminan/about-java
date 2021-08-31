e154dd7e95ccef12b4743c87e09d515922d08165
orgojy <giveawesome@gmail.com>
2021-08-31 23:45:59 +0900

    Update comment for class "SymmetryForEquicalenceRelation"

--- src/effectivejava/item10/equivalence_relation/SymmetryForEquicalenceRelation.java
+++ src/effectivejava/item10/equivalence_relation/SymmetryForEquicalenceRelation.java
@@ -4,7 +4,7 @@ package effectivejava.item10.equivalence_relation;
  * "동치관계(equivalence relation)의 대칭성(symmetry)"
  * - 두 객체는 서로에 대한 동치 여부에 똑같이 답해야 한다.<p>
  * - 반사성(reflexivity)과 달리 대칭성(symmetry) 요건은 자칫하면 어길 수 있다.<p>
- * -
+ * - ㄷㅏ음과 같다.
  */
 class SymmetryForEquicalenceRelation {
 }
