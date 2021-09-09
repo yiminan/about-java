15672620dea4c3775f968698b3cd4ec9a433ac2d
orgojy <giveawesome@gmail.com>
2021-09-09 23:43:23 +0900

    Update valid equals to show valid case

--- src/effectivejava/item10/equivalence_relation/SymmetryForEquivalenceRelation.java
+++ src/effectivejava/item10/equivalence_relation/SymmetryForEquivalenceRelation.java
@@ -7,7 +7,7 @@ import java.util.List;
  * "동치관계(equivalence relation)의 대칭성(symmetry)"
  * - 두 객체는 서로에 대한 동치 여부에 똑같이 답해야 한다.<p>
  * - 반사성(reflexivity)과 달리 대칭성(symmetry) 요건은 자칫하면 어길 수 있다.<p>
- * - 
+ * -
  */
 class SymmetryForEquivalenceRelation {
 
@@ -39,13 +39,24 @@ class SymmetryForEquivalenceRelation {
             this.name = name;
         }
 
+        /**
+         * 대칭성 위반 equals 예시
+         */
+//        @Override
+//        public boolean equals(Object obj) {
+//            if (obj instanceof Symmetry)
+//                return name.equalsIgnoreCase(((Symmetry) obj).name);
+//            if (obj instanceof String)
+//                return name.equalsIgnoreCase((String) obj);
+//            return false;
+//        }
+
+        /**
+         * 대칭성을 지키기 위해서 String 케이스를 비교하는 부분을 없애버립니다.
+         */
         @Override
         public boolean equals(Object obj) {
-            if (obj instanceof Symmetry)
-                return name.equalsIgnoreCase(((Symmetry) obj).name);
-            if (obj instanceof String)
-                return name.equalsIgnoreCase((String) obj);
-            return false;
+            return obj instanceof Symmetry && ((Symmetry) obj).name.equalsIgnoreCase(name);
         }
     }
 }
