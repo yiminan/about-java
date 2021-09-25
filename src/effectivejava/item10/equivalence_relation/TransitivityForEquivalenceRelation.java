a8cddfa9ce205a99c55c3f5a48db31bd10e98c1e
orgojy <giveawesome@gmail.com>
2021-09-25 21:46:26 +0900

    Update comment for class TransitivityForEquivalenceRelation

--- src/effectivejava/item10/equivalence_relation/TransitivityForEquivalenceRelation.java
+++ src/effectivejava/item10/equivalence_relation/TransitivityForEquivalenceRelation.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 /**
  * 동치관계(equivalence relation)의 추이성(transitivity)"
  * - 추이성은 'A->B'이고 'B->C' 이면, 'A->C'를 만족해야한다.<p>
- * -
+ * - 동치관계를 지정한다.
  */
 class TransitivityForEquivalenceRelation {
 
