43391a4953e9ef0e69eb1062b691209fa9013658
orgojy <giveawesome@gmail.com>
2021-09-27 23:18:37 +0900

    Update example equals() for ColorPoint to show invalid symmetry

--- src/effectivejava/item10/equivalence_relation/TransitivityForEquivalenceRelation.java
+++ src/effectivejava/item10/equivalence_relation/TransitivityForEquivalenceRelation.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 /**
  * 동치관계(equivalence relation)의 추이성(transitivity)"
  * - 추이성은 'A->B'이고 'B->C' 이면, 'A->C'를 만족해야한다.<p>
- * - 동치관계를 지정한다.
+ * - 동치관계를 지정한다.<p>
  */
 class TransitivityForEquivalenceRelation {
 
@@ -35,6 +35,14 @@ class TransitivityForEquivalenceRelation {
             super(x, y);
             this.color = color;
         }
+
+//        "대칭성 위배"
+//        @Override
+//        public boolean equals(Object o) {
+//            if (!(o instanceof ColorPoint))
+//                return false;
+//            return super.equals(o) && ((ColorPoint) o).color == color;
+//        }
     }
 
     private enum Color {
