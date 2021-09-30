302e8311fb128789d8fa391aa457d7aa64661314
orgojy <giveawesome@gmail.com>
2021-09-30 23:42:50 +0900

    Update TransitivityForEquivalenceRelation to show invalid transitivity equals

--- src/effectivejava/item10/equivalence_relation/TransitivityForEquivalenceRelation.java
+++ src/effectivejava/item10/equivalence_relation/TransitivityForEquivalenceRelation.java
@@ -37,13 +37,17 @@ class TransitivityForEquivalenceRelation {
             this.color = color;
         }
 
-//        "대칭성 위배"
-//        @Override
-//        public boolean equals(Object o) {
-//            if (!(o instanceof ColorPoint))
-//                return false;
-//            return super.equals(o) && ((ColorPoint) o).color == color;
-//        }
+        /**
+         * 추이성 위배
+         */
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Point))
+                return false;
+            if (!(o instanceof ColorPoint))
+                return o.equals(this);
+            return super.equals(o) && ((ColorPoint) o).color == color;
+        }
     }
 
     private enum Color {
