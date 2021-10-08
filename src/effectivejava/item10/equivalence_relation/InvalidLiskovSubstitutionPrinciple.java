ac19cb6ae697f5b372aa815c0ae05da0a58da60a
orgojy <giveawesome@gmail.com>
2021-10-08 19:24:39 +0900

    Update example class for InvalidLiskovSubstitutionPrinciple

--- src/effectivejava/item10/equivalence_relation/InvalidLiskovSubstitutionPrinciple.java
+++ src/effectivejava/item10/equivalence_relation/InvalidLiskovSubstitutionPrinciple.java
@@ -3,6 +3,18 @@ package effectivejava.item10.equivalence_relation;
 /**
  * "리스코프 치환 원칙 위배"한 equals() 예시<p>
  */
-class InvalidLiskovSubstitutionPrinciple {
+class InvalidLiskovSubstitutionPrinciple extends Point {
 
+    public InvalidLiskovSubstitutionPrinciple(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != getClass()) {
+            return false;
+        }
+        Point p = (Point) obj;
+        return p.x == x && p.y == y;
+    }
 }
