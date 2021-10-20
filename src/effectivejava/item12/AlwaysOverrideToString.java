a6cc751702a2e0d78956b8e5038b9c99d190fd1a
orgojy <giveawesome@gmail.com>
2021-10-20 20:54:06 +0900

    Update comment for AlwaysOverrideToString

--- src/effectivejava/item12/AlwaysOverrideToString.java
+++ src/effectivejava/item12/AlwaysOverrideToString.java
@@ -7,6 +7,8 @@ package effectivejava.item12;
  * - 실전에서 toString은 그 객체가 가진 주요 정보 모두를 반환하는게 좋습니다.<p>
  * - 정적 유틸리티 클래스는 toString을 제공할 이유가 없기 때문에 재정할 필요가 없다.<p>
  * - 열거 타입도 자바가 이미 좋은 toString을 제공하니 재정의할 필요가 없다.<p>
+ * - 하위 클래스에서 공유해야할 표현이 있는 추상 클래스라면 재정의할 필요가 있다.<p>
+ * - 대다수의 컬렉션 구현체는 추상 컬렉션의 클래스들의 toString을 상속해서 사용한다.<p>
  */
 class AlwaysOverrideToString {
 }
