e68272beee8176b3c9ceb56033a4e33627778aa8
orgojy <giveawesome@gmail.com>
2021-10-22 17:21:32 +0900

    Add example class PhoneNumber for OverrideCloneCarefully

--- src/effectivejava/item13/OverrideCloneCarefully.java
+++ src/effectivejava/item13/OverrideCloneCarefully.java
@@ -4,4 +4,19 @@ package effectivejava.item13;
  * "clone 재정의는 주의해서 진행하라."<p>
  */
 class OverrideCloneCarefully {
+
+    private static final class PhoneNumber {
+        private final String number;
+
+        public PhoneNumber(String number) {
+            this.number = number;
+        }
+
+        @Override
+        public String toString() {
+            return "PhoneNumber{" +
+                    "number='" + number + '\'' +
+                    '}';
+        }
+    }
 }
