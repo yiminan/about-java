56c85f4302615dceb74d517bec1a74887e80e6e4
orgojy <giveawesome@gmail.com>
2021-10-26 23:45:08 +0900

    Update main method to show example case for ConsiderWhetherToImplementComparable

--- src/effectivejava/item14/ConsiderWhetherToImplementComparable.java
+++ src/effectivejava/item14/ConsiderWhetherToImplementComparable.java
@@ -1,4 +1,17 @@
 package effectivejava.item14;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * "{@link Comparable}을 구현할지 고려해야합니다."<p>
+ */
 public class ConsiderWhetherToImplementComparable {
+
+    public static void main(String[] args) {
+        Set<String> s = new TreeSet<>();
+        Collections.addAll(s, args);
+        System.out.println(s);
+    }
 }
