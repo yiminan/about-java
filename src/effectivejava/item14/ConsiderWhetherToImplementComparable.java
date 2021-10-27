59f2f655bfdc45c7cfb5a696d51961079d8a8ebd
orgojy <giveawesome@gmail.com>
2021-10-28 00:55:56 +0900

    Update comment for class ConsiderWhetherToImplementComparable

--- src/effectivejava/item14/ConsiderWhetherToImplementComparable.java
+++ src/effectivejava/item14/ConsiderWhetherToImplementComparable.java
@@ -8,7 +8,11 @@ import java.util.TreeSet;
  * "{@link Comparable}을 구현할지 고려해야합니다."<p>
  * {@link Comparable}를 구현하면, 수많은 제네릭 알고리즘과 컬렉션의 힘을 누릴 수 있습니다.<p>
  * 사실상 자바 플랫폼 라이브러리의 모든 값 클래스와 열거타입이 {@link Comparable}를 구현했다.<p>
+ * compareTo 메서드를 구현할때는 필드 값 비교시 <와 >를 사용하면 안됩니다.<p>
+ * 결론)<p>
  * 알파벳, 숫자, 연대 같이 순서가 명확한 값 클래스를 작성한다면 {@link Comparable}를 구현하면 좋다.<p>
+ * 박싱된 기본 타입 클래스가 제공하는 정적 compare 메서드를 사용하거나,<p>
+ * {@link java.util.Comparator}가 제공하는 비교자 생성 메서드를 사용해야합니다.<p>
  */
 public class ConsiderWhetherToImplementComparable {
 
