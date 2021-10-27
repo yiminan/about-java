304502ef0d74bfc73bd23af2fa9c8e6bae071942
orgojy <giveawesome@gmail.com>
2021-10-28 00:49:11 +0900

    Update comment for class ConsiderWhetherToImplementComparable

--- src/effectivejava/item14/ConsiderWhetherToImplementComparable.java
+++ src/effectivejava/item14/ConsiderWhetherToImplementComparable.java
@@ -19,7 +19,8 @@ public class ConsiderWhetherToImplementComparable {
         Collections.addAll(strings, "C");
         Collections.addAll(strings, "A");
         /**
-         * String은 {@link Comparable}을 구현한 구현체이기 때문에
+         * String은 {@link Comparable}을 구현한 구현체입니다.<p>
+         * compareTo를 통해서 단순 동치성 비교에 더해 순서까지 비교할 수 있습니다.<p>
          */
         System.out.println(strings);// [A, B, C]
     }
