99e41ddf78efd8b4e7e16383bd7b036f75bbf10b
orgojy <giveawesome@gmail.com>
2021-10-30 00:36:18 +0900

    Update comment for class MinimizeAccessAuthorityOfClassAndMember

--- src/effectivejava/item15/MinimizeAccessAuthorityOfClassAndMember.java
+++ src/effectivejava/item15/MinimizeAccessAuthorityOfClassAndMember.java
@@ -1,7 +1,8 @@
 package effectivejava.item15;
 
 /**
- * "클래스와 멤버의 접근권한을 최소화해야합니다."
+ * "클래스와 멤버의 접근권한을 최소화해야합니다."<p>
+ * 잘 설계된 클래스는 내부 데이터와 구현정보를 외부에 대하여 캡슐화(은닉)시켰는지 입니다.<p>
  */
 class MinimizeAccessAuthorityOfClassAndMember {
 }
