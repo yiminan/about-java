1174391fe10caa099f3888a3bd9a7fa2796b4959
orgojy <giveawesome@gmail.com>
2021-11-03 19:11:39 +0900

    Add example class InvalidPoint to show invalid case

--- src/effectivejava/item16/UseGetterInteadOfPublicField.java
+++ src/effectivejava/item16/UseGetterInteadOfPublicField.java
@@ -4,4 +4,9 @@ package effectivejava.item16;
  * "public 클래스에서는 public 필드가 아닌 접근자 메서드를 사용해야합니다"<p>
  */
 class UseGetterInteadOfPublicField {
+
+    private static class InvalidPoint {
+        public double x;
+        public double y;
+    }
 }
