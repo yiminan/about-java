314459872bf6d7a2627e3394c63a4b1305b9d860
orgojy <giveawesome@gmail.com>
2021-11-04 23:24:22 +0900

    Add class ImmutablePoint to show immutable object with 'Thread Safe'

--- src/effectivejava/item16/UseGetterInteadOfPublicField.java
+++ src/effectivejava/item16/UseGetterInteadOfPublicField.java
@@ -36,4 +36,22 @@ class UseGetterInteadOfPublicField {
             this.y = y;
         }
     }
+
+    private static class ImmutablePoint {
+        private final double x;
+        private final double y;
+
+        public ImmutablePoint(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+    }
 }
