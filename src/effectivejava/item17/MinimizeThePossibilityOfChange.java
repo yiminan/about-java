7a291a754fe52749a830836c92e6c5b913c3bc1a
orgojy <giveawesome@gmail.com>
2021-11-06 01:30:14 +0900

    Update comment for class MinimizeThePossibilityOfChange

--- src/effectivejava/item17/MinimizeThePossibilityOfChange.java
+++ src/effectivejava/item17/MinimizeThePossibilityOfChange.java
@@ -2,9 +2,17 @@ package effectivejava.item17;
 
 /**
  * "변경 가능성을 최소화해야합니다"<p>
+ * <p>
  * 불변 클래스는 인스턴스의 내부 값을 수정할 수 없는 클래스이다.<p>
  * 불변 인스턴스 내부 정보는 고정되어 객체가 파괴되는 순간까지 절대 달라지지 않습니다.<p>
  * ex) {@link String}, {@link java.math.BigInteger}, {@link java.math.BigDecimal}, etc.<p>
+ * <p>
+ * 불변 클래스의 장점<p>
+ * 불변 클래스는 초기화 이후에 변경하지 못하므로 가변 클래스보다 설계하고 구현하기 쉽니다.<p>
+ * 불변 클래스는 Thread Safe 합니다.<p>
+ * <p>
+ * 불변 클래스 작성 다섯 가지 규칙<p>
+ * 1.
  */
 class MinimizeThePossibilityOfChange {
 }
