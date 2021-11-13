581ce1cc4745398d2513b0885694ca61d582b700
orgojy <giveawesome@gmail.com>
2021-11-13 15:41:44 +0900

    Update name for class MutableCompanionExamples

--- src/effectivejava/item17/MutableCompanionClass.java
+++
@@ -1,8 +0,0 @@
-package effectivejava.item17;
-
-/**
- * "가변 동반 클래스 예시"<p>
- */
-class MutableCompanionClass {
-
-}
---
+++ src/effectivejava/item17/MutableCompanionExamples.java
@@ -0,0 +1,9 @@
+package effectivejava.item17;
+
+import java.math.BigDecimal;
+
+/**
+ * "가변 동반(Mutable Companion) 클래스 예시"<p>
+ */
+class MutableCompanionExamples {
+}
