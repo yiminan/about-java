de983df050d4dc5f28a5f130be8b978b73ebf53b
orgojy <giveawesome@gmail.com>
2021-11-17 23:44:08 +0900

    Update comment for class UseCompositionInsteadOfInheritance

--- src/effectivejava/item18/UseCompositionInsteadOfInheritance.java
+++ src/effectivejava/item18/UseCompositionInsteadOfInheritance.java
@@ -3,6 +3,7 @@ package effectivejava.item18;
 /**
  * "상속보다는 컴포지션을 사용해야합니다"<p>
  * 상속은 코드를 재사용하는 강력한 수단이지만, 항상 최선은 아닙니다.<p>
+ * 상위 클래스와 하위 클래스를 모두 같은 프로그래머가 통제하는 패키지 안에서라면 상속도 안전한 방법이다.<p>
  */
 class UseCompositionInsteadOfInheritance {
 }
