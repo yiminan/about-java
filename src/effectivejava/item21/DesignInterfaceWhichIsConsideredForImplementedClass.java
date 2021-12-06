5fe213107c94a9fab28a9bf35209cd23d8b3fa8d
orgojy <giveawesome@gmail.com>
2021-12-06 19:42:15 +0900

    Update comment for class DesignInterfaceWhichIsConsideredForImplementedClass

--- src/effectivejava/item21/DesignInterfaceWhichIsConsideredForImplementedClass.java
+++ src/effectivejava/item21/DesignInterfaceWhichIsConsideredForImplementedClass.java
@@ -3,6 +3,7 @@ package effectivejava.item21;
 /**
  * "인터페이스는 구현하는 쪽을 생각해 설계해야합니다"<p>
  * Java8 전에는 기존 구현체를 깨뜨리지 않고 인터페이스에 메서드를 추가하지 못합니다.<p>
+ * 그래서 인터페이스에 디폴트 메서드라는 개념이 추가되었습니다.<p>
  */
 class DesignInterfaceWhichIsConsideredForImplementedClass {
 }
