73c9bf23341917522c0b5b71bfb450e7a26343e2
orgojy <giveawesome@gmail.com>
2021-12-12 15:41:19 +0900

    Update comment for class DesignInterfaceWhichIsConsideredForImplementedClass

--- src/effectivejava/item21/DesignInterfaceWhichIsConsideredForImplementedClass.java
+++ src/effectivejava/item21/DesignInterfaceWhichIsConsideredForImplementedClass.java
@@ -7,6 +7,7 @@ package effectivejava.item21;
  * 그래서 인터페이스에 디폴트 메서드라는 개념이 추가되었습니다.<p>
  * 하지만 디폴트 메서드를 추가하면 재정의하지 않은 모든 클래스에서 디폴트 메서드 구현이 쓰이게 됩니다.<p>
  * 결국 디폴트 메서드는 은근슬적 기존의 구현 클래스들이 모르는 상태로 추가되게 됩니다.<p>
+ * 기존 인터페이스에 디폴트 메서드로 새 메서드를 추가하는 일은 꼭 핖요한 경우가 아니면 피해야 한다.<p>
  */
 class DesignInterfaceWhichIsConsideredForImplementedClass {
 }
