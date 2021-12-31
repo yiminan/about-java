2ee2fe3e3731c08ddaf9d59faa4248bc7b2cffd0
orgojy <giveawesome@gmail.com>
2022-01-01 01:19:21 +0900

    Test static method by keyword "from"

--- src/test/kotlin/com/example/aboutjava/effectivejava/item1/ConsiderStaticFactoryMethodInsteadOfConstructorTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item1/ConsiderStaticFactoryMethodInsteadOfConstructorTest.java
@@ -1,19 +1,23 @@
 package com.example.aboutjava.effectivejava.item1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 import java.util.Date;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ConsiderStaticFactoryMethodInsteadOfConstructorTest {
 
+    @DisplayName("1개 파라미터로 호출되는 클래스 타입을 반환하는 정적 팩토리 메서드")
     @Test
-    void adsjfklsdf() {
-        /**
-         * "from"
-         * - from이 호출되는 클래스 타입을 반환하는 정적 팩토리 메서드
-         * - 1개 파라미터를 전달받는 경우
-         */
-        Date nowDate = Date.from(Instant.now());
+    void from() {
+        // given
+        Instant now = Instant.now();
+        // when
+        Date nowDate = Date.from(now);
+        // then
+        assertThat(nowDate).isEqualTo(new Date(now.toEpochMilli()));
     }
 }
