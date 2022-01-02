b386d093490de74d3886c33ff2381ed29aad5260
orgojy <giveawesome@gmail.com>
2022-01-03 03:21:14 +0900

    Test cases for UserFakeObjectInsteadOfMockObject

--- src/test/kotlin/com/example/aboutjava/elegantobject/step2_education/step2_8_usefakeobjectinsteadofmockobject/UseFakeObjectInsteadOfMockObjectTest.java
+++ src/test/kotlin/com/example/aboutjava/elegantobject/step2_education/step2_8_usefakeobjectinsteadofmockobject/UseFakeObjectInsteadOfMockObjectTest.java
@@ -4,28 +4,30 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static com.example.aboutjava.elegantobject.step2_education.step2_8_usefakeobjectinsteadofmockobject.UseFakeObjectInsteadOfMockObject.Cash;
+import static com.example.aboutjava.elegantobject.step2_education.step2_8_usefakeobjectinsteadofmockobject.UseFakeObjectInsteadOfMockObject.Exchange;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 
 class UseFakeObjectInsteadOfMockObjectTest {
 
     @DisplayName("Mock 객체를 사용한 테스트")
     @Test
     void mock() {
-        UseFakeObjectInsteadOfMockObject.Exchange exchange = Mockito.mock(UseFakeObjectInsteadOfMockObject.Exchange.class);
-        Mockito.doReturn(1.15)
-                .when(exchange)
-                .rate("USD", "EUR");
-        UseFakeObjectInsteadOfMockObject.Cash dollar = new UseFakeObjectInsteadOfMockObject.Cash(exchange, 500);
-        UseFakeObjectInsteadOfMockObject.Cash euro = dollar.in("EUR");
-        assertThat(euro.cents()).isEqualTo(617);
+        Exchange exchange = Mockito.mock(Exchange.class);
+        given(exchange.rate(anyString(), anyString())).willReturn(1.15f);
+        Cash dollar = new Cash(exchange, 500);
+        Cash euro = dollar.in("EUR");
+        assertThat(euro.cents()).isEqualTo(575);
     }
 
     @DisplayName("Fake 객체를 사용한 테스트")
     @Test
     void fake() {
-        UseFakeObjectInsteadOfMockObject.Exchange exchange = new UseFakeObjectInsteadOfMockObject.Exchange.Fake();
-        UseFakeObjectInsteadOfMockObject.Cash dollar = new UseFakeObjectInsteadOfMockObject.Cash(exchange, 500);
-        UseFakeObjectInsteadOfMockObject.Cash euro = dollar.in("EUR");
+        Exchange exchange = new Exchange.Fake();
+        Cash dollar = new Cash(exchange, 500);
+        Cash euro = dollar.in("EUR");
         assertThat(euro.cents()).isEqualTo(617);
     }
 }
