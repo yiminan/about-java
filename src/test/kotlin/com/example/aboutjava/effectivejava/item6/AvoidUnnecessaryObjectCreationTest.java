88589953029a43618aa7a4227ba3b26f9541391c
orgojy <giveawesome@gmail.com>
2022-01-07 13:46:06 +0900

    Test class for AvoidUnnecessaryObjectCreationTest

---
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item6/AvoidUnnecessaryObjectCreationTest.java
@@ -0,0 +1,10 @@
+package com.example.aboutjava.effectivejava.item6;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Item 6 - 불필요한 객체 생성을 피해야합니다")
+class AvoidUnnecessaryObjectCreationTest {
+
+}
