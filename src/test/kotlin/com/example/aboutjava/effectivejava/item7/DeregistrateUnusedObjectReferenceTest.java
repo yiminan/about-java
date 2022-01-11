21e36921ea5ec8776612a01112e8844e6fa04ea7
orgojy <giveawesome@gmail.com>
2022-01-11 23:13:48 +0900

    Test case to use array in case of memory leak

--- src/test/kotlin/com/example/aboutjava/effectivejava/item7/DeregistrateUnusedObjectReferenceTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item7/DeregistrateUnusedObjectReferenceTest.java
@@ -1,7 +1,15 @@
 package com.example.aboutjava.effectivejava.item7;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeregistrateUnusedObjectReferenceTest {
 
+    @DisplayName("메모리가 누수가 되는 Array 사용 형태")
+    @Test
+    void usageArrayInCaseOfMemoryLeak() {
+
+    }
 }
