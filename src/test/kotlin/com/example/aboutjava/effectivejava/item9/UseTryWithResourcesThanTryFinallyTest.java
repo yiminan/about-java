850ef5edecec88f76dc669650c562e0f19bef6dd
orgojy <giveawesome@gmail.com>
2022-01-16 22:35:47 +0900

    Test comment for case UseTryWithResourcesThanTryFinallyTest

--- src/test/kotlin/com/example/aboutjava/effectivejava/item9/UseTryWithResourcesThanTryFinallyTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item9/UseTryWithResourcesThanTryFinallyTest.java
@@ -1,7 +1,10 @@
 package com.example.aboutjava.effectivejava.item9;
 
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Item 9 - try-finally보다는 try-with-resources를 사용해야합니다.")
 class UseTryWithResourcesThanTryFinallyTest {
 
 }
