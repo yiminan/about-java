4c8acdb62facbefb5e20ca07bcb3a6fc24df9d73
orgojy <giveawesome@gmail.com>
2022-01-18 23:48:53 +0900

    Test case useTryFinally to show first line in file

--- src/test/kotlin/com/example/aboutjava/effectivejava/item9/UseTryWithResourcesThanTryFinallyTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item9/UseTryWithResourcesThanTryFinallyTest.java
@@ -1,10 +1,24 @@
 package com.example.aboutjava.effectivejava.item9;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Item 9 - try-finally보다는 try-with-resources를 사용해야합니다.")
 class UseTryWithResourcesThanTryFinallyTest {
 
+    @DisplayName("보완이 필요한 try-finally 사용 예시")
+    @Test
+    void useTryFinally() throws IOException {
+        // given
+        String testFileName = "test.txt";
+        // when
+        String firstLine = ExampleForTryFinally.firstLineOfFile(testFileName);
+        // then
+        assertThat(firstLine).isEqualTo("test");
+    }
 }
