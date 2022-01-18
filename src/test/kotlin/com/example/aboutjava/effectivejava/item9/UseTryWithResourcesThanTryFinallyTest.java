415decddf98878863952ecbfda04c99923b8063a
orgojy <giveawesome@gmail.com>
2022-01-18 23:51:50 +0900

    Test case useTryWithResources to show first line in file

--- src/test/kotlin/com/example/aboutjava/effectivejava/item9/UseTryWithResourcesThanTryFinallyTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item9/UseTryWithResourcesThanTryFinallyTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Item 9 - try-finally보다는 try-with-resources를 사용해야합니다.")
 class UseTryWithResourcesThanTryFinallyTest {
@@ -18,6 +17,17 @@ class UseTryWithResourcesThanTryFinallyTest {
         String testFileName = "test.txt";
         // when
         String firstLine = ExampleForTryFinally.firstLineOfFile(testFileName);
+        // then
+        assertThat(firstLine).isEqualTo("test");
+    }
+
+    @DisplayName("개선된 try-with-resources")
+    @Test
+    void useTryWithResources() {
+        // given
+        String testFileName = "test.txt";
+        // when
+        String firstLine = ExampleForTryWithResources.firstLineOfFile(testFileName, "test");
         // then
         assertThat(firstLine).isEqualTo("test");
     }
