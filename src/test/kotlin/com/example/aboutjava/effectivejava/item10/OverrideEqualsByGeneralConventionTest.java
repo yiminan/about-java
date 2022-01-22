32e48c7f664c06b33da851c8848aa50f6111bea4
orgojy <giveawesome@gmail.com>
2022-01-22 17:57:15 +0900

    Update display name for OverrideEqualsByGeneralConventionTest

--- src/test/kotlin/com/example/aboutjava/effectivejava/item10/OverrideEqualsByGeneralConventionTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item10/OverrideEqualsByGeneralConventionTest.java
@@ -1,7 +1,10 @@
 package com.example.aboutjava.effectivejava.item10;
 
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Item 10 - equals 는 일반 규약을 지켜 재정의 해야 한다.")
 class OverrideEqualsByGeneralConventionTest {
 
 }
