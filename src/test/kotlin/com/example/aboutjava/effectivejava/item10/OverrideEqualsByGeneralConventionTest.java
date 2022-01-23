0a4864c548c616687ad43d6584d576628c0e60ff
orgojy <giveawesome@gmail.com>
2022-01-23 17:12:16 +0900

    Test repository for JPA about override equals

--- src/test/kotlin/com/example/aboutjava/effectivejava/item10/OverrideEqualsByGeneralConventionTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item10/OverrideEqualsByGeneralConventionTest.java
@@ -1,10 +1,16 @@
 package com.example.aboutjava.effectivejava.item10;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Item 10 - equals 는 일반 규약을 지켜 재정의 해야 한다.")
+@DataJpaTest
 class OverrideEqualsByGeneralConventionTest {
 
+    @Autowired
+    private UserRepository userRepository;
 }
