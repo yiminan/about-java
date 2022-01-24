dd2e79dcfcc2bffa504d3c589957928a718c024f
orgojy <giveawesome@gmail.com>
2022-01-24 23:51:55 +0900

    Test display name for OverrideEqualsWithHashCodeTest

--- src/test/kotlin/com/example/aboutjava/effectivejava/item11/OverrideEqualsWithHashCodeTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item11/OverrideEqualsWithHashCodeTest.java
@@ -1,7 +1,10 @@
 package com.example.aboutjava.effectivejava.item11;
 
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Item 11 - equals 를 재정의 할 때는 hashCode 도 재정의 해야 합니다")
 class OverrideEqualsWithHashCodeTest {
 
 }
