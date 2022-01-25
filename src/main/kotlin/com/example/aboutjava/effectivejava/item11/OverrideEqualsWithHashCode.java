9c002bb00d99bcc90e8623c1cf3851e4d7943ab8
orgojy <giveawesome@gmail.com>
2022-01-25 23:21:53 +0900

    Minor comment for class OverrideEqualsWithHashCode

--- src/main/kotlin/com/example/aboutjava/effectivejava/item11/OverrideEqualsWithHashCode.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item11/OverrideEqualsWithHashCode.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Objects;
 
 /**
- * "equals를 재정의할때는 hashCode도 재정의해야합니다"<p>
+ * "Item 11 - equals 를 재정의할 때는 hashCode 도 재정의해야 합니다"<p>
  * hashCode를 재정의하지 않으면 일반 규약을 어기게 됩니다.<p>
  * 규약을 어긴 클래스의 인스턴스를 {@link HashMap}, {@link java.util.HashSet}와 같은 컬렉션의 원소로 사용할 때 문제를 일으킨다.<p>
  * <p>
