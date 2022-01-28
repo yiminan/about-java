6ca421b6a359cce6f84a479f0d49657ee4916758
orgojy <giveawesome@gmail.com>
2022-01-28 23:21:48 +0900

    Update comment for class AlwaysOverrideToString

--- src/main/kotlin/com/example/aboutjava/effectivejava/item12/AlwaysOverrideToString.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item12/AlwaysOverrideToString.java
@@ -1,7 +1,8 @@
 package com.example.aboutjava.effectivejava.item12;
 
 /**
- * "toString method는 항상 재정의해야합니다."<p>
+ * "Item 12 - toString method는 항상 재정의해야합니다."<p>
+ * <p>
  * toString의 규약은 '모든 하위 클래스에서 이 메서드를 재정의하라'입니다.<p>
  * - toString을 잘 구현한 클래스는 사용하기 좋고, 디버깅하기 쉽습니다.<p>
  * - 실전에서 toString은 그 객체가 가진 주요 정보 모두를 반환하는게 좋습니다.<p>
