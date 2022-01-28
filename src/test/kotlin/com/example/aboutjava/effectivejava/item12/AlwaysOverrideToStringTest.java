de9539e18df4ba3e3bca62fa3149aa28474a9543
orgojy <giveawesome@gmail.com>
2022-01-28 23:27:43 +0900

    Test class AlwaysOverrideToStringTest

---
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item12/AlwaysOverrideToStringTest.java
@@ -0,0 +1,10 @@
+package com.example.aboutjava.effectivejava.item12;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Item 12 - toString method 는 항상 재정의 해야 합니다.")
+class AlwaysOverrideToStringTest {
+
+}
