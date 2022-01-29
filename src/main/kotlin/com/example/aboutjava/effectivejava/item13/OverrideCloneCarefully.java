80297a4e76c0bbca60b4f9547013657fc024ddc1
orgojy <giveawesome@gmail.com>
2022-01-29 21:49:36 +0900

    Test case to clone with good override

--- src/main/kotlin/com/example/aboutjava/effectivejava/item13/OverrideCloneCarefully.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item13/OverrideCloneCarefully.java
@@ -1,7 +1,9 @@
 package com.example.aboutjava.effectivejava.item13;
 
+import java.util.Objects;
+
 /**
- * "clone 재정의는 주의해서 진행하라."<p>
+ * "Item 13 - clone 재정의는 주의해서 진행하라."<p>
  * {@link Cloneable}을 확장해서 인터페이스를 만들어서도, 구현 클래스를 만들어서도 안됩니다.<p>
  * final 클래스라면 {@link Cloneable}를 구현해도 위험이 작지만, 성능적인 검토는 필요하다.<p>
  * 단, 배열에 대해서는 clone 메서드를 사용하는 것이 적절한 판단이 될 수 있다.<p>
@@ -9,17 +11,24 @@ package com.example.aboutjava.effectivejava.item13;
  */
 class OverrideCloneCarefully {
 
-    public static void main(String[] args) {
-        PhoneNumber phoneNumber = new PhoneNumber("010-1234-5678");
-        PhoneNumber copyPhoneNumber = (PhoneNumber) phoneNumber.clone();
-        System.out.println(copyPhoneNumber);
-    }
-
-    private static final class PhoneNumber implements Cloneable {
+    static final class PhoneNumber implements Cloneable {
         private final String number;
 
         public PhoneNumber(String number) {
             this.number = number;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PhoneNumber that = (PhoneNumber) o;
+            return Objects.equals(number, that.number);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(number);
         }
 
         @Override
--- src/test/kotlin/com/example/aboutjava/effectivejava/item13/OverrideCloneCarefullyTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item13/OverrideCloneCarefullyTest.java
@@ -1,7 +1,26 @@
 package com.example.aboutjava.effectivejava.item13;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static com.example.aboutjava.effectivejava.item13.OverrideCloneCarefully.PhoneNumber;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("Item 13 - clone 재정의는 주의해서 진행하라.")
 class OverrideCloneCarefullyTest {
 
+    @DisplayName("clone의 재정의가 문제 없는 경우")
+    @Test
+    void cloneWithGoodOverride() {
+        // given
+        PhoneNumber phoneNumber = new PhoneNumber("010-1234-5678");
+        // when
+        PhoneNumber copyPhoneNumber = (PhoneNumber) phoneNumber.clone();
+        // then
+        assertAll(
+                () -> assertThat(phoneNumber).isNotSameAs(copyPhoneNumber),
+                () -> assertThat(phoneNumber).isEqualTo(copyPhoneNumber)
+        );
+    }
 }
