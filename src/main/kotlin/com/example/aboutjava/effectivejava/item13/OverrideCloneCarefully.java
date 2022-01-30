f4e816bc25038d2e1a2d7748546459cf2898a8a9
orgojy <giveawesome@gmail.com>
2022-01-30 21:59:26 +0900

    Test comment for OverrideCloneCarefully

--- src/main/kotlin/com/example/aboutjava/effectivejava/item13/OverrideCloneCarefully.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item13/OverrideCloneCarefully.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 /**
  * "Item 13 - clone 재정의는 주의해서 진행하라."<p>
+ * <p>
  * {@link Cloneable}을 확장해서 인터페이스를 만들어서도, 구현 클래스를 만들어서도 안됩니다.<p>
  * final 클래스라면 {@link Cloneable}를 구현해도 위험이 작지만, 성능적인 검토는 필요하다.<p>
  * 단, 배열에 대해서는 clone 메서드를 사용하는 것이 적절한 판단이 될 수 있다.<p>
