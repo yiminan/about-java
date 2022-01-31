7f83a0e6504d1c3f7626cb790981a86ed5bb40e4
orgojy <giveawesome@gmail.com>
2022-01-31 21:43:57 +0900

    Test case for ConsiderWhetherToImplementComparableTest

--- src/test/kotlin/com/example/aboutjava/effectivejava/item14/ConsiderWhetherToImplementComparableTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item14/ConsiderWhetherToImplementComparableTest.java
@@ -1,7 +1,10 @@
 package com.example.aboutjava.effectivejava.item14;
 
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Item 14 - Comparable을 구현할지 고려해야합니다.")
 class ConsiderWhetherToImplementComparableTest {
 
 }
