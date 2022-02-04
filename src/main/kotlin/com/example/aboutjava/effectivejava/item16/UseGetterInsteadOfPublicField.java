b110cf3e52253a1542f7de38181b01d7858810b7
orgojy <giveawesome@gmail.com>
2022-02-05 00:49:03 +0900

    Update class name for UseGetterInsteadOfPublicField

--- src/main/kotlin/com/example/aboutjava/effectivejava/item16/UseGetterInsteadOfPublicField.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item16/UseGetterInsteadOfPublicField.java
@@ -4,7 +4,7 @@ package com.example.aboutjava.effectivejava.item16;
  * "Item 16 - public 클래스에서는 public 필드가 아닌 접근자 메서드를 사용해야합니다"<p>
  * 기본적으로 내부 인스턴스 필드는 private을 적용하고, 필드의 내용을 변경하거나, 가져올땐 public setter, getter를 사용합니다.<p>
  */
-class UseGetterInteadOfPublicField {
+class UseGetterInsteadOfPublicField {
 
     /**
      * public class로 지정해서 사용한다면 절대 내부 가변 필드를 public으로 노출해서는 안된다.
