2e953e56407a703cfa1dd33c6a2688ab541ea506
orgojy <giveawesome@gmail.com>
2022-02-06 23:29:00 +0900

    Test display name for UseGetterInsteadOfPublicFieldTest

--- src/test/kotlin/com/example/aboutjava/effectivejava/item16/UseGetterInsteadOfPublicFieldTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item16/UseGetterInsteadOfPublicFieldTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static com.example.aboutjava.effectivejava.item16.UseGetterInsteadOfPublicField.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("Item 16 - public 클래스에서는 public 필드가 아닌 접근자 메서드를 사용해야합니다")
 class UseGetterInsteadOfPublicFieldTest {
 
     @DisplayName("Thread Safe 하지 않은 public 인스턴스 필드를 사용하는 경우")
