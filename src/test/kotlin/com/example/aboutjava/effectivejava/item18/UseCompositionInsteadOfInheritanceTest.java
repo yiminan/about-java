612cf09ab6e7134f19b85927244a82be9c545aef
orgojy <giveawesome@gmail.com>
2022-02-11 21:22:26 +0900

    Update display name for UseCompositionInsteadOfInheritanceTest

--- src/test/kotlin/com/example/aboutjava/effectivejava/item18/UseCompositionInsteadOfInheritanceTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item18/UseCompositionInsteadOfInheritanceTest.java
@@ -1,7 +1,10 @@
 package com.example.aboutjava.effectivejava.item18;
 
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Item 18 - 상속보다는 컴포지션(구성)을 사용해야합니다")
 class UseCompositionInsteadOfInheritanceTest {
 
 }
