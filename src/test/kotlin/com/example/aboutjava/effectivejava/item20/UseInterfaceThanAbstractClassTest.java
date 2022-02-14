e3d130a61f09ef1cf831981b1a28b861bf585d1a
orgojy <giveawesome@gmail.com>
2022-02-14 23:49:18 +0900

    Test display name for class UseInterfaceThanAbstractClassTest

--- src/test/kotlin/com/example/aboutjava/effectivejava/item20/UseInterfaceThanAbstractClassTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item20/UseInterfaceThanAbstractClassTest.java
@@ -1,7 +1,10 @@
 package com.example.aboutjava.effectivejava.item20;
 
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Item 20 - 추상 클래스보다는 인터페이스를 우선하라")
 class UseInterfaceThanAbstractClassTest {
 
 }
