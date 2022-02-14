15448db700598a0e72705d04bf4e9390f4970ea9
orgojy <giveawesome@gmail.com>
2022-02-14 23:48:05 +0900

    Update comment for class UseInterfaceThanAbstractClass

--- src/main/kotlin/com/example/aboutjava/effectivejava/item20/UseInterfaceThanAbstractClass.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item20/UseInterfaceThanAbstractClass.java
@@ -1,7 +1,7 @@
 package com.example.aboutjava.effectivejava.item20;
 
 /**
- * "추상 클래스보다는 인터페이스를 우선하라"<p>
+ * "Item 20 - 추상 클래스보다는 인터페이스를 우선하라"<p>
  * <p>
  * 자바가 제공하는 다중 구현 메커니증은 (1) 인터페이스 (2) 추상클래스 입니다.<p>
  * 물론 인터페이스에서 default method로 instance method를 구현형태로 제공할 수 있습니다.<p>
