986b6ec3e7a6d5c0c411d58704c335099c1b534a
orgojy <giveawesome@gmail.com>
2022-02-15 23:15:06 +0900

    Test case useClassWithInterfaceEasily

--- src/test/kotlin/com/example/aboutjava/effectivejava/item20/UseInterfaceThanAbstractClassTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item20/UseInterfaceThanAbstractClassTest.java
@@ -1,10 +1,22 @@
 package com.example.aboutjava.effectivejava.item20;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.example.aboutjava.effectivejava.item20.UseInterfaceThanAbstractClass.User;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Item 20 - 추상 클래스보다는 인터페이스를 우선하라")
 class UseInterfaceThanAbstractClassTest {
 
+    @DisplayName("기존 클래스에도 손쉽게 새로운 인터페이스를 사용 구현")
+    @Test
+    void useClassWithInterfaceEasily() {
+        // given
+        User user = new User("nickName", 25);
+        // when
+        int isSame = user.compareTo(new User("nickName", 25));
+        // then
+        assertThat(isSame).isZero();
+    }
 }
