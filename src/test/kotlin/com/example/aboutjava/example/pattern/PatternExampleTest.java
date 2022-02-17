c8af923634762ba871adca46b0176f546c5587a0
orgojy <giveawesome@gmail.com>
2022-02-17 18:03:01 +0900

    Test case for validateValidEmail

--- src/test/kotlin/com/example/aboutjava/example/pattern/PatternExampleTest.java
+++ src/test/kotlin/com/example/aboutjava/example/pattern/PatternExampleTest.java
@@ -11,6 +11,17 @@ class PatternExampleTest {
     // email 유효 검사 또는 suffix '_suspended' 까지 허용
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9._+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}(_suspended)?$");
 
+    @DisplayName("유효한 Email 검증 예시")
+    @Test
+    void validateValidEmail() {
+        // given
+        String email = "ryan.an@gmail.com";
+        // when
+        boolean isEmail = EMAIL_PATTERN.matcher(email).find();
+        // then
+        assertThat(isEmail).isTrue();
+    }
+
     @DisplayName("유효한 Email 검증 예시 with suffix `_suspended`")
     @Test
     void validateValidEmailWithSuffix_suspended() {
