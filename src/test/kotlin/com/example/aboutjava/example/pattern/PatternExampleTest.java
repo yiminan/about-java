d5f0f443132950bc3ad08c74c52588a1558e844b
orgojy <giveawesome@gmail.com>
2022-02-17 18:40:08 +0900

    Test case validateInvalidEmails

--- src/test/kotlin/com/example/aboutjava/example/pattern/PatternExampleTest.java
+++ src/test/kotlin/com/example/aboutjava/example/pattern/PatternExampleTest.java
@@ -3,7 +3,11 @@ package com.example.aboutjava.example.pattern;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,5 +35,19 @@ class PatternExampleTest {
         boolean isEmail = EMAIL_PATTERN.matcher(email).find();
         // then
         assertThat(isEmail).isTrue();
+    }
+
+    @DisplayName("유효한 Email 검증 예시 with suffix `_suspended`")
+    @Test
+    void validateInvalidEmails() {
+        // given
+        List<String> emails = Arrays.asList("ryan.an**@gmail.com", "120938", "ryan.an@gmail");
+        // when
+        List<Boolean> isEmails = emails.stream()
+                .map(EMAIL_PATTERN::matcher)
+                .map(Matcher::find)
+                .collect(Collectors.toList());
+        // then
+        assertThat(isEmails).containsOnly(false);
     }
 }
