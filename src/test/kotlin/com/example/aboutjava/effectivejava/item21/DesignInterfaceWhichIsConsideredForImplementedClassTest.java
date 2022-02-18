3320026471a25cfe46c3c9ce875587b8128ff8eb
orgojy <giveawesome@gmail.com>
2022-02-18 22:05:21 +0900

    Test class DesignInterfaceWhichIsConsideredForImplementedClassTest

---
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item21/DesignInterfaceWhichIsConsideredForImplementedClassTest.java
@@ -0,0 +1,10 @@
+package com.example.aboutjava.effectivejava.item21;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Item 21 - 인터페이스는 구현하는 쪽을 생각해 설계해야합니다")
+class DesignInterfaceWhichIsConsideredForImplementedClassTest {
+
+}
