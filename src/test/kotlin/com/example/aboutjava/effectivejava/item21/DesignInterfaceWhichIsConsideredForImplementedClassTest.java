f2e7bc170a0125cab0a8e666857efb5b30349f8a
orgojy <giveawesome@gmail.com>
2022-02-22 00:01:56 +0900

    Test case useImplementationThanDefaultMethodInInterface to implement it

--- src/test/kotlin/com/example/aboutjava/effectivejava/item21/DesignInterfaceWhichIsConsideredForImplementedClassTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item21/DesignInterfaceWhichIsConsideredForImplementedClassTest.java
@@ -3,7 +3,9 @@ package com.example.aboutjava.effectivejava.item21;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.example.aboutjava.effectivejava.item21.DesignInterfaceWhichIsConsideredForImplementedClass.DefaultTestable;
+import static com.example.aboutjava.effectivejava.item21.DesignInterfaceWhichIsConsideredForImplementedClass.StudentTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Item 21 - 인터페이스는 구현하는 쪽을 생각해 설계해야합니다")
 class DesignInterfaceWhichIsConsideredForImplementedClassTest {
@@ -11,6 +13,9 @@ class DesignInterfaceWhichIsConsideredForImplementedClassTest {
     @DisplayName("디폴트 메서드를 사용하는 형태보다는 구현체에서 직접 구현하는 방법")
     @Test
     void useImplementationThanDefaultMethodInInterface() {
-
+        // given
+        DefaultTestable studentTest = new StudentTest();
+        // when & then
+        assertThat(studentTest.rest()).isEqualTo("게임을 한다.");
     }
 }
