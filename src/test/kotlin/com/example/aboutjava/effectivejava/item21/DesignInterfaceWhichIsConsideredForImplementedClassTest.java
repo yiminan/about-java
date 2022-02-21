a32115da79bcd3122a527717cbbb5a9b6bd378fd
orgojy <giveawesome@gmail.com>
2022-02-22 00:12:38 +0900

    Add case dontUseImplementationWithDefaultMethodInInterface

--- src/test/kotlin/com/example/aboutjava/effectivejava/item21/DesignInterfaceWhichIsConsideredForImplementedClassTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item21/DesignInterfaceWhichIsConsideredForImplementedClassTest.java
@@ -3,8 +3,7 @@ package com.example.aboutjava.effectivejava.item21;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.example.aboutjava.effectivejava.item21.DesignInterfaceWhichIsConsideredForImplementedClass.DefaultTestable;
-import static com.example.aboutjava.effectivejava.item21.DesignInterfaceWhichIsConsideredForImplementedClass.StudentWithOverrideRest;
+import static com.example.aboutjava.effectivejava.item21.DesignInterfaceWhichIsConsideredForImplementedClass.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Item 21 - 인터페이스는 구현하는 쪽을 생각해 설계해야합니다")
@@ -17,5 +16,14 @@ class DesignInterfaceWhichIsConsideredForImplementedClassTest {
         DefaultTestable studentTest = new StudentWithOverrideRest();
         // when & then
         assertThat(studentTest.rest()).isEqualTo("게임을 한다.");
+    }
+
+    @DisplayName("디폴트 메서드를 사용하는 추천하지 않는 방법")
+    @Test
+    void dontUseImplementationWithDefaultMethodInInterface() {
+        // given
+        DefaultTestable studentTest = new StudentWithDefaultRest();
+        // when & then
+        assertThat(studentTest.rest()).isEqualTo("그냥 쉰다.");
     }
 }
