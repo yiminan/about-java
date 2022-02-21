5dd2c685c078bcc9c9e6a29a764028a48aea49ed
orgojy <giveawesome@gmail.com>
2022-02-21 23:49:55 +0900

    Test case useImplementationThanDefaultMethodInInterface

--- src/test/kotlin/com/example/aboutjava/effectivejava/item21/DesignInterfaceWhichIsConsideredForImplementedClassTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item21/DesignInterfaceWhichIsConsideredForImplementedClassTest.java
@@ -1,10 +1,16 @@
 package com.example.aboutjava.effectivejava.item21;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Item 21 - 인터페이스는 구현하는 쪽을 생각해 설계해야합니다")
 class DesignInterfaceWhichIsConsideredForImplementedClassTest {
 
+    @DisplayName("디폴트 메서드를 사용하는 형태보다는 구현체에서 직접 구현하는 방법")
+    @Test
+    void useImplementationThanDefaultMethodInInterface() {
+
+    }
 }
