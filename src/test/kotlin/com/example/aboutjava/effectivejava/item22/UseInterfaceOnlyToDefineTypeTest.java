f388784161013c1df5996df575beb7a923655241
orgojy <giveawesome@gmail.com>
2022-02-23 23:07:27 +0900

    Test class UseInterfaceOnlyToDefineTypeTest

--- src/test/kotlin/com/example/aboutjava/effectivejava/item22/UseInterfaceOnlyToDefineTypeTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item22/UseInterfaceOnlyToDefineTypeTest.java
@@ -1,7 +1,10 @@
 package com.example.aboutjava.effectivejava.item22;
 
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Item 22 - 인터페이스는 타입을 정의하는 용도로만 사용해야합니다")
 class UseInterfaceOnlyToDefineTypeTest {
 
 }
