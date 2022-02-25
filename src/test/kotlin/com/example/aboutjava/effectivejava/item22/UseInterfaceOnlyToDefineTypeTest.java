f23c2d39b319bc2a8438979820b42b77a470f7f0
orgojy <giveawesome@gmail.com>
2022-02-25 23:41:43 +0900

    Test case dontUseConstantByInterface

--- src/test/kotlin/com/example/aboutjava/effectivejava/item22/UseInterfaceOnlyToDefineTypeTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item22/UseInterfaceOnlyToDefineTypeTest.java
@@ -1,10 +1,15 @@
 package com.example.aboutjava.effectivejava.item22;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Item 22 - 인터페이스는 타입을 정의하는 용도로만 사용해야합니다")
 class UseInterfaceOnlyToDefineTypeTest {
-
+    @DisplayName("인터페이스로 상수를 사용하는 잘못된 사용 예시")
+    @Test
+    void dontUseConstantByInterface() {
+        assertThat(UseInterfaceOnlyToDefineType.InvalidInterface.ONE).isOne();
+    }
 }
