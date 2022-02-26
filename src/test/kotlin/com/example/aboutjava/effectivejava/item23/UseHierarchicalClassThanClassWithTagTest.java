72f4514b7c09090c62cfdcfb8e82c3ee39559f93
orgojy <giveawesome@gmail.com>
2022-02-26 19:34:17 +0900

    Test class UseHierarchicalClassThanClassWithTagTest

---
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item23/UseHierarchicalClassThanClassWithTagTest.java
@@ -0,0 +1,10 @@
+package com.example.aboutjava.effectivejava.item23;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("item 23 - 태그 달린 클래스보다는 클래스 계층구조를 활용해야 합니다.")
+class UseHierarchicalClassThanClassWithTagTest {
+
+}
