7e1d2e3653503f6d5420acdf21f101a8af2b397e
orgojy <giveawesome@gmail.com>
2022-02-27 22:53:15 +0900

    Update comment for ExampleHierarchicalClass

--- src/main/kotlin/com/example/aboutjava/effectivejava/item23/ExampleHierarchicalClass.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item23/ExampleHierarchicalClass.java
@@ -2,6 +2,10 @@ package com.example.aboutjava.effectivejava.item23;
 
 /**
  * 클래스 계층 구조로의 리팩터링한 예시<p>
+ * a. root가 될 추상 클래스를 정의하자
+ * b. 태그에 따라 행동이 달라지던 메서드는 추상 메서드로 구현하자 : 그렇지않으면 일반메서드
+ * c. 공통된 필드는 모두 root 클래스로 올리자
+ * d. 구체 클래스에서 추상클래스를 의미에 맞게 정의하자
  */
 class ExampleHierarchicalClass implements UseHierarchicalClassThanClassWithTag {
     abstract class Figure {
@@ -14,17 +18,26 @@ class ExampleHierarchicalClass implements UseHierarchicalClassThanClassWithTag {
 
         Rectangle(double length, double width) {
             this.length = length;
-            this.width  = width;
+            this.width = width;
         }
-        @Override double area() { return length * width; }
+
+        @Override
+        double area() {
+            return length * width;
+        }
     }
 
     class Circle extends Figure {
         final double radius;
 
-        Circle(double radius) { this.radius = radius; }
+        Circle(double radius) {
+            this.radius = radius;
+        }
 
-        @Override double area() { return Math.PI * (radius * radius); }
+        @Override
+        double area() {
+            return Math.PI * (radius * radius);
+        }
     }
 
     class Square extends Rectangle {
