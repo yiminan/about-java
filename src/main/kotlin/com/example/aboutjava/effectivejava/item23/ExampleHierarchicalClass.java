4a892570f0828472b0966053498245a4b5045ae8
orgojy <giveawesome@gmail.com>
2022-02-27 22:50:59 +0900

    Add ExampleHierarchicalClass to show good case hierarchical class

--- src/main/kotlin/com/example/aboutjava/effectivejava/item23/ExampleHierarchicalClass.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item23/ExampleHierarchicalClass.java
@@ -1,4 +1,35 @@
 package com.example.aboutjava.effectivejava.item23;
 
-class ExampleHierarchicalClass {
+/**
+ * 클래스 계층 구조로의 리팩터링한 예시<p>
+ */
+class ExampleHierarchicalClass implements UseHierarchicalClassThanClassWithTag {
+    abstract class Figure {
+        abstract double area();
+    }
+
+    class Rectangle extends Figure {
+        final double length;
+        final double width;
+
+        Rectangle(double length, double width) {
+            this.length = length;
+            this.width  = width;
+        }
+        @Override double area() { return length * width; }
+    }
+
+    class Circle extends Figure {
+        final double radius;
+
+        Circle(double radius) { this.radius = radius; }
+
+        @Override double area() { return Math.PI * (radius * radius); }
+    }
+
+    class Square extends Rectangle {
+        Square(double side) {
+            super(side, side);
+        }
+    }
 }
