5d59374e138b43fcb26823f0450c5f6eca6935d3
orgojy <giveawesome@gmail.com>
2022-02-27 22:40:28 +0900

    Update UseHierarchicalClassThanClassWithTag to apply type interface

--- src/main/kotlin/com/example/aboutjava/effectivejava/item23/UseHierarchicalClassThanClassWithTag.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item23/UseHierarchicalClassThanClassWithTag.java
@@ -4,5 +4,5 @@ package com.example.aboutjava.effectivejava.item23;
  * "item 23 - 태그 달린 클래스보다는 클래스 계층구조를 활용해야 합니다."<p>
  * todo: 커멘트 작성
  */
-class UseHierarchicalClassThanClassWithTag {
+interface UseHierarchicalClassThanClassWithTag {
 }
