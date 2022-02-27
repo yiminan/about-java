1e411220796a95390cfe7f7e873e7b4c92b69bac
orgojy <giveawesome@gmail.com>
2022-02-27 22:56:21 +0900

    Update comment for class ExampleClassWithTag

--- src/main/kotlin/com/example/aboutjava/effectivejava/item23/ExampleClassWithTag.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item23/ExampleClassWithTag.java
@@ -2,6 +2,10 @@ package com.example.aboutjava.effectivejava.item23;
 
 /**
  * item 23의 잘못된 태그형태 사용 예시<p>
+ * <p>
+ * a. 쓸데없는 코드가 너무 많다 : 열거 타입선언, 태그 필드, switch문<p>
+ * b. 장황하고 오류를 내기 쉽고 비효율적이다.<p>
+ * c. 클래스 계층 구조를 어설프레 흉내낸 것이다.<p>
  */
 class ExampleClassWithTag implements UseHierarchicalClassThanClassWithTag {
     static class Figure {
