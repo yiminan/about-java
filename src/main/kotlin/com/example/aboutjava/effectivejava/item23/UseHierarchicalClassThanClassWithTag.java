4e539b6dd5ced93f4ea6e97d77e7c170e9b99221
orgojy <giveawesome@gmail.com>
2022-02-28 21:47:48 +0900

    Update comment for class UseHierarchicalClassThanClassWithTag

--- src/main/kotlin/com/example/aboutjava/effectivejava/item23/UseHierarchicalClassThanClassWithTag.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item23/UseHierarchicalClassThanClassWithTag.java
@@ -2,7 +2,10 @@ package com.example.aboutjava.effectivejava.item23;
 
 /**
  * "item 23 - 태그 달린 클래스보다는 클래스 계층구조를 활용해야 합니다."<p>
- * todo: 커멘트 작성
+ * <p>
+ * 태그 달린 클래스를 써야 하는 상황은 거의 없습니다.<p>
+ * 새로운 클래스를 작성하는 데 태그 필드가 등장한다면 태그를 없애고 계층구조로 대체하는 방법을 생각해보아야 합니다.<p>
+ * 기존 클래스가 태그 필드를 사용하고 있다면 계층구조로 리팩터링하는 걸 고민해보아야합니다.<p>
  */
 interface UseHierarchicalClassThanClassWithTag {
 }
