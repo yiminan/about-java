91d6043094c4da359c1278a0ceae997001e78172
orgojy <giveawesome@gmail.com>
2022-03-03 23:42:49 +0900

    Update comment for class MakeMemberClassWithStatic

--- src/main/kotlin/com/example/aboutjava/effectivejava/item24/MakeMemberClassWithStatic.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item24/MakeMemberClassWithStatic.java
@@ -4,6 +4,7 @@ package com.example.aboutjava.effectivejava.item24;
  * "Item 24 - 멤버 클래스는 되도록 static으로 만들어야 합니다"<p>
  * <p>
  * 중첩 클래스(nested class)란 다른 클래스 안에 정의된 클래스를 말합니다.<p>
+ * <p>
  * 중첩 클래스의 종류<p>
  * 1. 정적 멤버 클래스<p>
  * 2. 비정적 멤버 클래스<p>
