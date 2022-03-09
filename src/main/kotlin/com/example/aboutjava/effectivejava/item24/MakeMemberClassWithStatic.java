2043e1357494e12e04351fd9cee95d19e62bc14a
orgojy <giveawesome@gmail.com>
2022-03-09 23:38:55 +0900

    Update comment for class MakeMemberClassWithStatic

--- src/main/kotlin/com/example/aboutjava/effectivejava/item24/MakeMemberClassWithStatic.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item24/MakeMemberClassWithStatic.java
@@ -21,6 +21,7 @@ package com.example.aboutjava.effectivejava.item24;
  * <p>
  * 중첩 클래스의 인스턴스가 바깥 인스턴스와 독립적으로 존재할 수 있다면 정적 멤버 클래스로 만들어야합니다.<p>
  * 비정적 멤버 클래스는 바깥 인스턴스 없이는 생성할 수 없기 때문입니다.<p>
+ * 비정적 멤버 클래스의 인스턴스와 바깥 인스턴스 사이의 관계는 멤버 클래스가 인스턴스화될 때 확립하고, 변경할 수 없다.<p>
  */
 class MakeMemberClassWithStatic {
     public static class Calculator {
