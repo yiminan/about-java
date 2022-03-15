d8505b03d30090d3647288ab1ca3f11048e1ed19
orgojy <giveawesome@gmail.com>
2022-03-16 00:01:32 +0900

    Update comment for class DontUseRawType

--- src/main/kotlin/com/example/aboutjava/effectivejava/item26/DontUseRawType.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item26/DontUseRawType.java
@@ -2,6 +2,8 @@ package com.example.aboutjava.effectivejava.item26;
 
 /**
  * "Item 26 - Raw Type 은 사용하지 말아야합니다"<p>
+ * <p>
+ * 클래스와 인터페이스 선언에 타입 매개변(type parameter)가 쓰이면, 제네릭 클래스 혹은 제네릭 인터페이스라 한다.<p>
  */
 class DontUseRawType {
 }
