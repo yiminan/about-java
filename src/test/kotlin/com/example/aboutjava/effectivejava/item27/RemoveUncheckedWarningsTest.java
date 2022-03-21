3c3d9795b230ca4b0678dd28687d4afb1a639463
orgojy <giveawesome@gmail.com>
2022-03-21 21:14:45 +0900

    Update RemoveUncheckedWarningsTest to apply display name

--- src/test/kotlin/com/example/aboutjava/effectivejava/item27/RemoveUncheckedWarningsTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item27/RemoveUncheckedWarningsTest.java
@@ -1,7 +1,10 @@
 package com.example.aboutjava.effectivejava.item27;
 
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Item 27 - 비검사 경고를 제거해야 합니다.")
 class RemoveUncheckedWarningsTest {
 
 }
