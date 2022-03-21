6219c80a28b483e61ae13fae5b5955e8ad2a49fa
orgojy <giveawesome@gmail.com>
2022-03-21 21:21:48 +0900

    Update test case for makeUncheckedWarnings

--- src/test/kotlin/com/example/aboutjava/effectivejava/item27/RemoveUncheckedWarningsTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item27/RemoveUncheckedWarningsTest.java
@@ -1,10 +1,21 @@
 package com.example.aboutjava.effectivejava.item27;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Item 27 - 비검사 경고를 제거해야 합니다.")
 class RemoveUncheckedWarningsTest {
-
+    @DisplayName("비검사 경고 발생 예시")
+    @Test
+    void makeUncheckedWarnings() {
+        // given & when
+        Set<String> names = new HashSet();
+        // then
+        assertThat(names).isNotNull();
+    }
 }
