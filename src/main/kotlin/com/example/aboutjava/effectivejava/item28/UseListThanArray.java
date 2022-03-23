c6ee173969a2f57b3048512f017e54cdc87e584a
orgojy <giveawesome@gmail.com>
2022-03-23 23:15:05 +0900

    Update comment for UseListThanArray

--- src/main/kotlin/com/example/aboutjava/effectivejava/item28/UseListThanArray.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item28/UseListThanArray.java
@@ -2,6 +2,10 @@ package com.example.aboutjava.effectivejava.item28;
 
 /**
  * "Item 28 - 배열보다는 리스트를 사용해야 합니다"<p>
+ * <p>
+ * 배열과 제네릭 타입에는 중요한 차이가 있습니다.<p>
+ * 배열 - 공변(함께 변한다), 제네릭 - 불공변(함께 변하지 않는다)<p>
+ * Sub가 Super의 하위 타입이라면 배열 Sub[]는 배열 Super[]의 하위 타입이 된다.<p>
  */
 class UseListThanArray {
 }
