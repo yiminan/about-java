66d332f5e94394faf036075a4cfe74e1eec750b1
orgojy <giveawesome@gmail.com>
2022-03-24 22:59:00 +0900

    Update failAtRuntimeWithArray for UseListThanArrayTest

--- src/test/kotlin/com/example/aboutjava/effectivejava/item28/UseListThanArrayTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item28/UseListThanArrayTest.java
@@ -1,10 +1,18 @@
 package com.example.aboutjava.effectivejava.item28;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("Item 28 - 배열보다는 리스트를 사용합니다.")
 class UseListThanArrayTest {
-
+    @DisplayName("배열을 사용하여, 런타임에 실패하는 경우")
+    @Test
+    void failAtRuntimeWithArray() {
+        // given
+        Object[] objects = new Long[1];
+        // when & then
+        Assertions.assertThatExceptionOfType(ArrayStoreException.class)
+                .isThrownBy(() -> objects[0] = "타입이 달라 넣을 수 없다.");
+    }
 }
