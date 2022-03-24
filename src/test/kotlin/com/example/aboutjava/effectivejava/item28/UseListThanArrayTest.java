0ffb9968e84924d3503c1eb0f145f5ad1b49f69d
orgojy <giveawesome@gmail.com>
2022-03-24 22:51:16 +0900

    Add Test class UseListThanArrayTest

--- src/test/kotlin/com/example/aboutjava/effectivejava/item28/UseListThanArrayTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item28/UseListThanArrayTest.java
@@ -2,6 +2,9 @@ package com.example.aboutjava.effectivejava.item28;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * "Item 28 - 배열보다는 리스트를 사용합니다"<p>
+ */
 class UseListThanArrayTest {
 
 }
