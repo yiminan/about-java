521891447d46990ece8dcbe4461095da673521f6
orgojy <giveawesome@gmail.com>
2022-03-24 22:52:10 +0900

    Update display name for UseListThanArrayTest

--- src/test/kotlin/com/example/aboutjava/effectivejava/item28/UseListThanArrayTest.java
+++ src/test/kotlin/com/example/aboutjava/effectivejava/item28/UseListThanArrayTest.java
@@ -1,10 +1,10 @@
 package com.example.aboutjava.effectivejava.item28;
 
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * "Item 28 - 배열보다는 리스트를 사용합니다"<p>
- */
+@DisplayName("Item 28 - 배열보다는 리스트를 사용합니다.")
 class UseListThanArrayTest {
 
 }
