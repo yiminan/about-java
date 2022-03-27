1faae3d7013d2ee3dba130dfc3f70e353caa5d34
orgojy <giveawesome@gmail.com>
2022-03-27 23:38:51 +0900

    Update comment for class MakeGenericTypeIfPossible

--- src/main/kotlin/com/example/aboutjava/effectivejava/item29/MakeGenericTypeIfPossible.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item29/MakeGenericTypeIfPossible.java
@@ -2,6 +2,8 @@ package com.example.aboutjava.effectivejava.item29;
 
 /**
  * "Item 29 - 이왕이면 제네릭 타입으로 만들어야 합니다"<p>
+ * <p>
+ * JDK가 제공하는 제네릭 타입과 메서드를 사용하는 일은 일반적으로 쉬운 편이지만, 제네릭 타입을 새로 만드는 일은 조금 더 어렵습니다.<p>
  */
 class MakeGenericTypeIfPossible {
 }
