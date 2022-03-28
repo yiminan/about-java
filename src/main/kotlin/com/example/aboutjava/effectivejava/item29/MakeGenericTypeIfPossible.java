3655bd1abc7f1be0456c45be68f7e3f924292192
orgojy <giveawesome@gmail.com>
2022-03-28 22:36:42 +0900

    Update Stack2.java to show example

--- src/main/kotlin/com/example/aboutjava/effectivejava/item29/MakeGenericTypeIfPossible.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item29/MakeGenericTypeIfPossible.java
@@ -44,4 +44,40 @@ class MakeGenericTypeIfPossible {
             return result;
         }
     }
+
+    /**
+     * 제네릭 스택으로 가는 첫 단계 - 컴파일되지 않는다.
+     */
+    private static class Stack2<E> {
+        private E[] elements;
+        private int size = 0;
+        private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+        public Stack2() {
+            elements = (E[]) new Object[DEFAULT_INITIAL_CAPACITY];
+        }
+
+        public void push(E e) {
+            ensureCapacity();
+            elements[size++] = e;
+        }
+
+        // Corrected version of pop method (Page 27)
+        public Object pop() {
+            if (size == 0)
+                throw new EmptyStackException();
+            Object result = elements[--size];
+            elements[size] = null; // Eliminate obsolete reference
+            return result;
+        }
+
+        /**
+         * Ensure space for at least one more element, roughly
+         * doubling the capacity each time the array needs to grow.
+         */
+        private void ensureCapacity() {
+            if (elements.length == size)
+                elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
 }
