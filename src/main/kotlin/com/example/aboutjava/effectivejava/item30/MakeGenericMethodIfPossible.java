adab5a3f615e1cc6d8afd57b9c5b45bb89dc120d
orgojy <giveawesome@gmail.com>
2022-04-01 23:05:32 +0900

    Update example method for union

--- src/main/kotlin/com/example/aboutjava/effectivejava/item30/MakeGenericMethodIfPossible.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item30/MakeGenericMethodIfPossible.java
@@ -1,7 +1,16 @@
 package com.example.aboutjava.effectivejava.item30;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * "Item 30 - 이왕이면 제네릭 메서드로 만들어라"<p>
  */
 class MakeGenericMethodIfPossible {
+
+    public static Set union(Set s1, Set s2) {
+        Set result = new HashSet(s1);
+        result.addAll(s2);
+        return result;
+    }
 }
