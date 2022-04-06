ddf3dd7ad1531ae2af631fe547c79ab6cef99c7f
orgojy <giveawesome@gmail.com>
2022-04-06 17:14:42 +0900

    Add InterfaceToStringTest to show examples

---
+++ src/test/kotlin/com/example/aboutjava/example/_interface/tostring/InterfaceToStringTest.java
@@ -0,0 +1,9 @@
+package com.example.aboutjava.example._interface.tostring;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InterfaceToStringTest {
+}
