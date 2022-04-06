8475390474d2553b61910712227f81dd17429313
orgojy <giveawesome@gmail.com>
2022-04-06 17:17:14 +0900

    Update display name for InterfaceToStringTest

--- src/test/kotlin/com/example/aboutjava/example/_interface/tostring/InterfaceToStringTest.java
+++ src/test/kotlin/com/example/aboutjava/example/_interface/tostring/InterfaceToStringTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("인터페이스의 예시를 보기위한 테스트")
 class InterfaceToStringTest {
     @DisplayName("구현 클래스에서 toString()을 상속하고 있는지 여부 확인")
     @Test
