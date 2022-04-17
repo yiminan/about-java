f3e52450579525c59a9f65e61068ddf2bb4badbd
orgojy <giveawesome@gmail.com>
2022-04-17 23:26:41 +0900

    Test case StringTest to concat String

--- src/test/kotlin/com/example/aboutjava/example/_string/StringTest.java
+++ src/test/kotlin/com/example/aboutjava/example/_string/StringTest.java
@@ -1,7 +1,15 @@
 package com.example.aboutjava.example._string;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("`String` 의 사용법을 학습하기 위한 테스트")
 class StringTest {
+    @DisplayName("String의 concat(): 문자열 합치기")
+    @Test
+    void concat() {
+        assertThat("ab".concat("cd")).isEqualTo("abcd");
+    }
 }
