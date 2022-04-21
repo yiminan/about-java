05c62977bd2639fbe6ec03064ad4bed2d849bea8
orgojy <giveawesome@gmail.com>
2022-04-21 23:57:35 +0900

    Update MagicLiteralOrNumber to show comment

--- src/main/kotlin/com/example/aboutjava/example/magicliteral/MagicLiteralOrNumber.java
+++ src/main/kotlin/com/example/aboutjava/example/magicliteral/MagicLiteralOrNumber.java
@@ -2,15 +2,15 @@ package com.example.aboutjava.example.magicliteral;
 
 class MagicLiteralOrNumber {
     /**
-     * 1.매직 넘버가 그대로 사용된 예시
+     * 1.매직 넘버를 상수로 만든 예시
      */
-    static final class PositiveNumber {
+    static final class PositiveNumberWithConstant {
 
         private static final int MINIMUM_POSITIVE_NUMBER = 1; // 상수 선언
 
         private final int value;
 
-        public PositiveNumber(int value) {
+        public PositiveNumberWithConstant(int value) {
             if (value < MINIMUM_POSITIVE_NUMBER) {
                 throw new IllegalArgumentException(String.format("value(%d)는 0 또는 음수가 될 수 없습니다.", value));
             }
