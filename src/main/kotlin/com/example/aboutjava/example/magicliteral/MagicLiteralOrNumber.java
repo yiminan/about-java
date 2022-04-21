9086e20d17f196358c2492c6126944e82e0d8d4f
orgojy <giveawesome@gmail.com>
2022-04-21 23:59:06 +0900

    Update MagicLiteralOrNumber to show example PositiveNumberWithExtractedMethod

--- src/main/kotlin/com/example/aboutjava/example/magicliteral/MagicLiteralOrNumber.java
+++ src/main/kotlin/com/example/aboutjava/example/magicliteral/MagicLiteralOrNumber.java
@@ -17,4 +17,23 @@ class MagicLiteralOrNumber {
             this.value = value;
         }
     }
+
+    /**
+     * 2.매직 넘버를 메서드로 추출 고립시킨 예시
+     */
+    static final class PositiveNumberWithExtractedMethod {
+
+        private final int value;
+
+        public PositiveNumberWithExtractedMethod(int value) {
+            if (isNotPositiveNumber(value)) { // 메서드 추출
+                throw new IllegalArgumentException(String.format("value(%d)는 0 또는 음수가 될 수 없습니다.", value));
+            }
+            this.value = value;
+        }
+
+        private boolean isNotPositiveNumber(int value) {
+            return value < 1;
+        }
+    }
 }
