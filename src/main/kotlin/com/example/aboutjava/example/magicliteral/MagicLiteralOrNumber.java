b75c7c19c3f382949d6ecc9e7ad0a647a613e52f
orgojy <giveawesome@gmail.com>
2022-04-21 23:56:13 +0900

    Update MagicLiteralOrNumber to show example PositiveNumber with magic number

--- src/main/kotlin/com/example/aboutjava/example/magicliteral/MagicLiteralOrNumber.java
+++ src/main/kotlin/com/example/aboutjava/example/magicliteral/MagicLiteralOrNumber.java
@@ -1,4 +1,20 @@
 package com.example.aboutjava.example.magicliteral;
 
 class MagicLiteralOrNumber {
+    /**
+     * 1.매직 넘버가 그대로 사용된 예시
+     */
+    static final class PositiveNumber {
+
+        private static final int MINIMUM_POSITIVE_NUMBER = 1; // 상수 선언
+
+        private final int value;
+
+        public PositiveNumber(int value) {
+            if (value < MINIMUM_POSITIVE_NUMBER) {
+                throw new IllegalArgumentException(String.format("value(%d)는 0 또는 음수가 될 수 없습니다.", value));
+            }
+            this.value = value;
+        }
+    }
 }
