44d8c2983f7444dab178fb544ada3dd9f1a21b2c
orgojy <giveawesome@gmail.com>
2022-05-01 23:41:25 +0900

    Update IncreaseApiFlexibilityByUsingQualifyingWildcard to apply comment

--- src/main/kotlin/com/example/aboutjava/effectivejava/item31/IncreaseApiFlexibilityByUsingQualifyingWildcard.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item31/IncreaseApiFlexibilityByUsingQualifyingWildcard.java
@@ -2,6 +2,8 @@ package com.example.aboutjava.effectivejava.item31;
 
 /**
  * Item 31 - 한정적 와일드카드를 사용해 API 유연성을 높여야합니다.<p>
+ * Item 28 에서 이야기 하듯, 매개변수화 타입은 불공변(invariant)입니다.<p>
  */
 class IncreaseApiFlexibilityByUsingQualifyingWildcard {
+
 }
