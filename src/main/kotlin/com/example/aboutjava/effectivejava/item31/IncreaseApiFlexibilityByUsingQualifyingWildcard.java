3e73384650cb50f6020a0e3491ab1e8a9134b06b
orgojy <giveawesome@gmail.com>
2022-05-02 23:47:51 +0900

    Update IncreaseApiFlexibilityByUsingQualifyingWildcard to apply comment

--- src/main/kotlin/com/example/aboutjava/effectivejava/item31/IncreaseApiFlexibilityByUsingQualifyingWildcard.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item31/IncreaseApiFlexibilityByUsingQualifyingWildcard.java
@@ -3,6 +3,8 @@ package com.example.aboutjava.effectivejava.item31;
 /**
  * Item 31 - 한정적 와일드카드를 사용해 API 유연성을 높여야합니다.<p>
  * Item 28 에서 이야기 하듯, 매개변수화 타입은 불공변(invariant)입니다.<p>
+ * 즉, 서로 다른 타입 Type1과 Type2가 있을 때,<p>
+ * List&#60;Type1&#62;은 List&#60;Type2&#62;의 하위 타입도 상위 타입도 아니다.<p>
  */
 class IncreaseApiFlexibilityByUsingQualifyingWildcard {
 
