76c9d51af466201705959de728137b74723f0d9d
orgojy <giveawesome@gmail.com>
2022-05-03 23:14:39 +0900

    Update IncreaseApiFlexibilityByUsingQualifyingWildcard to apply comment

--- src/main/kotlin/com/example/aboutjava/effectivejava/item31/IncreaseApiFlexibilityByUsingQualifyingWildcard.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item31/IncreaseApiFlexibilityByUsingQualifyingWildcard.java
@@ -5,6 +5,7 @@ package com.example.aboutjava.effectivejava.item31;
  * Item 28 에서 이야기 하듯, 매개변수화 타입은 불공변(invariant)입니다.<p>
  * 즉, 서로 다른 타입 Type1과 Type2가 있을 때,<p>
  * List&#60;Type1&#62;은 List&#60;Type2&#62;의 하위 타입도 상위 타입도 아니다.<p>
+ * 직관적이지 않겠지만 List&#60;String&#62;은 List&#60;Object&#62;가 하는 일을 제대로 수행하지 못하니,<p>
  */
 class IncreaseApiFlexibilityByUsingQualifyingWildcard {
 
