316b2fe87378afee73d101f68827297b5d002efb
orgojy <giveawesome@gmail.com>
2022-05-08 23:45:31 +0900

    Update IncreaseApiFlexibilityByUsingQualifyingWildcard to apply comment

--- src/main/kotlin/com/example/aboutjava/effectivejava/item31/IncreaseApiFlexibilityByUsingQualifyingWildcard.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item31/IncreaseApiFlexibilityByUsingQualifyingWildcard.java
@@ -7,6 +7,7 @@ package com.example.aboutjava.effectivejava.item31;
  * List&#60;Type1&#62;은 List&#60;Type2&#62;의 하위 타입도 상위 타입도 아니다.<p>
  * 직관적이지 않겠지만 List&#60;String&#62;은 List&#60;Object&#62;가 하는 일을 제대로 수행하지 못하니,<p>
  * 하위 타입이 될 수 없습니다.(리스코프 치완 원칙에 어긋납니다. Item 10 참조)<p>
+ * 하지만 때로는 불공변 방식보다 유연한 방식이 필요합니다.<p>
  */
 class IncreaseApiFlexibilityByUsingQualifyingWildcard {
 
