446d9596047206165672822de1ae2dc718bd3578
orgojy <giveawesome@gmail.com>
2022-07-14 00:02:58 +0900

    Add LocalDateTimeExample to show examples

--- src/test/kotlin/com/example/aboutjava/example/time/LocalDateTimeExample.java
+++ src/test/kotlin/com/example/aboutjava/example/time/LocalDateTimeExample.java
@@ -1,34 +1,30 @@
 package com.example.aboutjava.example.time;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LocalDateTimeExample {
-    public static void main(String[] args) {
+
+    @DisplayName("ZoneOffset과 ZoneId를 이용한 Instant 생성")
+    @Test
+    void createInstantByZoneOffsetOrZoneId() {
+        java.time.LocalDateTime now = java.time.LocalDateTime.now();
         /**
          * UTC를 기준으로 시간을 표현해줍니다.
          */
-        Instant utcPlus9 = new LocalDateTime().instant(ZoneOffset.of("+09:00"));
+        Instant utcPlus9DateTime = now.toInstant(ZoneOffset.of("+09:00"));
         /**
          * Summer Time을 고려해서 해당 나라의 시간을 표시해주는 장점이 있습니다.
          * Summer Time과 관계 없는 지역의 시간은 ZoneOffset과 같은 결과를 내어줍니다.
          */
-        Instant seoulTime = new LocalDateTime().instant(ZoneId.of("Asia/Seoul"));
+        Instant asiaSeoulDateTime = now.atZone(ZoneId.of("Asia/Seoul")).toInstant();
 
-        System.out.println(utcPlus9);
-        System.out.println(seoulTime);
-    }
-
-    private static class LocalDateTime {
-        private final java.time.LocalDateTime now = java.time.LocalDateTime.now();
-
-        public Instant instant(ZoneOffset zoneOffset) {
-            return now.toInstant(zoneOffset);
-        }
-
-        public Instant instant(ZoneId zoneId) {
-            return now.atZone(zoneId).toInstant();
-        }
+        assertThat(utcPlus9DateTime).isEqualTo(asiaSeoulDateTime);
     }
 }
