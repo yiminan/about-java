bb098b0777290f8b06009d340c10132a515308b3
orgojy <giveawesome@gmail.com>
2022-07-16 23:51:15 +0900

    Update PersonTest.java to use Person.java divided from ParserTest

--- src/test/kotlin/com/example/aboutjava/example/parser/ParserTest.java
+++ src/test/kotlin/com/example/aboutjava/example/parser/ParserTest.java
@@ -2,12 +2,9 @@ package com.example.aboutjava.example.parser;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -26,36 +23,5 @@ class ParserTest {
 
         ObjectMapper objectMapper = new ObjectMapper();
         assertThat(objectMapper.convertValue(map, Person.class)).isEqualTo(new Person("Ryan", 20, "M"));
-    }
-
-    private static class Person {
-
-        private final String name;
-        private final int age;
-        private final String sex;
-
-        @JsonCreator
-        public Person(@JsonProperty("name") String name, @JsonProperty("age") int age, @JsonProperty("sex") String sex) {
-            this.name = name;
-            this.age = age;
-            this.sex = sex;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Person person = (Person) o;
-            return age == person.age && Objects.equals(name, person.name) && Objects.equals(sex, person.sex);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name, age, sex);
-        }
     }
 }
