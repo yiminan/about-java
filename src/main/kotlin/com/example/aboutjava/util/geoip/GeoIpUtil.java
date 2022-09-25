05e7e57371a0f2e3633c743ecf2eebbf59b7231c
orgojy <giveawesome@gmail.com>
2022-09-25 23:56:00 +0900

    Update GeoIpUtil.java to get information for Geo location

---
+++ src/main/kotlin/com/example/aboutjava/util/geoip/GeoIpUtil.java
@@ -0,0 +1,50 @@
+package com.example.aboutjava.util.geoip;
+
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
+import java.io.IOException;
+import java.net.InetAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class GeoIpUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(GeoIpUtil.class);
+
+    private static final DatabaseReader dbReader;
+
+    static {
+        try {
+            dbReader = new DatabaseReader
+                    .Builder(GeoIpUtil.class.getClassLoader().getResourceAsStream("GeoLite2-City.mmdb"))
+                    .build();
+        } catch (IOException e) {
+            throw new IllegalStateException("Geo IP data initialize failed", e);
+        }
+    }
+
+    public static GeoIp getLocation(String ip) {
+        try {
+            InetAddress ipAddress = InetAddress.getByName(ip);
+            CityResponse response = dbReader.city(ipAddress);
+            String country = response.getCountry().getIsoCode();
+            String accessSubdivision = response.getSubdivisions()
+                    .stream()
+                    .map(v -> v.getNames().get("en"))
+                    .findFirst()
+                    .orElse(null);
+            String cityName = response.getCity().getName();
+            String postal = response.getPostal().getCode();
+            String timezone = response.getLocation().getTimeZone();
+            String latitude = response.getLocation().getLatitude().toString();
+            String longitude = response.getLocation().getLongitude().toString();
+            return new GeoIp(ip, country, cityName, postal, timezone, latitude, longitude);
+        } catch (IOException e) {
+            log.error("Unknown Host", e);
+        } catch (GeoIp2Exception e) {
+            log.error("Failed to get GeoIP", e);
+        }
+        return null;
+    }
+}
