dee4ab158b2e8e335ecbfc287aa4765114072b8c
orgojy <giveawesome@gmail.com>
2022-10-05 23:22:31 +0900

    Minor IncreaseApiFlexibilityByUsingQualifyingWildcard.java to remove unused imports

--- src/main/kotlin/com/example/aboutjava/effectivejava/item31/IncreaseApiFlexibilityByUsingQualifyingWildcard.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item31/IncreaseApiFlexibilityByUsingQualifyingWildcard.java
@@ -1,9 +1,6 @@
 package com.example.aboutjava.effectivejava.item31;
 
-import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
-import net.bytebuddy.dynamic.scaffold.MethodGraph.Linked;
 
 /**
  * Item 31 - 한정적 와일드카드를 사용해 API 유연성을 높여야합니다.<p> Item 28 에서 이야기 하듯, 매개변수화 타입은 불공변(invariant)입니다.<p> 즉, 서로 다른 타입 Type1과 Type2가 있을 때,<p>
