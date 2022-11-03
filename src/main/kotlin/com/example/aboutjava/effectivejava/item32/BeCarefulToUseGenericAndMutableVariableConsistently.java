7c28f5afb17d7c03ee54fa4b4d2118e2ad115747
orgojy <giveawesome@gmail.com>
2022-11-04 00:30:04 +0900

    Update Test.java to rename into class BeCarefulToUseGenericAndMutableVariableConsistently

--- src/main/kotlin/com/example/aboutjava/effectivejava/item32/BeCarefulToUseGenericAndMutableVariableConsistently.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item32/BeCarefulToUseGenericAndMutableVariableConsistently.java
@@ -3,6 +3,6 @@ package com.example.aboutjava.effectivejava.item32;
 /**
  * Item 32 - 제네릭과 가변인수를 함께 쓸 때는 신중하라.<p>
  */
-class Test {
+class BeCarefulToUseGenericAndMutableVariableConsistently {
 
 }
