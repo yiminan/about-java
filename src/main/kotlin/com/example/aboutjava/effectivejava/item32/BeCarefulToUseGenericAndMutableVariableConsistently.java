64c635c9a039794ffe867df634404e08dc16d1a8
orgojy <giveawesome@gmail.com>
2022-11-06 23:47:12 +0900

    Update BeCarefulToUseGenericAndMutableVariableConsistently to show dangerous example

--- src/main/kotlin/com/example/aboutjava/effectivejava/item32/BeCarefulToUseGenericAndMutableVariableConsistently.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item32/BeCarefulToUseGenericAndMutableVariableConsistently.java
@@ -1,8 +1,16 @@
 package com.example.aboutjava.effectivejava.item32;
 
+import java.util.List;
+
 /**
  * Item 32 - 제네릭과 가변인수를 함께 쓸 때는 신중하라.<p>
  */
 class BeCarefulToUseGenericAndMutableVariableConsistently {
 
+    static void dangerous(List<String>... stringLists) {
+        List<Integer> intList = List.of(42);
+        Object[] objects = stringLists;
+        objects[0] = intList; // 힙 오염 발생
+        String s = stringLists[0].get(0); // ClassCastException
+    }
 }
