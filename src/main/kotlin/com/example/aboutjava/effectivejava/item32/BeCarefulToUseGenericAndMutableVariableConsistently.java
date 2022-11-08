7236d737b5f03f18644332bc1ee44d73f3834e5f
orgojy <giveawesome@gmail.com>
2022-11-08 23:59:44 +0900

    Update BeCarefulToUseGenericAndMutableVariableConsistently to apply comment for class

--- src/main/kotlin/com/example/aboutjava/effectivejava/item32/BeCarefulToUseGenericAndMutableVariableConsistently.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item32/BeCarefulToUseGenericAndMutableVariableConsistently.java
@@ -3,7 +3,9 @@ package com.example.aboutjava.effectivejava.item32;
 import java.util.List;
 
 /**
- * Item 32 - 제네릭과 가변인수를 함께 쓸 때는 신중하라.<p>
+ * Item 32 - 제네릭과 가변인수를 함께 쓸 때는 신중해야합니다.<p>
+ * <p>
+ * 가변인수는 메서드에 넘기는 인수의 개수를 클라이언트가 조절할 수 있게 해주는데, 구현 방식에 허점이 있다.<p>
  */
 class BeCarefulToUseGenericAndMutableVariableConsistently {
 
