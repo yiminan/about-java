0f8006e86d64c2146fbd74035e3182de7c56f6e4
orgojy <giveawesome@gmail.com>
2022-11-10 23:55:05 +0900

    Update BeCarefulToUseGenericAndMutableVariableConsistently to apply comment for class

--- src/main/kotlin/com/example/aboutjava/effectivejava/item32/BeCarefulToUseGenericAndMutableVariableConsistently.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item32/BeCarefulToUseGenericAndMutableVariableConsistently.java
@@ -7,6 +7,8 @@ import java.util.List;
  * <p>
  * 가변인수는 메서드에 넘기는 인수의 개수를 클라이언트가 조절할 수 있게 해주는데, 구현 방식에 허점이 있다.<p>
  * 가변인수 메서드를 호출하면 가변인수를 담기 위한 배열이 자동으로 하나 만들어진다.<p>
+ * 메서드를 선언할 때 실체화 불가 타입으로 varargs 매개변수를 선언하면 컴파일러가 경고를 보낸다.<p>
+ * 가변인수 메서드를 호출할 때도 varargs 매개변수가 실체화 불가 타입으로 추론되면, 그 호출에 대해서도 경고를 낸다.<p>
  */
 class BeCarefulToUseGenericAndMutableVariableConsistently {
 
