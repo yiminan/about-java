ab9b9f4089329b0b793f2cc5e5e3abd0b3ce777c
orgojy <giveawesome@gmail.com>
2022-12-03 20:41:23 +0900

    Update ConsiderTypeSafeHeterogeneousContainer.java to add example interface 'Favorites'

--- src/main/kotlin/com/example/aboutjava/effectivejava/item33/ConsiderTypeSafeHeterogeneousContainer.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item33/ConsiderTypeSafeHeterogeneousContainer.java
@@ -2,7 +2,11 @@ package com.example.aboutjava.effectivejava.item33;
 
 /**
  * Item 33 - 타입 안정성을 위해서 이종 컨테이너를 고려해야합니다.<p>
+ *
  */
 class ConsiderTypeSafeHeterogeneousContainer {
-
+    interface Favorites {
+        public <T> void putFavorite(Class<T> type, T instance);
+        public <T> void getFavorite(Class<T> type);
+    }
 }
