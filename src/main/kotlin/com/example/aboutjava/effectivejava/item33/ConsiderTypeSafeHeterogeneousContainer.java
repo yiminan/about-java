40b2199ffc3bb88fd7b55a9f8100f9284e096661
orgojy <giveawesome@gmail.com>
2022-12-06 23:50:53 +0900

    Add ConsiderTypeSafeHeterogeneousContainer.FavoritesImpl.java to implement example for Favorites.java

--- src/main/kotlin/com/example/aboutjava/effectivejava/item33/ConsiderTypeSafeHeterogeneousContainer.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item33/ConsiderTypeSafeHeterogeneousContainer.java
@@ -1,5 +1,9 @@
 package com.example.aboutjava.effectivejava.item33;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * Item 33 - 타입 안정성을 위해서 이종 컨테이너를 고려해야합니다.<p>
  */
@@ -14,4 +18,23 @@ class ConsiderTypeSafeHeterogeneousContainer {
 
         public <T> T getFavorite(Class<T> type);
     }
+
+    /**
+     * Type safe heterogeneous container pattern - implementation
+     */
+    private static class FavoritesImpl implements Favorites {
+
+        private Map<Class<?>, Object> favorites = new HashMap<>();
+
+        @Override
+        public <T> void putFavorite(Class<T> type, T instance) {
+            favorites.put(Objects.requireNonNull(type), instance);
+        }
+
+        @Override
+        public <T> T getFavorite(Class<T> type) {
+            return type.cast(favorites.get(type));
+        }
+    }
+
 }
