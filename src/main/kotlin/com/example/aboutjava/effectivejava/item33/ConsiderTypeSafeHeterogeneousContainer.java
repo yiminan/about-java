6991438609b0518344210938099a60cb1c9f83d8
orgojy <giveawesome@gmail.com>
2022-12-07 23:56:37 +0900

    Update ConsiderTypeSafeHeterogeneousContainer.java to add running main method for example of Favorites.java

--- src/main/kotlin/com/example/aboutjava/effectivejava/item33/ConsiderTypeSafeHeterogeneousContainer.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item33/ConsiderTypeSafeHeterogeneousContainer.java
@@ -37,4 +37,16 @@ class ConsiderTypeSafeHeterogeneousContainer {
         }
     }
 
+    public static void main(String[] args) {
+        Favorites favorites = new FavoritesImpl();
+        favorites.putFavorite(String.class, "Java");
+        favorites.putFavorite(Integer.class, 0xcafebabe);
+        favorites.putFavorite(Class.class, Favorites.class);
+
+        String favoriteString = favorites.getFavorite(String.class);
+        int favoriteInteger = favorites.getFavorite(Integer.class);
+        Class<?> favoriteClass = favorites.getFavorite(Class.class);
+
+        System.out.printf("%s %x %s%n", favoriteString, favoriteInteger, favoriteClass.getName());
+    }
 }
