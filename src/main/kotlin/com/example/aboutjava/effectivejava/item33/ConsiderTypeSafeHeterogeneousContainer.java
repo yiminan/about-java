99e2f139654ba35df9c4cc005e50cd29b872d98d
orgojy <giveawesome@gmail.com>
2022-12-08 23:44:04 +0900

    Update ConsiderTypeSafeHeterogeneousContainer.java to apply comment for class

--- src/main/kotlin/com/example/aboutjava/effectivejava/item33/ConsiderTypeSafeHeterogeneousContainer.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item33/ConsiderTypeSafeHeterogeneousContainer.java
@@ -6,6 +6,9 @@ import java.util.Objects;
 
 /**
  * Item 33 - 타입 안정성을 위해서 이종 컨테이너를 고려해야합니다.<p>
+ * <p>
+ * 컬렉션 API로 대표되는 일반적인 제네릭 형태에는 한 컨테이너가 다룰 수 있는 타입 매개변수의 수가 고정되어 있다.<p>
+ * 하지만 컨테이너 자체가 아닌 키를 타입 매개변수로 바꾸면 이런 제약이 없는 타입 안전 이종 컨테이너를 만들 수 있다.<p>
  */
 class ConsiderTypeSafeHeterogeneousContainer {
 
@@ -28,7 +31,7 @@ class ConsiderTypeSafeHeterogeneousContainer {
 
         @Override
         public <T> void putFavorite(Class<T> type, T instance) {
-            favorites.put(Objects.requireNonNull(type), instance);
+            favorites.put(Objects.requireNonNull(type), type.cast(instance));
         }
 
         @Override
