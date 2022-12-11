93b862d1fd5ab7c1b64314a9f387a9aca677db3e
orgojy <giveawesome@gmail.com>
2022-12-11 21:32:35 +0900

    Update UseEnumTypeInsteadOfIntConstant.java to add show example 'int enum pattern'

--- src/main/kotlin/com/example/aboutjava/effectivejava/item34/UseEnumTypeInsteadOfIntConstant.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item34/UseEnumTypeInsteadOfIntConstant.java
@@ -1,5 +1,17 @@
 package com.example.aboutjava.effectivejava.item34;
 
+/**
+ * Item 34 - int 상수 대신 열거 타입을 사용하라.<p>
+ */
 class UseEnumTypeInsteadOfIntConstant {
 
+    /**
+     * 정수 열거 패턴(int enum pattern) 기법에는 단점이 많다.<p> 타입 안전을 보장할 방법이 없으며 표현력도 좋지 않다.
+     */
+    private static final int APPLE_FUJI = 0;
+    private static final int APPLE_PIPPIN = 1;
+    private static final int APPLE_GRANNY_SMITH = 2;
+    private static final int ORANGE_NAVEL = 0;
+    private static final int ORANGE_TEMPLE = 1;
+    private static final int ORANGE_BLOOD = 2;
 }
