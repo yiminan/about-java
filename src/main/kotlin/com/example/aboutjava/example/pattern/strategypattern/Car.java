38cec97010515c5abbf5c5e83704991b46b9c8d7
orgojy <giveawesome@gmail.com>
2022-12-14 16:36:00 +0900

    Add Car.java to use strategy pattern

---
+++ src/main/kotlin/com/example/aboutjava/example/pattern/strategypattern/Car.java
@@ -0,0 +1,34 @@
+package com.example.aboutjava.example.pattern.strategypattern;
+
+class Car {
+
+    private static final int FIRST_POSITION = 0;
+    private static final int POSITION_UNIT = 1;
+
+    private int position;
+
+    public Car() {
+        this(FIRST_POSITION);
+    }
+
+    private Car(int position) {
+        this.position = position;
+    }
+
+    public void move(MovingStrategy movingStrategy) {
+        if (movingStrategy.movable()) {
+            System.out.println("Car is moving");
+            movePosition();
+        } else {
+            System.out.println("Car is not moving");
+        }
+    }
+
+    private void movePosition() {
+        position = position + POSITION_UNIT;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
