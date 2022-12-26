fa13391e69f6049eed2555ca9666214dcac8b550
orgojy <giveawesome@gmail.com>
2022-12-26 23:53:57 +0900

    Update UseEnumTypeInsteadOfIntConstant.java to apply comment for class Operation

--- src/main/kotlin/com/example/aboutjava/effectivejava/item34/UseEnumTypeInsteadOfIntConstant.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item34/UseEnumTypeInsteadOfIntConstant.java
@@ -6,7 +6,7 @@ package com.example.aboutjava.effectivejava.item34;
 class UseEnumTypeInsteadOfIntConstant {
 
     /**
-     * 정수 열거 패턴(int enum pattern) 기법에는 단점이 많다.<p> 타입 안전을 보장할 방법이 없으며 표현력도 좋지 않다.
+     * 정수 열거 패턴(int enum pattern) 기법에는 단점이 많다.<p>타입 안전을 보장할 방법이 없으며 표현력도 좋지 않다.
      */
     private static final int APPLE_FUJI = 0;
     private static final int APPLE_PIPPIN = 1;
@@ -20,7 +20,8 @@ class UseEnumTypeInsteadOfIntConstant {
     private enum Orange {NAVEL, TEMPLE, BLOOD}
 
     /**
-     * 상수별 메서드 구현을 활용한 열거 타입
+     * 상수별 메서드 구현을 활용한 열거 타입<p>
+     * 각 상수는 하나의 상수 클래스 구현체와 같기에 추상 메서드를 개별 구현할 수 있다.
      */
     private enum Operation {
         PLUS("+") {
