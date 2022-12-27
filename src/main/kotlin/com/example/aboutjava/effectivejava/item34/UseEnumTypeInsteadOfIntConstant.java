812c8fbf46af847114671aeff5b3df44926eccca
orgojy <giveawesome@gmail.com>
2022-12-28 00:14:48 +0900

    Update UseEnumTypeInsteadOfIntConstant.java to add example enum 'PayrollDay'

--- src/main/kotlin/com/example/aboutjava/effectivejava/item34/UseEnumTypeInsteadOfIntConstant.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item34/UseEnumTypeInsteadOfIntConstant.java
@@ -20,8 +20,7 @@ class UseEnumTypeInsteadOfIntConstant {
     private enum Orange {NAVEL, TEMPLE, BLOOD}
 
     /**
-     * 상수별 메서드 구현을 활용한 열거 타입<p>
-     * 각 상수는 하나의 상수 클래스 구현체와 같기에 추상 메서드를 개별 구현할 수 있다.
+     * 상수별 메서드 구현을 활용한 열거 타입<p> 각 상수는 하나의 상수 클래스 구현체와 같기에 추상 메서드를 개별 구현할 수 있다.
      */
     private enum Operation {
         PLUS("+") {
@@ -62,6 +61,41 @@ class UseEnumTypeInsteadOfIntConstant {
     }
 
     private enum PayrollDay {
+        MONDAY(PayType.WEEKDAY), TUESDAY(PayType.WEEKDAY), WEDNESDAY(PayType.WEEKDAY),
+        THURSDAY(PayType.WEEKDAY), FRIDAY(PayType.WEEKDAY),
+        SATURDAY(PayType.WEEKEND), SUNDAY(PayType.WEEKEND);
 
+        private final PayType payType;
+
+        PayrollDay(PayType payType) {
+            this.payType = payType;
+        }
+
+        int pay(int hoursWorked, int payRate) {
+            return payType.pay(hoursWorked, payRate);
+        }
+
+        enum PayType {
+            WEEKDAY {
+                int overtimePay(int minsWorked, int payRate) {
+                    return minsWorked <= MINS_PER_SHIFT ? 0 :
+                            (minsWorked - MINS_PER_SHIFT) * payRate / 2;
+                }
+            },
+            WEEKEND {
+                int overtimePay(int minsWorked, int payRate) {
+                    return minsWorked * payRate / 2;
+                }
+            };
+
+            abstract int overtimePay(int minsWorked, int payRate);
+
+            private static final int MINS_PER_SHIFT = 8 * 60;
+
+            int pay(int minsWorked, int payRate) {
+                int basePay = minsWorked * payRate;
+                return basePay + overtimePay(minsWorked, payRate);
+            }
+        }
     }
 }
