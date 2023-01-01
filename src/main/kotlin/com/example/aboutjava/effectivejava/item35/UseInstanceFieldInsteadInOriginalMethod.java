0c833e38e24a5c0ec57d4f95bf1d867773967867
orgojy <giveawesome@gmail.com>
2023-01-01 23:13:52 +0900

    Add UseInstanceFieldInsteadInOriginalMethod.java to apply comment

--- src/main/kotlin/com/example/aboutjava/effectivejava/item35/UseInstanceFieldInsteadInOriginalMethod.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item35/UseInstanceFieldInsteadInOriginalMethod.java
@@ -1,5 +1,10 @@
 package com.example.aboutjava.effectivejava.item35;
 
+/**
+ * Item 35 - ordinal 인덱스 대신 인스턴스 필드를 사용하라.<p>
+ * <p>
+ *
+ */
 public class UseInstanceFieldInsteadInOriginalMethod {
 
 }
