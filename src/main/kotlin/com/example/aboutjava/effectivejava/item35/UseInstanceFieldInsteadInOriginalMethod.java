f6c998311ac5853a39d35083c02abf7859ba3093
orgojy <giveawesome@gmail.com>
2023-01-02 22:44:56 +0900

    Update UseInstanceFieldInsteadInOriginalMethod.java to add example enum 'Ensemble'

--- src/main/kotlin/com/example/aboutjava/effectivejava/item35/UseInstanceFieldInsteadInOriginalMethod.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item35/UseInstanceFieldInsteadInOriginalMethod.java
@@ -3,8 +3,18 @@ package com.example.aboutjava.effectivejava.item35;
 /**
  * Item 35 - ordinal 인덱스 대신 인스턴스 필드를 사용하라.<p>
  * <p>
- *
  */
 public class UseInstanceFieldInsteadInOriginalMethod {
 
+    /**
+     * "잘못된 형태의 enum 사용 예" ordinal 메서드는 열거 타입 상수가 정의된 순서를 반환한다.<p> 동작은 하지만 유지보수에 끔찍하다.<p>
+     */
+    private enum Ensemble {
+        SOLO, DUET, TRIO, QUARTET, QUINTET,
+        SEXTET, SEPTET, OCTET, NONET, DECTET;
+
+        public int numberOfMusicians() {
+            return ordinal() + 1;
+        }
+    }
 }
