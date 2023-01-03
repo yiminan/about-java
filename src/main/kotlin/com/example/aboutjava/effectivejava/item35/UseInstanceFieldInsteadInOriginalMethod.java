9c3623666f09728a2fce5358692970bb8588216d
orgojy <giveawesome@gmail.com>
2023-01-03 19:31:27 +0900

    Add GoodCase.java to add good case example

--- src/main/kotlin/com/example/aboutjava/effectivejava/item35/UseInstanceFieldInsteadInOriginalMethod.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item35/UseInstanceFieldInsteadInOriginalMethod.java
@@ -17,4 +17,19 @@ public class UseInstanceFieldInsteadInOriginalMethod {
             return ordinal() + 1;
         }
     }
+
+    private enum GoodCase {
+        SOLO(1), DUET(2), TRIO(3), QUARTET(4), QUINTET(5),
+        SEXTET(6), SEPTET(7), OCTET(8), NONET(9), DECTET(10);
+
+        private final int numberOfMusicians;
+
+        GoodCase(int numberOfMusicians) {
+            this.numberOfMusicians = numberOfMusicians;
+        }
+
+        public int numberOfMusicians() {
+            return numberOfMusicians;
+        }
+    }
 }
