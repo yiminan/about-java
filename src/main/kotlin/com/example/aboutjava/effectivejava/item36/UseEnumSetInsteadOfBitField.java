ca678be26f211914212686bca6c77fee23167d30
orgojy <giveawesome@gmail.com>
2023-01-10 23:22:38 +0900

    Add UseEnumSetInsteadOfBitField.NewText.class to apply example case

--- src/main/kotlin/com/example/aboutjava/effectivejava/item36/UseEnumSetInsteadOfBitField.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item36/UseEnumSetInsteadOfBitField.java
@@ -1,5 +1,7 @@
 package com.example.aboutjava.effectivejava.item36;
 
+import java.util.Set;
+
 /**
  * Item 36 - 비트 필드 대신 EnumSet을 사용하라.<p>
  */
@@ -9,12 +11,22 @@ class UseEnumSetInsteadOfBitField {
      * 비트 필드 열거 상수 - 구닥다리 기법!<p>
      */
     private class OldText {
+
         public static final int STYLE_BOLD = 1 << 0;
         public static final int STYLE_ITALIC = 1 << 1;
         public static final int STYLE_UNDERLINE = 1 << 2;
         public static final int STYLE_STRIKETHROUGH = 1 << 3;
 
         public void applyStyles(int styles) {
+            // ...
+        }
+    }
+
+    private static class NewText {
+
+        public enum Style {BOLD, ITALIC, UNDERLINE, STRIKETHROUGH}
+
+        public void applyStyles(Set<Style> styles) {
             // ...
         }
     }
