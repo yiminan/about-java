e936a2277732fa2a45ec065e2bdc520c5193ceb8
orgojy <giveawesome@gmail.com>
2023-01-10 23:18:26 +0900

    Update UseEnumSetInsteadOfBitField.Text.class to modify name into 'OldText'

--- src/main/kotlin/com/example/aboutjava/effectivejava/item36/UseEnumSetInsteadOfBitField.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item36/UseEnumSetInsteadOfBitField.java
@@ -8,7 +8,7 @@ class UseEnumSetInsteadOfBitField {
     /**
      * 비트 필드 열거 상수 - 구닥다리 기법!<p>
      */
-    private class Text {
+    private class OldText {
         public static final int STYLE_BOLD = 1 << 0;
         public static final int STYLE_ITALIC = 1 << 1;
         public static final int STYLE_UNDERLINE = 1 << 2;
