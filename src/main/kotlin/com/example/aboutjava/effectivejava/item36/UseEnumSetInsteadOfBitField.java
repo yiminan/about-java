b6c2f186c65f3f8b4386c4f8e0a017791115e892
orgojy <giveawesome@gmail.com>
2023-01-11 23:39:57 +0900

    Update UseEnumSetInsteadOfBitField to apply class comment

--- src/main/kotlin/com/example/aboutjava/effectivejava/item36/UseEnumSetInsteadOfBitField.java
+++ src/main/kotlin/com/example/aboutjava/effectivejava/item36/UseEnumSetInsteadOfBitField.java
@@ -4,6 +4,11 @@ import java.util.Set;
 
 /**
  * Item 36 - 비트 필드 대신 EnumSet을 사용하라.<p>
+ * <p>
+ * 열거할 수 있는 타입을 한데 모아 집합 형태로 사용한다고 해도 비트 필드를 사용할 이유는 없다.<p>
+ * EnumSet 클래스는 비트 필드를 대체할 만한 성능과 기능을 제공한다.<p>
+ * EnumSet의 유일한 단점이라면 (자바 9까지는 아직) 불변 EnumSet을 만들 수 없다는 것이다.<p>
+ * Collections.unmodifiableSet(EnumSet)을 사용하면 불변 EnumSet을 만들 수 있다.<p>
  */
 class UseEnumSetInsteadOfBitField {
 
