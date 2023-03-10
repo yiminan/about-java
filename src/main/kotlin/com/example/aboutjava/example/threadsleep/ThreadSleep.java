1dcec1c2a5e2c3aead7f8e9a478ca082db729f79
orgojy <giveawesome@gmail.com>
2023-03-10 22:37:18 +0900

    Update ThreadSleep.java to apply comment about Thread

--- src/main/kotlin/com/example/aboutjava/example/threadsleep/ThreadSleep.java
+++ src/main/kotlin/com/example/aboutjava/example/threadsleep/ThreadSleep.java
@@ -2,17 +2,21 @@ package com.example.aboutjava.example.threadsleep;
 
 import java.util.stream.IntStream;
 
+/**
+ * Thread.sleep()을 사용한 예제
+ */
 class ThreadSleep {
     public static void main(String[] args) {
-        IntStream.rangeClosed(1, 10).forEach(v -> {
-            System.out.println(v);
-            sleep();
-        });
+        IntStream.rangeClosed(1, 10)
+                .forEach(v -> {
+                    System.out.println(v);
+                    sleep();
+                });
     }
 
     private static void sleep() {
         try {
-            Thread.sleep(3_000);
+            Thread.sleep(1_000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
